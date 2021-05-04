c9f84d12d81df2cb7d1eb980cdc8ad9d8391c3b1
Ryan Lafferty <73855198+ryandialpad@users.noreply.github.com>
2021-05-04 12:09:42 -0400

    Add Unit Tests Workflow (#1)
    * Add Unit Tests Workflow 
    * Fix broken mocha tests as a result of a bad default babel config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset',
+    // Installed with @vue/cli-plugin-babel
+    '@babel/preset-env',
+  ],
+  plugins: [
+    // Installed with @vue/cli-plugin-babel
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/transform-runtime',
   ],
 };
