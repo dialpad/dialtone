9667081e30aec18ed6a60326fff2be5b6ecf0107
Brad Paugh <brad.paugh@dialpad.com>
2021-05-13 09:47:54 -0700

    put constants in constants file. fix long lines

--- components/button/button.vue
+++ components/button/button.vue
@@ -26,60 +26,15 @@
 <script>
 import Vue from 'vue';
 
-export const BUTTON_SIZE_MODIFIERS = {
-  xs: 'd-btn--xs',
-  sm: 'd-btn--sm',
-  md: '',
-  lg: 'd-btn--lg',
-  xl: 'd-btn--xl',
-};
-
-export const BUTTON_KIND_MODIFIERS = {
-  default: '',
-  danger: 'd-btn--danger',
-  inverted: 'd-btn--inverted',
-};
-
-export const BUTTON_IMPORTANCE_MODIFIERS = {
-  clear: '',
-  primary: 'd-btn--primary',
-  outlined: 'd-btn--outlined',
-};
-
-export const BUTTON_TYPES = ['submit', 'reset', 'button'];
-
-export const ICON_POSITION_MODIFIERS = {
-  left: 'd-btn__icon--left',
-  right: 'd-btn__icon--right',
-};
-
-export const LINK_KIND_MODIFIERS = {
-  default: 'd-link',
-  warning: 'd-link--warning',
-  danger: 'd-link--danger',
-  success: 'd-link--success',
-  muted: 'd-link--muted',
-  inverted: 'd-link--inverted',
-};
-
-export const INVALID_COMBINATION = [
-  { circle: true, kind: 'default', importance: 'primary', message: invalidCombinationMessage(true, 'default', 'primary') },
-  { circle: true, kind: 'danger', importance: 'outlined', message: invalidCombinationMessage(true, 'danger', 'outlined') },
-];
-
-function invalidCombinationMessage (circle, kind, importance) {
-  return `You cannot not have a ${circle ? 'circle ' : ''}button with kind: ${kind} and importance: ${importance} as it does not exist in our design system. See https://dialpad.design/components/button for a list of available button styles`;
-}
-
-function isInvalidPropCombination (circle, kind, importance) {
-  for (const row of INVALID_COMBINATION) {
-    if (circle === row.circle && kind === row.kind && importance === row.importance) {
-      console.error(invalidCombinationMessage(circle, kind, importance));
-      return false;
-    }
-  }
-  return true;
-}
+import {
+  BUTTON_SIZE_MODIFIERS,
+  BUTTON_KIND_MODIFIERS,
+  BUTTON_IMPORTANCE_MODIFIERS,
+  BUTTON_TYPES,
+  ICON_POSITION_MODIFIERS,
+  LINK_KIND_MODIFIERS,
+  INVALID_COMBINATION,
+} from './button_constants';
 
 /**
  * Base Vue component for Dialtone Buttons.
@@ -230,13 +185,27 @@ export default {
 
     buttonClasses () {
       if (this.link) {
-        return ['d-link', LINK_KIND_MODIFIERS[this.linkKind], BUTTON_SIZE_MODIFIERS[this.size]];
+        return ['d-link',
+          LINK_KIND_MODIFIERS[this.linkKind],
+          BUTTON_SIZE_MODIFIERS[this.size],
+        ];
       }
-      return ['d-btn', BUTTON_IMPORTANCE_MODIFIERS[this.importance], BUTTON_KIND_MODIFIERS[this.kind], BUTTON_SIZE_MODIFIERS[this.size], { 'd-btn--circle': this.circle, 'd-btn--loading': this.loading }];
+      return ['d-btn',
+        BUTTON_IMPORTANCE_MODIFIERS[this.importance],
+        BUTTON_KIND_MODIFIERS[this.kind],
+        BUTTON_SIZE_MODIFIERS[this.size],
+        {
+          'd-btn--circle': this.circle,
+          'd-btn--loading': this.loading,
+        },
+      ];
     },
 
     iconClasses () {
-      return ['d-btn__icon', ICON_POSITION_MODIFIERS[this.iconPosition]];
+      return [
+        'd-btn__icon',
+        ICON_POSITION_MODIFIERS[this.iconPosition],
+      ];
     },
   },
 
@@ -250,8 +219,20 @@ export default {
           Vue.util.warn('You cannot enable circle and link at the same time', this);
         }
 
-        isInvalidPropCombination(this.circle, this.kind, this.importance);
+        this.isInvalidPropCombination(this.circle, this.kind, this.importance);
       },
+    },
+  },
+
+  methods: {
+    isInvalidPropCombination (circle, kind, importance) {
+      for (const row of INVALID_COMBINATION) {
+        if (circle === row.circle && kind === row.kind && importance === row.importance) {
+          console.error(row.message);
+          return false;
+        }
+      }
+      return true;
     },
   },
 };
