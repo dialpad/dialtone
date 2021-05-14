92aa5bad7e3fabae579810df1739272953fb3d3f
Brad Paugh <brad.paugh@dialpad.com>
2021-05-13 17:41:20 -0700

    Button documentation for vue dt6 update

---
+++ components/button/button_default.story.vue
@@ -0,0 +1,38 @@
+<template>
+  <hs-button
+    :importance="importance"
+    :type="type"
+    :size="size"
+    :kind="kind"
+    :circle="circle"
+    :loading="loading"
+    :assertive-on-focus="assertiveOnFocus"
+    :link="link"
+    :link-kind="linkKind"
+    :icon-position="iconPosition"
+    :disabled="disabled"
+    :style="{ width: width }"
+    @click="onClick"
+    @focusout="onFocusOut"
+    @focusin="onFocusIn"
+  >
+    <span v-html="defaultSlot" />
+    <template
+      v-if="icon"
+      #icon
+    >
+      <component :is="icon" />
+    </template>
+  </hs-button>
+</template>
+
+<script>
+import HsButton from './button';
+import icon from '../mixins/icon';
+
+export default {
+  name: 'ButtonDefault',
+  components: { HsButton },
+  mixins: [icon],
+};
+</script>
---
+++ components/button/button_variants.story.vue
@@ -0,0 +1,111 @@
+<template>
+  <div id="elements-button--variants-container">
+    <div class="d-mb16">
+      <!-- Default -->
+      <hs-button>
+        Primary
+      </hs-button>
+
+      <!-- Outlined -->
+      <hs-button importance="outlined">
+        Outlined
+      </hs-button>
+
+      <!-- Clear -->
+      <hs-button importance="clear">
+        Clear
+      </hs-button>
+    </div>
+    <div class="d-mb16">
+      <!-- Danger -->
+      <hs-button kind="danger">
+        Danger
+      </hs-button>
+
+      <!-- Inverted -->
+      <hs-button kind="inverted">
+        Inverted
+      </hs-button>
+
+      <!-- Disabled -->
+      <hs-button disabled>
+        Disabled
+      </hs-button>
+    </div>
+    <div class="d-mb16">
+      <!-- Extra Small -->
+      <hs-button size="xs">
+        Extra Small
+      </hs-button>
+
+      <!-- Small -->
+      <hs-button size="sm">
+        Small
+      </hs-button>
+
+      <!-- Medium -->
+      <hs-button size="md">
+        Medium
+      </hs-button>
+
+      <!-- Large -->
+      <hs-button size="lg">
+        Large
+      </hs-button>
+
+      <!-- Extra Large -->
+      <hs-button size="xl">
+        Extra Large
+      </hs-button>
+    </div>
+    <div class="d-mb16">
+      <!-- Circle -->
+      <hs-button
+        importance="outlined"
+        circle
+      >
+        <template #icon>
+          <icon-dialpad-glyph />
+        </template>
+      </hs-button>
+
+      <!-- Icon Left -->
+      <hs-button>
+        <template #icon>
+          <icon-dialpad-glyph />
+        </template>
+        Icon Left
+      </hs-button>
+
+      <!-- Icon Right -->
+      <hs-button icon-position="right">
+        <template #icon>
+          <icon-dialpad-glyph />
+        </template>
+        Icon Right
+      </hs-button>
+    </div>
+    <div class="d-mb16">
+      <!-- Loading -->
+      <hs-button loading>
+        Button
+      </hs-button>
+    </div>
+    <div class="d-mb16">
+      <!-- Link Button -->
+      <hs-button link>
+        Link Button
+      </hs-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import HsButton from './button';
+import IconDialpadGlyph from '@dialpad/dialtone/lib/dist/vue/icons/IconDialpadGlyph';
+
+export default {
+  name: 'ButtonVariants',
+  components: { HsButton, IconDialpadGlyph },
+};
+</script>
