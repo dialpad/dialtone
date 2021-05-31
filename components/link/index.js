d1d998abe5adcda5be3fcb229a8c071398da5c32
Nikita <81983630+nikitadialpad@users.noreply.github.com>
2021-05-31 23:43:17 +0300

    Link. remove unnecessary import (#64)

--- components/link/index.js
+++ components/link/index.js
@@ -1,2 +1,2 @@
 export { default as DtLink } from './link.vue';
-export { LINK_VARIANTS, LINK_VARIANTS_CLASSES, LINK_MODIFIER_CLASSES } from './link_constants';
+export { LINK_VARIANTS, LINK_VARIANTS_CLASSES } from './link_constants';
