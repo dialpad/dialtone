21160deaf5292b08e300a1416af548dd0806aea4
juliodialpad <julio.ortega@dialpad.com>
2021-11-18 14:49:06 -0600

    vue-compat install and config

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,20 @@ module.exports = {
   css: { extract: false },
   chainWebpack: config => {
     config.externals({ '@dialpad/dialtone': '@dialpad/dialtone' });
+    config.resolve.alias.set('vue', '@vue/compat');
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2,
+            },
+          },
+        };
+      });
   },
 };
