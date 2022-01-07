2ca4577fafca59ece95bfa86a7b0e6a880141e39
Brad Paugh <brad.paugh@dialpad.com>
2022-01-07 09:53:58 -0800

    fix: include tabindex=-1 in focusFirstElement (#219)
    * fix: include tabindex=-1 in focusFirstElement
    
    * refactor to use filter
    
    * fix var names

--- components/mixins/modal.js
+++ components/mixins/modal.js
@@ -1,11 +1,6 @@
-const nonFocusableAttrs = 'not(:disabled):not([aria-disabled="true"]):not([tabindex="-1"])';
-const focusableElementsList = `button:${nonFocusableAttrs},
-                           [href]:${nonFocusableAttrs},
-                           input:${nonFocusableAttrs},
-                           select:${nonFocusableAttrs},
-                           textarea:${nonFocusableAttrs},
-                           details:${nonFocusableAttrs},
-                           [tabindex]:not([tabindex="-1"])`;
+const focusableAttrs = ':not(:disabled):not([aria-disabled="true"])';
+const tabbableAttrs = `${focusableAttrs}:not([tabindex="-1"])`;
+const focusableElementsList = `button,[href],input,select,textarea,details,[tabindex]`;
 
 /**
  * this mixin provides the methods to automatically trap tab focus within
@@ -14,18 +9,19 @@ const focusableElementsList = `button:${nonFocusableAttrs},
  * on things like important actionable alerts. Use focusFirstElement to
  * focus on the first tabbable element within your component, and call
  * focusTrappedTabPress every time tab is pressed to trap tab within this
- * component.
+ * component. Note that focusFirstElement WILL focus elements with tabindex="-1",
+ * however focusTrappedTabPress will not.
  */
 export default {
   methods: {
     /**
-     * set focus to the first focusable element in your component
+     * set focus to the first focusable element in your component, includes tabindex="-1".
      * @param {object} el - optional - ref of dom element to trap focus on.
      *  will default to the root node of the vue component
      */
     async focusFirstElement (el) {
       await this.$nextTick();
-      const focusableElements = this._getFocusableElements(el);
+      const focusableElements = this._getFocusableElements(el, true);
       const elToFocus = this._getFirstFocusElement(focusableElements);
       elToFocus?.focus();
     },
@@ -54,18 +50,22 @@ export default {
      *
      * gets all the focusable elements within the component
      * and sets the first and last of those elements.
+     *
+     * @param {object} el - the root dom element to find focusable elements in.
+     * @param {bool} includeNegativeTabIndex - will include tabindex="-1" in the list of focusable elements.
      */
-    _getFocusableElements (el = this.$el) {
+    _getFocusableElements (el = this.$el, includeNegativeTabIndex = false) {
       const focusableContent = [...el.querySelectorAll(focusableElementsList)];
       return focusableContent.filter((fc) => {
         const style = window.getComputedStyle(fc);
         return style.getPropertyValue('display') !== 'none' &&
-          style.getPropertyValue('visibility') !== 'hidden';
+          style.getPropertyValue('visibility') !== 'hidden' &&
+          fc.matches(includeNegativeTabIndex ? focusableAttrs : tabbableAttrs);
       });
     },
 
     /**
-     * tabs to the next element contained within your component
+     * tabs to the next element contained within your component, does not include tabindex="-1".
      * @param {object} e - keypress event
      * @param {object} el - optional - ref of dom element to trap focus on.
      *  will default to the root node of the vue component
