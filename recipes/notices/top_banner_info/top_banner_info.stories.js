8f474073743c5123396252ec288a468affdaef4a
Yuyuan Liu <yuyuan.liu@dialpad.com>
2022-02-24 16:43:09 -0800

    feat: add top banner info component for call info display (#246)
    * Add top banner info component for call info display
    
    * update package loc
    
    * bump dialtone version
    
    * address cr: added contral for slots and prop, minor fixes
    
    * address cr, update doc
    
    Co-authored-by: Brad Paugh <brad.paugh@dialpad.com>

---
+++ recipes/notices/top_banner_info/top_banner_info.stories.js
@@ -0,0 +1,124 @@
+import { action } from '@storybook/addon-actions';
+import { createTemplateFromVueFile } from '@/common/storybook_utils';
+import DtRecipeTopBannerInfo from './top_banner_info';
+import DtRecipeTopBannerInfoMdx from './top_banner_info.mdx';
+import DtRecipeTopBannerInfoDefaultTemplate from './top_banner_info_default.story.vue';
+import DtRecipeTopBannerInfoVariantsTemplate from './top_banner_info_variants.story.vue';
+import { COLOR_CODES } from './top_banner_info_constants';
+
+// Default Prop Values
+export const argsData = {
+  colorCode: 'green400',
+  onEvent: action('event'),
+};
+
+/**
+ * example prop description decorator
+ */
+
+/*
+  Controls
+  ========
+
+  Here we define any custom controls or control overrides for our components.
+
+  By default storybook will attempt to provide an appropriate control of the same name for each property in the
+  component as well as include any description provided using a prop decorator within your component (see above).
+
+  Storybook will also attempt to provide an appropriate control for each slot in the component as well as include any
+  description provided using a slot decorator within your component (see below).
+
+  <!-- @slot example slot decorator -->
+*/
+export const argTypesData = {
+  // Props
+  colorCode: {
+    description: 'Background color of the banner',
+    control: {
+      type: 'select',
+      options: COLOR_CODES,
+    },
+  },
+
+  // Slots
+  default: {
+    description: 'Display content in the center of the banner',
+    table: {
+      type: { summary: 'VNode' },
+    },
+    control: {
+      type: 'text',
+    },
+  },
+  /*
+    We use the following naming scheme `<SLOT_NAME>Slot` for slot controls to prevent conflicts with props that share
+    the same name. We provide the correct name of the slot using the name control attribute to ensure that the argument
+    table and description within the controls accurately reflects the correct names of our component's props and slots.
+  */
+  left: {
+    name: 'left',
+    table: {
+      type: { summary: 'VNode' },
+    },
+    control: {
+      type: 'text',
+    },
+  },
+
+  right: {
+    name: 'right',
+    table: {
+      type: { summary: 'VNode' },
+    },
+    control: {
+      type: 'text',
+    },
+  },
+
+  // Action Event Handlers
+  onEvent: {
+    table: {
+      disable: true,
+    },
+  },
+};
+
+// Story Collection
+export default {
+  title: 'Recipes/Notices/Top Banner Info',
+  component: DtRecipeTopBannerInfo,
+  args: argsData,
+  argTypes: argTypesData,
+  excludeStories: /.*Data$/,
+  parameters: {
+    controls: {
+      sort: 'requiredFirst',
+    },
+    docs: {
+      page: DtRecipeTopBannerInfoMdx,
+    },
+    options: {
+      showPanel: true,
+    },
+  },
+};
+
+// Templates
+const DefaultTemplate = (args, { argTypes }) => createTemplateFromVueFile(
+  args,
+  argTypes,
+  DtRecipeTopBannerInfoDefaultTemplate,
+);
+const VariantsTemplate = (args, { argTypes }) => createTemplateFromVueFile(
+  args,
+  argTypes,
+  DtRecipeTopBannerInfoVariantsTemplate,
+);
+
+// Stories
+export const Default = DefaultTemplate.bind({});
+Default.args = {};
+
+export const Variants = VariantsTemplate.bind({});
+Variants.args = {};
+Variants.parameters = { controls: { disable: true }, actions: { disable: true }, options: { showPanel: false } };
---
+++ recipes/notices/top_banner_info/top_banner_info.test.js
@@ -0,0 +1,98 @@
+import { assert } from 'chai';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import DtRecipeTopBannerInfo from './top_banner_info.vue';
+import {
+  itBehavesLikeFailsCustomPropValidation,
+  itBehavesLikePassesCustomPropValidation,
+} from '../../../tests/shared_examples/validation';
+
+// Constants
+const basePropsData = {
+  colorCode: 'green400',
+};
+
+describe('DtRecipeTopBannerInfo Tests', function () {
+  // Wrappers
+  let wrapper;
+  let middleContent;
+  let rootElement;
+
+  // Environment
+  const propsData = basePropsData;
+  const attrs = {};
+  const slots = {
+    default: 'this is the content',
+  };
+  const provide = {};
+
+  // Helpers
+  const _setChildWrappers = () => {
+    middleContent = wrapper.find('.d-top-banner-info__middle');
+    rootElement = wrapper.find('[data-qa="banner-info"]');
+  };
+
+  const _setWrappers = () => {
+    wrapper = shallowMount(DtRecipeTopBannerInfo, {
+      propsData,
+      attrs,
+      slots,
+      provide,
+      localVue: this.localVue,
+    });
+    _setChildWrappers();
+  };
+
+  // Setup
+  before(function () {
+    this.localVue = createLocalVue();
+  });
+  beforeEach(function () {
+    _setWrappers();
+  });
+
+  describe('Presentation Tests', function () {
+    /*
+     * Test(s) to ensure that the component is correctly rendering
+     */
+
+    describe('When pass default content', function () {
+      it('Should render info banner component', function () {
+        assert.isTrue(wrapper.exists());
+      });
+      it('Should display content correctly', function () {
+        assert.strictEqual(middleContent.text(), slots.default);
+      });
+      it('Should display with default background color', function () {
+        assert.include(rootElement.classes(), 'd-bgc-green-400');
+      });
+    });
+
+    describe('When colorCode is passed', function () {
+      beforeEach(async function () {
+        await wrapper.setProps({ colorCode: 'black100' });
+      });
+
+      it('Should display with passed background color', function () {
+        assert.include(rootElement.classes(), 'd-bgc-black-100');
+      });
+    });
+  });
+
+  describe('Validation Tests', function () {
+    /*
+     * Test(s) to ensure that custom validators are working as expected
+     */
+    describe('ColorCode Validator', function () {
+      // Test Environment
+      const prop = DtRecipeTopBannerInfo.props.colorCode;
+
+      describe('When provided color code is in COLOR_CODES', function () {
+        itBehavesLikePassesCustomPropValidation(prop, prop.default);
+      });
+
+      describe('When provided color code is not in COLOR_CODES', function () {
+        itBehavesLikeFailsCustomPropValidation(prop, `INVALID_CODE`);
+      });
+    });
+  });
+});
