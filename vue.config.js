122573b1d87f9ab9173f6017917ab332a45af870
Julio Ortega <87546543+juliodialpad@users.noreply.github.com>
2022-03-24 14:02:05 -0600

    feat(vue 3): remove @vue/compat (#291)

--- vue.config.js
+++ vue.config.js
@@ -4,21 +4,6 @@ module.exports = {
   css: { extract: false },
   chainWebpack: config => {
     config.externals({ '@dialpad/dialtone': '@dialpad/dialtone' });
-    config.resolve.alias.set('vue', '@vue/compat');
-
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap(options => {
-        return {
-          ...options,
-          compilerOptions: {
-            compatConfig: {
-              MODE: 2,
-            },
-          },
-        };
-      });
   },
   configureWebpack: {
     resolve: {
