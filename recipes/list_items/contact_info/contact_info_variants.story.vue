c5dd5c21fba28965f5d15270457444b4b358acde
Julio Ortega <87546543+juliodialpad@users.noreply.github.com>
2022-03-28 15:31:06 -0600

    refactor(contact info): fix typo on function naming (#302)

--- recipes/list_items/contact_info/contact_info_variants.story.vue
+++ recipes/list_items/contact_info/contact_info_variants.story.vue
@@ -48,7 +48,7 @@
       <dt-button
         importance="clear"
         kind="muted"
-        @click="onGoupContactClick"
+        @click="onGroupContactClick"
       >
         <dt-recipe-contact-info>
           <template #header>
