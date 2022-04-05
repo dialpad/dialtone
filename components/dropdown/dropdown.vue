f9f1356e39639741197a47958dc499afc5aacf00
Brad Paugh <brad.paugh@dialpad.com>
2022-04-05 09:11:56 -0700

    feat(dropdown): add maxWidth, maxHeight, listClass props (#310)

--- components/dropdown/dropdown.vue
+++ components/dropdown/dropdown.vue
@@ -9,6 +9,8 @@
     padding="none"
     role="menu"
     :modal="modal"
+    :max-height="maxHeight"
+    :max-width="maxWidth"
     v-on="$listeners"
     @opened="updateInitialHighlightIndex"
     @keydown.enter="onEnterKey"
@@ -30,7 +32,7 @@
       <ul
         :id="listId"
         ref="listWrapper"
-        :class="['d-ps-relative', 'd-px0', DROPDOWN_PADDING_CLASSES[padding]]"
+        :class="['d-ps-relative', 'd-px0', DROPDOWN_PADDING_CLASSES[padding], listClass]"
         data-qa="dt-dropdown-list-wrapper"
         @mouseleave="clearHighlightIndex"
         @mousemove.capture="onMouseHighlight"
@@ -116,6 +118,24 @@ export default {
     },
 
     /**
+     * Determines maximum height for the popover before overflow.
+     * Possible units rem|px|em
+     */
+    maxHeight: {
+      type: String,
+      default: '',
+    },
+
+    /**
+     * Determines maximum width for the popover before overflow.
+     * Possible units rem|px|%|em
+     */
+    maxWidth: {
+      type: String,
+      default: '',
+    },
+
+    /**
      * Sets an ID on the list element of the component. Used by several aria attributes
      * as well as when deriving the IDs for each item.
      */
@@ -169,6 +189,14 @@ export default {
     onEndOfList: {
       type: Function,
       default: null,
+    },
+
+    /**
+     * Additional class for the wrapper list element.
+     */
+    listClass: {
+      type: [String, Array, Object],
+      default: '',
     },
   },
 
