35aca0199babc86f5046e3eba32a12d308385952
Brad Paugh <brad.paugh@dialpad.com>
2022-08-08 12:41:12 -0700

    fix(emoji): dynamic import fix (#443)
    * fix(emoji): attempt codesplitting fix
    
    * fix(emoji): modify webpack
    
    * Revert "fix(emoji): modify webpack"
    
    This reverts commit 7826634abff5d125b6c9d1e73d5e5a142af23d5a.
    
    * fix(emoji): change main to exports
    
    * update docs
    
    * update doc
    
    * fix custom emoji import, remove emoji loading

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,7 +6,7 @@ import theme from './theme';
 import Vue from 'vue';
 import fixDefaultSlot from '../components/plugins/fixDefaultSlot';
 import { setEmojiAssetUrlSmall, setEmojiAssetUrlLarge, setCustomEmojiUrl, setCustomEmojiJson } from '../common/emoji.js';
-import * as customEmojiJson from '@/common/custom-emoji.json';
+import customEmojiJson from '@/common/custom-emoji.json';
 import focusVisiblePolyfill from 'focus-visible';
 
 setEmojiAssetUrlSmall('https://static.dialpadcdn.com/joypixels/png/unicode/32/', '.png');
