ac2897e0983f7e7b24b41ea0dda4d3120c82084a
Julio Ortega <julio.ortega@dialpad.com>
2022-11-03 15:26:55 -0600

    beta 1

---
+++ src/icons/List.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg aria-hidden="true" focusable="false" data-name="List" class="d-icon d-icon--list" viewBox="0 0 12 12"><path fill="currentColor" fill-rule="evenodd" d="M3.5 3a.5.5 0 0 1 .5-.5h6.5a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.5-.5Zm0 3a.5.5 0 0 1 .5-.5h6.5a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.5-.5Zm0 3a.5.5 0 0 1 .5-.5h6.5a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.5-.5ZM1 3a.5.5 0 0 1 .5-.5h.005a.5.5 0 0 1 0 1H1.5A.5.5 0 0 1 1 3Zm0 3a.5.5 0 0 1 .5-.5h.005a.5.5 0 0 1 0 1H1.5A.5.5 0 0 1 1 6Zm0 3a.5.5 0 0 1 .5-.5h.005a.5.5 0 0 1 0 1H1.5A.5.5 0 0 1 1 9Z" clip-rule="evenodd"/></svg>
+</template>
