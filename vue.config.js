bc22d8961e76e51e601e24953076bd7d4b0ad52f
Julio Ortega <julio.ortega@dialpad.com>
2022-11-10 15:28:10 -0600

    feat: add svg files

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
+const {resolve} = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    configureWebpack: {
+        plugins: [
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: resolve(__dirname, 'src/svg'),
+                        to: resolve(__dirname, 'dist/svg'),
+                        toType: 'dir',
+                    },
+                ],
+            }),
+        ],
+    },
 })
