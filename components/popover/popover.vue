ea87aa7c966bb76466e1c6a5a89fdb4961193233
Brad Paugh <brad.paugh@dialpad.com>
2022-11-10 11:07:22 -0800

    Revert "fix(popover): prevent default context menu when openOnContext is true (#587)"
    This reverts commit 63ae93782dabb71a9d0602164df4881f64f9429f.

--- components/popover/popover.vue
+++ components/popover/popover.vue
@@ -20,7 +20,7 @@
         data-qa="dt-popover-anchor"
         :tabindex="openOnContext ? 0 : undefined"
         @click.capture="defaultToggleOpen"
-        @contextmenu="onContext"
+        @contextmenu.prevent="onContext"
         @keydown.up.prevent="onArrowKeyPress"
         @keydown.down.prevent="onArrowKeyPress"
         @wheel="(e) => (isOpen && modal) && e.preventDefault()"
@@ -656,8 +656,6 @@ export default {
 
     onContext (event) {
       if (!this.openOnContext) { return; }
-
-      event.preventDefault();
 
       this.tip?.setProps({
         placement: 'right-start',
