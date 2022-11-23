c1d7a1354b1e427e2feb29f55d92713d487a4995
Brad Paugh <brad.paugh@dialpad.com>
2022-11-23 12:32:58 -0800

    fix: rename swift package to use proper conventions

--- dist_ios/Package.swift
+++ dist_ios/Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "dialtone-tokens",
+    name: "DialtoneTokens",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "dialtone-tokens",
-            targets: ["dialtone-tokens"]),
+            name: "DialtoneTokens",
+            targets: ["DialtoneTokens"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "dialtone-tokens",
+            name: "DialtoneTokens",
             dependencies: []),
     ]
 )
