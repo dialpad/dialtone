4475ded66e632bdf767e45eb536f3afb9008e733
Brad Paugh <brad.paugh@dialpad.com>
2022-12-14 14:54:34 -0800

    fix(tooltip): use selector for focus visible rather than attr (#645)

--- components/tooltip/tooltip.vue
+++ components/tooltip/tooltip.vue
@@ -319,7 +319,7 @@ export default {
     },
 
     hasVisibleFocus () {
-      return getAnchor(this.$refs.anchor).hasAttribute('data-focus-visible-added');
+      return getAnchor(this.$refs.anchor).matches(':focus-visible');
     },
 
     onEnterAnchor (e) {
