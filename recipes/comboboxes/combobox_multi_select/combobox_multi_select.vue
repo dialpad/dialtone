5d92519e2ec625fbe88026ed3959445049e406fb
iropolo <89984179+iropolo@users.noreply.github.com>
2022-12-21 19:07:41 -0300

    fix(combobox-multi-select): firefox fix (#642)
    * Fix(combobox_multi_select): Firefox fix
    
    * Fix(combobox_multi_select): Update removed deprecated class
    
    * Fix(combobox_multi_select): Update to labelClass and clean code
    
    * Fix(combobox_multi_select): Clean code
    
    * Fix(chip): Code improvement
    
    Co-authored-by: iropolo <ignacio.ropolo@dialpad.com>

--- recipes/comboboxes/combobox_multi_select/combobox_multi_select.vue
+++ recipes/comboboxes/combobox_multi_select/combobox_multi_select.vue
@@ -14,7 +14,7 @@
     <template #input="{ onInput }">
       <span
         ref="inputSlotWrapper"
-        class="d-ps-relative"
+        class="d-ps-relative d-d-block"
       >
         <span
           ref="chipsWrapper"
@@ -24,7 +24,8 @@
             v-for="item in selectedItems"
             ref="chips"
             :key="item.id"
-            class="d-mt4 d-mx2 d-zi-base1"
+            :label-class="['d-chip__label']"
+            :class="['d-mt4', 'd-mx2', 'd-zi-base1']"
             :close-button-props="{ ariaLabel: 'close' }"
             :size="size"
             v-on="chipListeners"
@@ -38,7 +39,7 @@
         <dt-input
           ref="input"
           v-model="value"
-          class="d-fl-grow1 d-mb4"
+          class="d-fl-grow1"
           :aria-label="label"
           :label="labelVisible ? label : ''"
           :description="description"
