639dd92cac24d1944be19f9c68d6c4f855cba980
Jose Silva <jose.silva@dialpad.com>
2023-01-17 19:22:08 -0300

    feat: include CHANGELOG files in the package (#704)

--- release-local.config.js
+++ release-local.config.js
@@ -25,10 +25,13 @@ module.exports = {
       config: '@dialpad/conventional-changelog-angular',
     }],
     '@semantic-release/changelog',
+    '@dialpad/semantic-release-changelog-json',
     ['@semantic-release/npm', {
       npmPublish: false,
     }],
     ['@semantic-release/git', {
+      assets: ['CHANGELOG.md', 'CHANGELOG.json', 'package.json', 'package-lock.json', 'npm-shrinkwrap.json'],
+      /* eslint-disable-next-line no-template-curly-in-string */
       message: 'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}',
     }],
   ],
