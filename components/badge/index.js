4e3cb2056d96ae763851a61e9d886c059b287838
Brad Paugh <brad.paugh@dialpad.com>
2023-03-20 16:49:39 -0700

    fix(badge): export decoration modifiers (#847)

--- components/badge/index.js
+++ components/badge/index.js
@@ -1,2 +1,2 @@
 export { default as DtBadge } from './badge.vue';
-export { BADGE_TYPE_MODIFIERS, BADGE_KIND_MODIFIERS } from './badge_constants';
+export { BADGE_TYPE_MODIFIERS, BADGE_KIND_MODIFIERS, BADGE_DECORATION_MODIFIERS } from './badge_constants';
