a47a93a7c3b3a5d6499954ff17f33af31d623c6d
Julio Ortega <87546543+juliodialpad@users.noreply.github.com>
2023-03-20 11:06:45 -0600

    feat: generate unique ids (#17)

---
+++ src/baseIconTemplate.vue
@@ -0,0 +1,13 @@
+<template>
+/*SVG-CONTENT*/
+</template>
+
+<script>
+import { getUniqueString } from "@/utils";
+
+export default {
+  created () {
+    this.uniqueID = getUniqueString();
+  },
+}
+</script>
