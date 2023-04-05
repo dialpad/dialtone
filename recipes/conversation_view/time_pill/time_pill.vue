fe388a48a64fd007f840e728122f339b49ba6e1f
AlexLamDialpad <92543354+AlexLamDialpad@users.noreply.github.com>
2023-04-05 14:47:24 -0700

    feat(feed-items): create feed item time pill (#885)
    * feat(feed-items): create feed item time pill
    
    * Fill in story mdx
    
    * address PR comments
    
    * Add conversation view to yeo generator
    
    * Simplify validator
    
    * last CR comments

---
+++ recipes/conversation_view/time_pill/time_pill.vue
@@ -0,0 +1,48 @@
+<template>
+  <time
+    data-qa="dt-time-pill"
+    :dateTime="dateTime"
+    class="d-ba d-bar-pill d-baw1 d-bc-black-300 d-px16 d-py4 d-fs-100"
+  >
+    {{ dateTimeDisplay }}
+  </time>
+</template>
+
+<script>
+import {} from './time_pill_constants.js';
+
+export default {
+  name: 'DtRecipeTimePill',
+
+  props: {
+    /**
+     * Date time display value
+     */
+    dateTimeDisplay: {
+      type: String,
+      required: true,
+    },
+
+    /**
+     * Machine-readable attribute
+     * Accepts a string value of YYYY-MM-DD or YYYY-MM-DDThh:mm:ssTZD
+     */
+    dateTime: {
+      type: String,
+      required: true,
+      validator: (t) => {
+        // Since this will only ever be used in the context of a date, we're rejecting non date inputs
+        const x = new Date(t);
+        if (x instanceof Date && !isNaN(x)) {
+          return true;
+        }
+        return false;
+      },
+    },
+  },
+};
+</script>
+
+<style lang="less">
+
+</style>
---
+++ recipes/conversation_view/time_pill/time_pill_default.story.vue
@@ -0,0 +1,15 @@
+<template>
+  <dt-recipe-time-pill
+    :date-time="dateTime"
+    :date-time-display="dateTimeDisplay"
+  />
+</template>
+
+<script>
+import DtRecipeTimePill from './time_pill';
+
+export default {
+  name: 'DtRecipeTimePillDefault',
+  components: { DtRecipeTimePill },
+};
+</script>
