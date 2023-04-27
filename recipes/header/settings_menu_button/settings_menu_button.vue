e58daa53a5b3740511b2621e5fac6b05bb76d3b4
laurafirespotter <laura@dialpad.com>
2023-04-27 16:38:03 -0500

    feat(header): add setting menu button with update option (#947)
    * feat(header): add setting menu button with update option
    
    * Add ariaLabel prop
    
    * Code review feedback - remove variants, classes updates, add aria and click
    
    * vue3 updates
    
    * Fix click

---
+++ recipes/header/settings_menu_button/settings_menu_button.vue
@@ -0,0 +1,89 @@
+<template>
+  <dt-button
+    v-if="updateAvailable"
+    importance="outlined"
+    size="xs"
+    icon-position="right"
+    class="d-h32 d-bar-pill d-bgc-blue-200 d-bgo25 h:d-bgo50 d-bc-blue-500 d-bco25 d-fc-blue-500"
+    :aria-label="ariaLabel"
+    v-bind="$attrs"
+  >
+    <slot />
+    <template #icon>
+      <dt-icon
+        name="more-vertical"
+        size="400"
+        class="d-fc-blue-500"
+      />
+    </template>
+  </dt-button>
+  <dt-button
+    v-else
+    importance="clear"
+    kind="inverted"
+    class="settings-menu-button d-p0 d-bar24 d-h32 d-w24 d-theme-topbar-fc"
+    :aria-label="ariaLabel"
+    v-bind="$attrs"
+  >
+    <template #icon>
+      <dt-icon
+        name="more-vertical"
+        size="400"
+      />
+    </template>
+  </dt-button>
+</template>
+
+<script>
+import DtButton from '@/components/button/button';
+import { DtIcon } from '@/components/icon';
+
+export default {
+  name: 'SettingsMenuButton',
+
+  components: {
+    DtButton,
+    DtIcon,
+  },
+
+  inheritAttrs: false,
+
+  props: {
+    /**
+     * Determines whether the button should display the update state
+     * default is false
+     * @values true, false
+     */
+    updateAvailable: {
+      type: Boolean,
+      default: false,
+    },
+
+    /**
+     * Aria label
+     */
+    ariaLabel: {
+      type: String,
+      required: true,
+    },
+  },
+
+};
+</script>
+
+<style scoped lang="less">
+.d-btn:active {
+  --bgo: 85% !important;
+}
+
+.settings-menu-button {
+  &:hover {
+    background-color: hsla(var(--theme-topbar-color-hsl) / .05);
+  }
+
+  &:focus-visible {
+    color: var(--theme-topbar-color-hsl);
+    box-shadow: 0 0 0 var(--size-200) hsla(var(--theme-sidebar-color-hsl) / .15);
+  }
+}
+</style>
---
+++ recipes/header/settings_menu_button/settings_menu_button_default.story.vue
@@ -0,0 +1,20 @@
+<template>
+  <dt-recipe-settings-menu-button
+    :update-available="$attrs.updateAvailable"
+    :aria-label="$attrs.ariaLabel"
+    @click="$attrs.onClick"
+  >
+    <template v-if="defaultSlot">
+      {{ defaultSlot }}
+    </template>
+  </dt-recipe-settings-menu-button>
+</template>
+
+<script>
+import DtRecipeSettingsMenuButton from './settings_menu_button';
+
+export default {
+  name: 'DtRecipeSettingsMenuButtonDefault',
+  components: { DtRecipeSettingsMenuButton },
+};
+</script>
