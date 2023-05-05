d15347a23b5fb67c05b8f1f717ccc105d38ee1c5
Brad Paugh <brad.paugh@dialpad.com>
2023-05-05 10:38:33 -0700

    fix: add missing plugin to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import path, { resolve } from 'path';
 import { fileURLToPath } from 'url';
+import vue from '@vitejs/plugin-vue';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -23,6 +24,7 @@ export default defineConfig({
       },
     },
   },
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('.', import.meta.url)),
