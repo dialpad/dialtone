38f8a5c1e393798896c5d5fe067cab409cdada84
Trevor Jones <76007643+trevor-dialpad@users.noreply.github.com>
2023-06-15 12:13:28 -0400

    fix: updated iOS source destination (#8)
    * Updated iOS source destination
    
    * change mkdir check
    
    ---------
    
    Co-authored-by: Brad Paugh <brad.paugh@dialpad.com>

--- build-ios.js
+++ build-ios.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 
 const THEMES = require('./build').THEMES;
 
-if (!fs.existsSync('dist_ios/Sources')) {
-  fs.mkdirSync('dist_ios/Sources')
+if (!fs.existsSync('dist_ios/Sources/DialtoneTokens')) {
+  fs.mkdirSync( 'dist_ios/Sources/DialtoneTokens', { recursive: true })
 }
 
 THEMES.forEach(theme => {
-  fs.copyFile(`dist/ios/tokens-${theme}.swift`, `dist_ios/Sources/tokens-${theme}.swift`, (err) => {
+  fs.copyFile(`dist/ios/tokens-${theme}.swift`, `dist_ios/Sources/DialtoneTokens/tokens-${theme}.swift`, (err) => {
     if (err) throw err;
-    console.log(`dialtone.swift was copied to dist_ios/Sources/tokens-${theme}.swift`);
+    console.log(`dialtone.swift was copied to dist_ios/Sources/DialtoneTokens/tokens-${theme}.swift`);
   });
 });
