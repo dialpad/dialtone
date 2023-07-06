f673bc8ccc725dffec13d08b5e3e8d811f0358ed
Brad Paugh <brad.paugh@dialpad.com>
2023-07-06 13:06:56 -0700

    fix(item-layout): remove align-self from item layout

--- components/item_layout/item_layout.vue
+++ components/item_layout/item_layout.vue
@@ -98,12 +98,10 @@ export default {
   }
 
   &--right {
-    align-self: flex-start;
     padding-left: var(--space-400);
   }
 
   &--left {
-    align-self: flex-start;
     justify-content: flex-end;
     padding-right: var(--space-400);
   }
