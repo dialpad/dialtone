6aafedeaaa847ec8397279a882717c255468074a
Ignacio Ropolo <89984179+iropolo@users.noreply.github.com>
2023-07-07 17:40:58 -0300

    feat(emoji-picker): exported skin tones and categories (#1063)
    Co-authored-by: iropolo <ignacio.ropolo@dialpad.com>

--- components/emoji_picker/emoji_picker_constants.js
+++ components/emoji_picker/emoji_picker_constants.js
@@ -1,2 +1,22 @@
 export const CDN_URL = 'https://static.dialpadcdn.com/joypixels/png/unicode/32/';
 export const EMOJIS_PER_ROW = 9;
+export const EMOJI_SKIN_TONE_MODIFIERS = {
+  DEFAULT: 'Default',
+  LIGHT: 'Light',
+  MEDIUM_LIGHT: 'MediumLight',
+  MEDIUM: 'Medium',
+  MEDIUM_DARK: 'MediumDark',
+  DARK: 'Dark',
+};
+
+export const EMOJI_CATEGORIES = {
+  MOST_RECENTLY_USED: 'Most recently used',
+  SMILEYS_AND_PEOPLE: 'Smileys and people',
+  NATURE: 'Nature',
+  FOOD: 'Food',
+  ACTIVITY: 'Activity',
+  TRAVEL: 'Travel',
+  OBJECTS: 'Objects',
+  SYMBOLS: 'Symbols',
+  FLAGS: 'Flags',
+};
