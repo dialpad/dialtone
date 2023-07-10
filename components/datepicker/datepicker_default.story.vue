c0b45701cded29f0e480e21b6237d27de1271c0e
Ignacio Ropolo <89984179+iropolo@users.noreply.github.com>
2023-07-10 10:17:57 -0600

    feat(datepicker): date format utils vue3 (#1061)
    * feat(datepicker): date format utils
    
    * feat(datepicker): exported format utils
    
    * feat(datepicker): date format doc and package update
    
    ---------
    
    Co-authored-by: iropolo <ignacio.ropolo@dialpad.com>

--- components/datepicker/datepicker_default.story.vue
+++ components/datepicker/datepicker_default.story.vue
@@ -3,7 +3,44 @@
     <p>
       Selected date: {{ date }}
     </p>
-
+    <br>
+    <br>
+    <table>
+      <tr>
+        <th>Format</th>
+        <th>Result</th>
+      </tr>
+      <tr>
+        <td>formatLong</td>
+        <td>{{ formatLong(date) }}</td>
+      </tr>
+      <tr>
+        <td>formatMedium</td>
+        <td>{{ formatMedium(date) }}</td>
+      </tr>
+      <tr>
+        <td>formatShort</td>
+        <td>{{ formatShort(date) }}</td>
+      </tr>
+      <tr>
+        <td>formatShort (no weekday)</td>
+        <td>{{ formatShort(date, false) }}</td>
+      </tr>
+      <tr>
+        <td>formatNoYear</td>
+        <td>{{ formatNoYear(date) }}</td>
+      </tr>
+      <tr>
+        <td>formatNoYear (abbreviated)</td>
+        <td>{{ formatNoYear(date, true) }}</td>
+      </tr>
+      <tr>
+        <td>formatNumerical</td>
+        <td>{{ formatNumerical(date) }}</td>
+      </tr>
+    </table>
+    <br>
+    <br>
     <dt-datepicker
       :locale="$attrs.locale"
       :prev-month-label="$attrs.prevMonthLabel"
@@ -21,7 +58,29 @@
 
 <script setup>
 import DtDatepicker from './DtDatepicker.vue';
+import { formatLong, formatMedium, formatShort, formatNoYear, formatNumerical } from './formatUtils.js';
 import { ref } from 'vue';
 
 const date = ref(new Date());
 </script>
+
+<style scoped>
+table {
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+  }
+
+  th {
+    background-color: #f2f2f2;
+  }
+
+  tr:nth-child(even) {
+    background-color: #f9f9f9;
+  }
+</style>
