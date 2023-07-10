00464f4a96b4294401c9f0f1c495bc6443397991
Ignacio Ropolo <89984179+iropolo@users.noreply.github.com>
2023-07-10 13:18:12 -0300

    feat(datepicker): date format utils (#1060)
    * feat(datepicker): date format utils
    
    * feat(datepicker): test styles fix issue
    
    * feat(datepicker): exported format utils
    
    * feat(datepicker): rollback to scoped styles
    
    * feat(datepicker): date format doc and package update
    
    ---------
    
    Co-authored-by: iropolo <ignacio.ropolo@dialpad.com>

--- components/datepicker/datepicker_default.story.vue
+++ components/datepicker/datepicker_default.story.vue
@@ -3,6 +3,44 @@
     <p>
       Selected date: {{ date }}
     </p>
+    <br>
+    <br>
+    <table>
+      <tr>
+        <th>Format</th>
+        <th>Result</th>
+      </tr>
+      <tr>
+        <td>formatLong</td>
+        <td>{{ formatLong(date) }}</td>
+      </tr>
+      <tr>
+        <td>formatMedium</td>
+        <td>{{ formatMedium(date) }}</td>
+      </tr>
+      <tr>
+        <td>formatShort</td>
+        <td>{{ formatShort(date) }}</td>
+      </tr>
+      <tr>
+        <td>formatShort (no weekday)</td>
+        <td>{{ formatShort(date, false) }}</td>
+      </tr>
+      <tr>
+        <td>formatNoYear</td>
+        <td>{{ formatNoYear(date) }}</td>
+      </tr>
+      <tr>
+        <td>formatNoYear (abbreviated)</td>
+        <td>{{ formatNoYear(date, true) }}</td>
+      </tr>
+      <tr>
+        <td>formatNumerical</td>
+        <td>{{ formatNumerical(date) }}</td>
+      </tr>
+    </table>
+    <br>
+    <br>
 
     <dt-datepicker
       :locale="locale"
@@ -21,6 +59,7 @@
 
 <script>
 import DtDatepicker from './DtDatepicker.vue';
+import { formatLong, formatMedium, formatShort, formatNoYear, formatNumerical } from './formatUtils.js';
 
 export default {
   name: 'DtDatepickerDefault',
@@ -31,5 +70,34 @@ export default {
       date: new Date(),
     };
   },
+
+  methods: {
+    formatLong,
+    formatMedium,
+    formatShort,
+    formatNoYear,
+    formatNumerical,
+  },
 };
 </script>
+
+<style scoped>
+table {
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+  }
+
+  th {
+    background-color: #f2f2f2;
+  }
+
+  tr:nth-child(even) {
+    background-color: #f9f9f9;
+  }
+</style>
