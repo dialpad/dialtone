4f5e2ec8c45338e301024496c5bc7d674d5fc08d
Julio Ortega <julio.ortega@dialpad.com>
2023-08-03 09:53:27 -0600

    feat: add typescript (#1108)
    * chore: update storybook and remove unneeded dependency
    
    * feat: add typescript
    
    * fix issues
    
    * update package-lock.json

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 /** @type { import('@storybook/vue-vite').StorybookConfig } */
 const config = {
   stories: ['../components/**/*.stories.@(js|jsx|ts|tsx)', '../components/**/*.mdx', '../recipes/**/*.stories.@(js|jsx|ts|tsx)', '../recipes/**/*.mdx', '../docs/**/*.mdx', '../visual_testing/*.stories.@(js|jsx|ts|tsx)', '../visual_testing/*.mdx'],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-a11y", "@storybook/addon-styling"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-a11y"],
   framework: {
     name: "@storybook/vue-vite",
     options: {}
