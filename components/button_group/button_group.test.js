e13527b8ef477cb2e11502256b0ac8a96638a8f7
Ignacio Ropolo <89984179+iropolo@users.noreply.github.com>
2023-08-26 19:58:41 -0300

    test(button-group, card, checkbox-group): new test template (#1141)
    Co-authored-by: iropolo <ignacio.ropolo@dialpad.com>

--- components/button_group/button_group.test.js
+++ components/button_group/button_group.test.js
@@ -1,99 +1,67 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils';
+import { createLocalVue, mount } from '@vue/test-utils';
 import DtButtonGroup from './button_group.vue';
 import { DtButton } from '../button';
 import ButtonsFixture from './buttons_decorator.vue';
-import {
-  itBehavesLikeFailsCustomPropValidation,
-  itBehavesLikePassesCustomPropValidation,
-} from '../../tests/shared_examples/validation';
 
-// Constants
-const basePropsData = {};
+const MOCK_DEFAULT_PROP = DtButtonGroup.props.alignment;
+
+const baseSlots = {};
+
+let mockSlots = {};
+const testContext = {};
 
 describe('DtButtonGroup Tests', () => {
-  let testContext;
-
-  beforeAll(() => {
-    testContext = {};
-  });
-
-  // Wrappers
   let wrapper;
   let buttonGroup;
 
-  // Environment
-  let propsData = basePropsData;
-  let attrs = {};
-  let slots = {};
-  let provide = {};
+  const updateWrapper = () => {
+    wrapper = mount(DtButtonGroup, {
+      slots: { ...baseSlots, ...mockSlots },
+      localVue: testContext.localVue,
+    });
 
-  // Helpers
-  const _setChildWrappers = () => {
     buttonGroup = wrapper.find('.d-btn-group');
   };
 
-  const _setWrappers = () => {
-    wrapper = shallowMount(DtButtonGroup, {
-      propsData,
-      attrs,
-      slots,
-      provide,
-      localVue: testContext.localVue,
-    });
-    _setChildWrappers();
-  };
-
-  // Setup
   beforeAll(() => {
     testContext.localVue = createLocalVue();
   });
-  beforeEach(() => {});
 
-  // Teardown
-  afterEach(() => {
-    propsData = basePropsData;
-    attrs = {};
-    slots = {};
-    provide = {};
+  beforeEach(() => {
+    updateWrapper();
   });
-  afterAll(() => {});
+
+  afterEach(() => {
+    mockSlots = {};
+  });
 
   describe('Presentation Tests', () => {
     describe('When rendered with default content', () => {
-      // Test Setup
-      beforeEach(() => { _setWrappers(); });
+      it('should have a button group', () => {
+        expect(buttonGroup.exists()).toBe(true);
+      });
 
-      it(
-        'should have a button group',
-        () => { expect(buttonGroup.exists()).toBe(true); },
-      );
       it('should not have buttons', () => {
         expect(wrapper.findAllComponents(DtButton).length).toBe(0);
       });
     });
 
     describe('When buttons are provided', () => {
-      // Test Setup
-      beforeEach(() => {
-        slots = { default: ButtonsFixture };
-      });
-
       describe('When the button group renders', () => {
-        // Test Setup
-        beforeEach(() => { _setWrappers(); });
-
         it('should have buttons', () => {
+          mockSlots = { default: ButtonsFixture };
+
+          updateWrapper();
+
           expect(wrapper.findAllComponents(DtButton).length).toBe(2);
         });
       });
     });
 
     describe('When alignment is set to end', () => {
-      beforeEach(async () => {
-        await wrapper.setProps({ alignment: 'end' });
-      });
-
       it('should have correct class', async () => {
+        await wrapper.setProps({ alignment: 'end' });
+
         expect(buttonGroup.classes().includes('d-btn-group--end')).toBe(true);
       });
     });
@@ -109,15 +77,16 @@ describe('DtButtonGroup Tests', () => {
 
   describe('Validation Tests', () => {
     describe('Alignment Validator', () => {
-      // Test Environment
-      const prop = DtButtonGroup.props.alignment;
-
       describe('When provided alignment is in BUTTON_GROUP_ALIGNMENT', () => {
-        itBehavesLikePassesCustomPropValidation(prop, prop.default);
+        it('passes custom prop validation', () => {
+          expect(MOCK_DEFAULT_PROP.validator(MOCK_DEFAULT_PROP.default)).toBe(true);
+        });
       });
 
       describe('When provided alignment is not in BUTTON_GROUP_ALIGNMENT', () => {
-        itBehavesLikeFailsCustomPropValidation(prop, `INVALID_ALIGNMENT`);
+        it('fails custom prop validation', () => {
+          expect(MOCK_DEFAULT_PROP.validator(`INVALID_ALIGNMENT`)).toBe(false);
+        });
       });
     });
   });
--- components/checkbox_group/checkbox_group.test.js
+++ components/checkbox_group/checkbox_group.test.js
@@ -1,433 +1,345 @@
 import Vue from 'vue';
 import { flushPromises } from '@/common/utils';
-import {
-  createLocalVue,
-  shallowMount,
-  mount,
-} from '@vue/test-utils';
-import {
-  itBehavesLikeEmitsExpectedEvent,
-  itBehavesLikeDoesNotEmitEvents,
-} from '../../tests/shared_examples/events';
-import {
-  itBehavesLikePassesCustomPropValidation,
-  itBehavesLikeFailsCustomPropValidation,
-} from '../../tests/shared_examples/validation';
-import {
-  itBehavesLikeAppliesClassToChild,
-  itBehavesLikeAppliesChildProp,
-} from '../../tests/shared_examples/extendability';
+import { createLocalVue, mount } from '@vue/test-utils';
 import { DtValidationMessages } from '../validation_messages';
 import { DtCheckbox } from '../checkbox';
 import CheckboxesFixture from './checkboxes_decorator.vue';
 import DtCheckboxGroup from './checkbox_group.vue';
 
-// Constants
-const basePropsData = {
-  name: 'test-checkbox-group',
-};
+const MOCK_LEGEND = 'My Legend';
+const MOCK_SELECTED_VALUE = 'apple';
+const MOCK_SELECTED_VALUE2 = 'other';
+
+const baseProps = { name: 'test-checkbox-group' };
+const baseSlots = {};
 const baseAttrs = { 'aria-label': 'Test Checkbox Group' };
+const baseProvide = {};
 
+let mockProps = {};
+let mockSlots = {};
+let mockAttrs = {};
+let mockProvide = {};
+const testContext = {};
 describe('Checkbox Group Tests', () => {
-  let testContext;
-
-  beforeAll(() => {
-    testContext = {};
-  });
-
-  // Wrappers
   let wrapper;
   let checkboxGroup;
   let checkboxGroupLegend;
   let checkboxGroupMessages;
 
-  // Environment
-  let propsData = basePropsData;
-  let attrs = baseAttrs;
-  let slots = {};
-  let provide = {};
+  const updateWrapper = () => {
+    wrapper = mount(DtCheckboxGroup, {
+      propsData: { ...baseProps, ...mockProps },
+      slots: { ...baseSlots, ...mockSlots },
+      attrs: { ...baseAttrs, ...mockAttrs },
+      provide: { ...baseProvide, ...mockProvide },
+      localVue: testContext.localVue,
+    });
 
-  // Helpers
-  const _setChildWrappers = () => {
     checkboxGroup = wrapper.find('[data-qa="checkbox-group"]');
     checkboxGroupLegend = wrapper.find('[data-qa="checkbox-group-legend"]');
     checkboxGroupMessages = wrapper.find('[data-qa="checkbox-group-messages"]');
   };
 
-  const _setWrappers = () => {
-    wrapper = shallowMount(DtCheckboxGroup, {
-      propsData,
-      attrs,
-      slots,
-      provide,
-      localVue: testContext.localVue,
-    });
-    _setChildWrappers();
-  };
-
-  const _mountWrappers = () => {
-    wrapper = mount(DtCheckboxGroup, {
-      propsData,
-      slots,
-      attrs,
-      provide,
-      localVue: testContext.localVue,
-    });
-    _setChildWrappers();
-  };
-
-  // Setup
   beforeAll(() => {
     testContext.localVue = createLocalVue();
   });
-  beforeEach(() => {});
 
-  // Teardown
-  afterEach(() => {
-    propsData = basePropsData;
-    attrs = baseAttrs;
-    slots = {};
-    provide = {};
+  beforeEach(() => {
+    updateWrapper();
   });
-  afterAll(() => {});
+
+  afterEach(() => {
+    mockProps = {};
+    mockSlots = {};
+    mockAttrs = {};
+    mockProvide = {};
+  });
 
   describe('Presentation Tests', () => {
-    // Test Environment
-    const legend = 'My Legend';
-
-    // Shared Examples
-    const itBehavesLikeHasLegend = () => {
-      it(
-        'should have a legend',
-        () => { expect(checkboxGroupLegend.exists()).toBe(true); },
-      );
-      it('should have text matching the provided legend', () => {
-        expect(checkboxGroupLegend.text()).toBe(legend);
-      });
-    };
-
     describe('When rendered with default content', () => {
-      // Test Setup
-      beforeEach(() => { _setWrappers(); });
+      it('should have a checkbox group', () => {
+        expect(checkboxGroup.exists()).toBe(true);
+      });
 
-      it(
-        'should have a checkbox group',
-        () => { expect(checkboxGroup.exists()).toBe(true); },
-      );
-      it(
-        'should not have a legend',
-        () => { expect(checkboxGroupLegend.exists()).toBe(false); },
-      );
+      it('should not have a legend', () => {
+        expect(checkboxGroupLegend.exists()).toBe(false);
+      });
+
       it('should not have checkboxes', () => {
         expect(wrapper.findAllComponents(DtCheckbox).length).toBe(0);
       });
+
       it('should not have validation messages', () => {
-        expect(
-          wrapper.findComponent(DtValidationMessages)?.props('validationMessages').length,
-        ).toBe(0);
+        expect(wrapper.findComponent(DtValidationMessages)?.props('validationMessages').length).toBe(0);
       });
     });
 
     describe('When checkboxes are provided', () => {
-      // Test Setup
-      beforeEach(() => {
-        slots = { default: CheckboxesFixture };
-      });
-
       describe('When the checkbox group renders', () => {
-        // Test Setup
-        beforeEach(() => { _setWrappers(); });
-
         it('should have checkboxes', () => {
+          mockSlots = { default: CheckboxesFixture };
+
+          updateWrapper();
+
           expect(wrapper.findAllComponents(DtCheckbox).length).toBe(3);
         });
       });
     });
 
     describe('When a legend is provided via prop', () => {
-      // Test Setup
       beforeEach(() => {
-        propsData = { ...basePropsData, legend };
-        _setWrappers();
+        mockProps = { legend: MOCK_LEGEND };
+
+        updateWrapper();
       });
 
-      itBehavesLikeHasLegend();
+      it('should have a legend', () => {
+        expect(checkboxGroupLegend.exists()).toBe(true);
+      });
+
+      it('should have text matching the provided legend', () => {
+        expect(checkboxGroupLegend.text()).toBe(MOCK_LEGEND);
+      });
     });
 
     describe('When a legend is provided via slot', () => {
-      // Test Setup
       beforeEach(() => {
-        slots = { legend };
+        mockSlots = { legend: MOCK_LEGEND };
+
+        updateWrapper();
       });
 
       describe('When a legend is not provided via prop', () => {
-        // Test Setup
-        beforeEach(() => { _setWrappers(); });
+        it('should have a legend', () => {
+          expect(checkboxGroupLegend.exists()).toBe(true);
+        });
 
-        itBehavesLikeHasLegend();
+        it('should have text matching the provided legend', () => {
+          expect(checkboxGroupLegend.text()).toBe(MOCK_LEGEND);
+        });
       });
 
       describe('When a legend is also provided via prop', () => {
-        // Test Setup
         beforeEach(() => {
-          propsData = {
-            ...basePropsData,
-            legend: 'A legend which should not be displayed',
-          };
-          _setWrappers();
+          mockProps = { legend: 'A legend which should not be displayed' };
+
+          updateWrapper();
         });
 
-        itBehavesLikeHasLegend();
+        it('should have a legend', () => {
+          expect(checkboxGroupLegend.exists()).toBe(true);
+        });
+
+        it('should have text matching the provided legend', () => {
+          expect(checkboxGroupLegend.text()).toBe(MOCK_LEGEND);
+        });
       });
     });
 
     describe('When validation messages are provided', () => {
-      // Shared Examples
-      const itBehavesLikeHasValidationMessages = (numMessages) => {
-        it('should have validation messages', () => {
-          expect(checkboxGroupMessages?.props('validationMessages').length).toBe(numMessages);
-        });
-      };
-
-      // Test Setup
       beforeEach(() => {
-        propsData = { ...basePropsData, messages: ['Error'] };
+        mockProps = { messages: ['Error'] };
+
+        updateWrapper();
       });
 
       describe('When the validation messages are shown', () => {
-        // Test Setup
-        beforeEach(() => { _setWrappers(); });
+        it('should have validation messages', () => {
+          expect(checkboxGroupMessages?.props('validationMessages').length).toBe(1);
+        });
 
-        itBehavesLikeHasValidationMessages(1);
         it('should show validation messages', () => {
           expect(checkboxGroupMessages?.props('showMessages')).toBe(true);
         });
       });
 
       describe('When the validation messages are hidden', () => {
-        // Test Setup
-        beforeEach(() => {
-          propsData = { ...propsData, showMessages: false };
-          _setWrappers();
-        });
-
-        itBehavesLikeHasValidationMessages(1);
         it('should hide validation messages', () => {
-          expect(checkboxGroupMessages?.props('showMessages')).toBe(false);
+          mockProps = { ...mockProps, showMessages: false };
+
+          updateWrapper();
+
+          expect(checkboxGroupMessages.exists()).toBe(false);
         });
       });
     });
   });
 
   describe('Interactivity Tests', () => {
-    // Test Environment
-    const selectedValue = 'apple';
-
-    // Helpers
-    const _selectCheckbox = async (value) => {
+    const MOCK_SELECTED_CHECKBOX_FUNCTION = async (value) => {
       const selectedCheckbox = checkboxGroup.find(`[value="${value}"]`);
       selectedCheckbox.element.checked = true;
       await selectedCheckbox.trigger('change');
       await flushPromises();
     };
 
-    // Shared Examples
-    const itBehavesLikeUpdatesProvideObj = (selectedValues) => {
-      it('should update provide object', () => {
-        selectedValues.forEach(value => {
-          expect(wrapper.vm.provideObj?.selectedValues?.includes(value)).toBe(true);
-        });
-      });
-    };
+    beforeEach(async () => {
+      mockSlots = { default: CheckboxesFixture };
 
-    // Test Setup
-    beforeEach(() => {
-      slots = { default: CheckboxesFixture };
+      updateWrapper();
     });
 
     describe('When initial selected values are provided', () => {
-      // Test Setup
       beforeEach(() => {
-        propsData = { ...basePropsData, selectedValues: [selectedValue] };
-        _mountWrappers();
+        mockProps = { selectedValues: [MOCK_SELECTED_VALUE] };
+
+        updateWrapper();
       });
 
       describe('When another checkbox is not selected', () => {
-        itBehavesLikeUpdatesProvideObj([selectedValue]);
+        it('should update provide object', () => {
+          [MOCK_SELECTED_VALUE].forEach(value => {
+            expect(wrapper.vm.provideObj?.selectedValues?.includes(value)).toBe(true);
+          });
+        });
       });
 
       describe('When another checkbox is selected', () => {
-        // Test Environment
-        const selectedValue2 = 'other';
-        const selectedValues = [selectedValue, selectedValue2];
-
-        // Test Setup
         beforeEach(async () => {
-          await _selectCheckbox(selectedValue2);
+          await MOCK_SELECTED_CHECKBOX_FUNCTION(MOCK_SELECTED_VALUE2);
         });
 
-        itBehavesLikeUpdatesProvideObj(selectedValues);
+        it('should update provide object', () => {
+          [MOCK_SELECTED_VALUE, MOCK_SELECTED_VALUE2].forEach(value => {
+            expect(wrapper.vm.provideObj?.selectedValues?.includes(value)).toBe(true);
+          });
+        });
+
         it('Should emit an input event', () => {
-          itBehavesLikeEmitsExpectedEvent(wrapper, 'input', selectedValues);
+          expect(wrapper.emitted('input')[0][0]).toEqual([MOCK_SELECTED_VALUE, MOCK_SELECTED_VALUE2]);
         });
       });
     });
 
     describe('When a checkbox is selected', () => {
-      // Test Environment
-      const selectedValues = [selectedValue];
-
-      // Test Setup
-      beforeEach(() => {
-        _mountWrappers();
-        _selectCheckbox(selectedValue);
+      beforeEach(async () => {
+        await MOCK_SELECTED_CHECKBOX_FUNCTION(MOCK_SELECTED_VALUE);
       });
 
-      itBehavesLikeUpdatesProvideObj(selectedValues);
+      it('should update provide object', () => {
+        [MOCK_SELECTED_VALUE].forEach(value => {
+          expect(wrapper.vm.provideObj?.selectedValues?.includes(value)).toBe(true);
+        });
+      });
+
       it('should emit an input event', () => {
-        itBehavesLikeEmitsExpectedEvent(wrapper, 'input', selectedValues);
+        expect(wrapper.emitted('input')[0][0]).toEqual([MOCK_SELECTED_VALUE]);
       });
     });
 
     describe('When the checkbox group is disabled', () => {
-      // Test Setup
-      beforeEach(() => {
-        propsData = { ...basePropsData, disabled: true };
-        _mountWrappers();
-      });
-
       describe('When a checkbox is selected', () => {
-        // Test Setup
-        beforeEach(() => { _selectCheckbox(selectedValue); });
+        it('does not emit an input event', () => {
+          mockProps = { disabled: true };
 
-        it(
-          'does not emit an input event',
-          () => { itBehavesLikeDoesNotEmitEvents(wrapper); },
-        );
+          updateWrapper();
+
+          MOCK_SELECTED_CHECKBOX_FUNCTION(MOCK_SELECTED_VALUE);
+
+          expect(wrapper.emitted()).toEqual({});
+        });
       });
     });
   });
 
   describe('Validation Tests', () => {
-    // Test Environment
     let previousVueSilentConfig;
 
-    // Test Setup
     beforeAll(() => {
       previousVueSilentConfig = Vue.config.silent;
       Vue.config.silent = true;
     });
 
-    // Test Teardown
     afterAll(() => {
       Vue.config.silent = previousVueSilentConfig;
     });
 
     describe('value', () => {
-      // Test Environment
-      const prop = DtCheckboxGroup.props.value;
+      const MOCK_PROP = DtCheckboxGroup.props.value;
 
       describe('When a value is not provided', () => {
-        itBehavesLikePassesCustomPropValidation(prop);
+        it('passes custom prop validation', () => {
+          expect(MOCK_PROP.validator(undefined)).toBe(true);
+        });
       });
 
       describe('When a value is provided', () => {
-        itBehavesLikeFailsCustomPropValidation(prop, 'some value');
+        it('fails custom prop validation', () => {
+          expect(MOCK_PROP.validator('some value')).toBe(false);
+        });
       });
     });
   });
 
   describe('Extendability Tests', () => {
-    let element;
-    const customClass = 'my-custom-class';
-    const propName = 'some';
-    const propValue = 'prop';
-    const childProps = {};
-
-    // Helpers
-    const _setupChildClassTest = (childClassName, selector) => {
-      propsData[childClassName] = customClass;
-      _setWrappers();
-      element = wrapper.find(selector);
-    };
-
-    const _setupChildPropsTest = (childPropsName, selector) => {
-      propsData[childPropsName] = childProps;
-      _setWrappers();
-      element = wrapper.find(selector);
-    };
-
-    // Shared Examples
-    const itBehavesLikeAppliesClassToChildLocal = () => {
-      it('should apply custom class to child', () => {
-        itBehavesLikeAppliesClassToChild(wrapper, '.my-custom-class', element);
-      });
-    };
-
-    const itBehavesLikeAppliesChildPropLocal = () => {
-      it('should have provided child prop', () => {
-        itBehavesLikeAppliesChildProp(element, propName, propValue);
-      });
-    };
-
-    // Test Setup
-    beforeAll(() => {
-      childProps[propName] = propValue;
-    });
+    let MOCK_ELEMENT;
+    const MOCK_PROP_NAME = 'some';
+    const MOCK_PROP_VALUE = 'prop';
 
     beforeEach(() => {
-      propsData = {
-        ...basePropsData,
-        legend: 'My Legend',
-        messages: ['Error'],
-      };
+      mockProps = { legend: 'My Legend', messages: ['Error'] };
+
+      updateWrapper();
     });
 
     describe('When a legend class is provided', () => {
-      // Test Setup
-      beforeEach(
-        () => { _setupChildClassTest('legendClass', '[data-qa="checkbox-group-legend"]'); },
-      );
+      it('should apply custom class to child', () => {
+        mockProps = { ...mockProps, legendClass: 'my-custom-class' };
 
-      itBehavesLikeAppliesClassToChildLocal();
+        updateWrapper();
+
+        MOCK_ELEMENT = wrapper.find('[data-qa="checkbox-group-legend"]');
+
+        expect(wrapper.find('.my-custom-class').html()).toBe(MOCK_ELEMENT.html());
+      });
     });
 
     describe('When a messages class is provided', () => {
-      // Test Setup
-      beforeEach(
-        () => { _setupChildClassTest('messagesClass', '[data-qa="checkbox-group-messages"]'); },
-      );
+      it('should apply custom class to child', () => {
+        mockProps = { ...mockProps, messagesClass: 'my-custom-class' };
 
-      itBehavesLikeAppliesClassToChildLocal();
+        updateWrapper();
+
+        MOCK_ELEMENT = wrapper.find('[data-qa="checkbox-group-messages"]');
+
+        expect(wrapper.find('.my-custom-class').html()).toBe(MOCK_ELEMENT.html());
+      });
     });
 
     describe('When legend child props are provided', () => {
-      // Test Setup
-      beforeEach(
-        () => { _setupChildPropsTest('legendChildProps', '[data-qa="checkbox-group-legend"]'); },
-      );
+      it('should have provided child prop', () => {
+        mockProps = { ...mockProps, legendChildProps: { [MOCK_PROP_NAME]: MOCK_PROP_VALUE } };
 
-      itBehavesLikeAppliesChildPropLocal();
+        updateWrapper();
+
+        MOCK_ELEMENT = wrapper.find('[data-qa="checkbox-group-legend"]');
+
+        expect(MOCK_ELEMENT.attributes(MOCK_PROP_NAME)).toBe(MOCK_PROP_VALUE);
+      });
     });
 
     describe('When messages child props are provided', () => {
-      // Test Setup
-      beforeEach(
-        () => { _setupChildPropsTest('messagesChildProps', '[data-qa="checkbox-group-messages"]'); },
-      );
+      it('should have provided child prop', () => {
+        mockProps = { ...mockProps, messagesChildProps: { [MOCK_PROP_NAME]: MOCK_PROP_VALUE } };
 
-      itBehavesLikeAppliesChildPropLocal();
+        updateWrapper();
+
+        MOCK_ELEMENT = wrapper.find('[data-qa="checkbox-group-messages"]');
+
+        expect(MOCK_ELEMENT.attributes(MOCK_PROP_NAME)).toBe(MOCK_PROP_VALUE);
+      });
     });
 
     describe('When attrs are provided', () => {
-      // Test Setup
-      beforeEach(() => {
-        attrs = { ...baseAttrs, some: 'prop' };
-        _setWrappers();
-        element = checkboxGroup;
-      });
+      it('should have provided child prop', () => {
+        mockAttrs = { some: 'prop' };
 
-      itBehavesLikeAppliesChildPropLocal();
+        updateWrapper();
+
+        MOCK_ELEMENT = checkboxGroup;
+
+        expect(MOCK_ELEMENT.attributes(MOCK_PROP_NAME)).toBe(MOCK_PROP_VALUE);
+      });
     });
   });
 });
