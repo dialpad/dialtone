12068253679ddbacb0cae7ebdb1616f171f3da72
Julio Ortega <julio.ortega@dialpad.com>
2023-08-28 10:14:02 -0600

    refactor(callbox): visuals (#1139)
    * refactor(callbox): visuals
    
    * simplify default story
    
    * remove nonexistent variants story
    
    * add avatarSeed

--- recipes/leftbar/callbox/callbox.vue
+++ recipes/leftbar/callbox/callbox.vue
@@ -1,6 +1,9 @@
 <template>
   <dt-item-layout class="dt-recipe-callbox">
-    <template #default>
+    <template
+      v-if="$slots.video"
+      #default
+    >
       <div class="dt-recipe-callbox--video">
         <!-- @slot Slot for video stream -->
         <slot name="video" />
@@ -10,38 +13,52 @@
       <dt-item-layout :class="['dt-recipe-callbox--main-content', borderClass]">
         <template #default>
           <dt-item-layout class="dt-recipe-callbox--top-content">
-            <template #left>
+            <template
+              v-if="shouldShowAvatar"
+              #left
+            >
               <dt-avatar
-                v-if="shouldShowAvatar"
                 :image-src="avatarSrc"
                 :full-name="avatarFullName"
+                :seed="avatarSeed"
                 size="sm"
               />
             </template>
             <template #default>
-              <div class="dt-recipe-callbox--title">
-                {{ title }}
-              </div>
+              <span
+                class="dt-recipe-callbox--title"
+                v-text="title"
+              />
             </template>
             <template #subtitle>
               <dt-item-layout class="dt-recipe-callbox--subtitle">
                 <template #default>
-                  <!-- @slot Slot for call center badge -->
-                  <slot name="badge">
-                    <dt-badge
-                      v-if="badgeText"
-                      :class="badgeClass"
-                      :text="badgeText"
-                    />
-                  </slot>
+                  <div class="dt-recipe-callbox--subtitle-badge">
+                    <!-- @slot Slot for call center badge -->
+                    <slot name="badge">
+                      <dt-badge
+                        v-if="badgeText"
+                        :class="badgeClass"
+                        :text="badgeText"
+                      />
+                    </slot>
+                  </div>
                 </template>
-                <template #bottom>
-                  <!-- @slot Slot for subtitle -->
-                  <slot name="subtitle" />
+                <template
+                  v-if="$slots.subtitle"
+                  #bottom
+                >
+                  <div class="dt-recipe-callbox--subtitle-content">
+                    <!-- @slot Slot for subtitle -->
+                    <slot name="subtitle" />
+                  </div>
                 </template>
               </dt-item-layout>
             </template>
-            <template #right>
+            <template
+              v-if="$slots.right"
+              #right
+            >
               <div class="dt-recipe-callbox--right">
                 <!-- @slot Slot for right icons -->
                 <slot name="right" />
@@ -49,7 +66,10 @@
             </template>
           </dt-item-layout>
         </template>
-        <template #bottom>
+        <template
+          v-if="$slots.bottom"
+          #bottom
+        >
           <div class="dt-recipe-callbox--bottom-content">
             <slot name="bottom" />
           </div>
@@ -152,6 +172,8 @@ export default {
 
 <style lang="less" scoped>
 .dt-recipe-callbox {
+  padding: 0;
+  color: var(--dt-color-foreground-primary);
   background-color: var(--dt-color-surface-primary);
   border-radius: var(--dt-size-radius-300);
 
@@ -164,19 +186,34 @@ export default {
     overflow: hidden;
   }
 
+  &:deep(.dt-item-layout--bottom) {
+    margin-top: 0;
+  }
+
   &:deep(.dt-item-layout--right) {
     flex-shrink: 0;
   }
 
-  &--video {}
+  &--video {
+    border-radius: var(--dt-size-radius-200) var(--dt-size-radius-200) 0 0;
+    overflow: hidden;
+    height: calc(var(--dt-size-760) + var(--dt-size-650));
+    margin-bottom: var(--dt-size-300-negative);
+
+    :deep(img) {
+      object-fit: cover;
+      width: 100%;
+      height: 100%;
+    }
+  }
 
   &--main-content {
     padding: 0;
     border-radius: var(--dt-size-radius-300);
-    border: var(--dt-size-border-200) solid transparent;
+    border: var(--dt-size-border-100) solid transparent;
 
     &.dt-recipe-callbox--border-default {
-      border: var(--dt-size-border-200) solid var(--dt-color-border-default);
+      border-color: var(--dt-color-border-default);
     }
 
     &.dt-recipe-callbox--border-ai {
@@ -200,14 +237,27 @@ export default {
     text-overflow: ellipsis;
     white-space: nowrap;
     overflow: hidden;
-    font-weight: var(--dt-font-weight-bold) ;
+    color: var(--dt-color-foreground-primary);
+    background-color: var(--dt-color-surface-primary);
+    font-weight: var(--dt-font-weight-bold);
   }
 
   &--subtitle {
     padding: 0;
     min-height: auto;
     font-size: var(--dt-font-size-100);
-    color: var(--dt-color-foreground-tertiary);
+
+    &-badge {
+      padding-top: var(--dt-space-300);
+    }
+
+    &-content :last-child {
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        color: var(--dt-color-foreground-tertiary);
+        background-color: var(--dt-color-surface-primary);
+    }
   }
 
   .dt-recipe-callbox-badge--warning {
--- recipes/leftbar/callbox/callbox_default.story.vue
+++ recipes/leftbar/callbox/callbox_default.story.vue
@@ -4,113 +4,56 @@
     :badge-color="badgeColor"
     :avatar-src="avatarSrc"
     :avatar-full-name="avatarFullName"
+    :avatar-seed="avatarSeed"
     :title="title"
     :border-color="borderColor"
   >
-    <template #video>
+    <template
+      v-if="video"
+      #video
+    >
       <div
-        v-if="video"
         v-html="video"
       />
     </template>
-    <template #badge>
+    <template
+      v-if="badge"
+      #badge
+    >
       <span
-        v-if="badge"
         v-html="badge"
       />
     </template>
-    <template #subtitle>
+    <template
+      v-if="subtitle"
+      #subtitle
+    >
       <span
-        v-if="subtitle"
         v-html="subtitle"
       />
-      <div
-        v-else
-        class="d-d-flex d-ai-center d-flow4"
-      >
-        <dt-icon
-          name="share-screen"
-          size="200"
-        />
-        <span>06:01</span>
-      </div>
     </template>
-    <template #right>
+    <template
+      v-if="right"
+      #right
+    >
       <div
-        v-if="right"
         v-html="right"
       />
-      <div v-else>
-        <dt-button
-          circle
-          importance="clear"
-        >
-          <template #icon>
-            <dt-icon
-              name="dialpad-ai-color"
-              size="400"
-            />
-          </template>
-        </dt-button>
-        <dt-button
-          circle
-          importance="clear"
-        >
-          <template #icon>
-            <dt-icon
-              name="phone-hang-up"
-              class="d-fc-critical"
-              size="400"
-            />
-          </template>
-        </dt-button>
-      </div>
     </template>
-    <template #bottom>
-      <dt-item-layout>
-        <template #left>
-          <dt-button
-            circle
-            importance="clear"
-          >
-            <template #icon>
-              <dt-icon
-                name="share-screen"
-                size="400"
-              />
-            </template>
-          </dt-button>
-        </template>
-        <template #default>
-          <span class="d-fs-100">Sharing screen</span>
-        </template>
-        <template #right>
-          <dt-button
-            importance="clear"
-            class="d-fc-critical"
-          >
-            <template #icon>
-              <dt-icon
-                name="stop-filled"
-                size="200"
-              />
-            </template>
-            Stop
-          </dt-button>
-        </template>
-      </dt-item-layout>
+    <template
+      v-if="bottom"
+      #bottom
+    >
+      <div v-html="bottom" />
     </template>
   </dt-recipe-callbox>
 </template>
 
 <script>
 import DtRecipeCallbox from './callbox.vue';
-import DtIcon from '@/components/icon/icon.vue';
-import DtButton from '@/components/button/button.vue';
-import DtItemLayout from '@/components/item_layout/item_layout.vue';
 
 export default {
   name: 'DtRecipeCallboxDefault',
-  components: { DtItemLayout, DtButton, DtIcon, DtRecipeCallbox },
+  components: { DtRecipeCallbox },
 };
 </script>
