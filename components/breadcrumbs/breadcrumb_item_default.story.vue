3ce325d2357962cf185d75119257070eaf5c144a
Brad Paugh <brad.paugh@dialpad.com>
2023-10-05 16:29:37 -0700

    fix(breadcrumbs): fix story configuration of BreadcrumbItem (#1246)
    * fix: href on link
    
    * fix(breadcrumbs): fix story configuration of BreadcrumbItem
    
    * fix whitespace
    
    * correct url -> href
    
    * Revert "fix: href on link"
    
    This reverts commit 5db2f406db0ffb7160151c320f8b096607ce4390.
    
    * fix markdown lint
    
    ---------
    
    Co-authored-by: Sanjeev Suresh <ssuresh@dialpad.com>

---
+++ components/breadcrumbs/breadcrumb_item_default.story.vue
@@ -0,0 +1,17 @@
+<template>
+  <dt-breadcrumb-item
+    :inverted="inverted"
+    :selected="selected"
+    :label="label"
+    :href="href"
+  />
+</template>
+
+<script>
+import DtBreadcrumbItem from './breadcrumb_item.vue';
+
+export default {
+  name: 'DtBreadcrumbItemDefault',
+  components: { DtBreadcrumbItem },
+};
+</script>
