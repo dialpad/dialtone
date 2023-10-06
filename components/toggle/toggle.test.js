13eeda42d8c595e135b8ca1a0ce0123e18a1a0cf
Ignacio Ropolo <89984179+iropolo@users.noreply.github.com>
2023-10-06 15:41:56 -0300

    test(toggle, tooltip, validation-messages): new test template (#1249)
    * test(toggle, tooltip, validation-messages): new test template
    
    ---------
    
    Co-authored-by: iropolo <ignacio.ropolo@dialpad.com>

--- components/toggle/toggle.test.js
+++ components/toggle/toggle.test.js
@@ -1,88 +1,65 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils';
+import { createLocalVue, mount } from '@vue/test-utils';
 import DtToggle from './toggle.vue';
-import Vue from 'vue';
-import {
-  itBehavesLikeDoesNotRaiseAnyVueWarnings,
-  itBehavesLikeRaisesSingleVueWarning,
-} from '../../tests/shared_examples/validation';
 
-// Constants
-const basePropsData = {};
-const baseSlotData = { default: 'My Toggle Label' };
+const baseProps = {};
+const baseSlot = { default: 'My Toggle Label' };
+const baseAttrs = {};
+
+let mockProps = {};
+let mockSlots = {};
+let mockAttrs = {};
+const testContext = {};
 
 describe('DtToggle Tests', () => {
-  let testContext;
-
-  beforeAll(() => {
-    testContext = {};
-  });
-
-  // Wrappers
   let wrapper;
   let button;
   let label;
   let icon;
 
-  // Environment
-  let propsData = basePropsData;
-  let attrs = {};
-  let slots = baseSlotData;
-  let provide = {};
-  let listeners = {};
+  const updateWrapper = () => {
+    wrapper = mount(DtToggle, {
+      propsData: { ...baseProps, ...mockProps },
+      attrs: { ...baseAttrs, ...mockAttrs },
+      slots: { ...baseSlot, ...mockSlots },
+      localVue: testContext.localVue,
+    });
 
-  // Helpers
-  const _setChildWrappers = () => {
     button = wrapper.find('button');
     label = wrapper.find('[data-qa="toggle-label"]');
     icon = wrapper.find('.d-toggle__inner');
   };
 
-  const _setWrappers = () => {
-    wrapper = shallowMount(DtToggle, {
-      propsData,
-      attrs,
-      slots,
-      provide,
-      listeners,
-      localVue: testContext.localVue,
-    });
-    _setChildWrappers();
-  };
-
-  // Setup
   beforeAll(() => {
     testContext.localVue = createLocalVue();
   });
-  beforeEach(() => {});
 
-  // Teardown
-  afterEach(() => {
-    propsData = basePropsData;
-    attrs = {};
-    slots = baseSlotData;
-    provide = {};
-    listeners = {};
+  beforeEach(() => {
+    updateWrapper();
   });
-  afterAll(() => {});
+
+  afterEach(() => {
+    mockProps = {};
+    mockAttrs = {};
+    mockSlots = {};
+  });
 
   describe('Presentation Tests', () => {
     describe('Common toggle button attrs', () => {
-      // Test Setup
-      beforeEach(() => {
-        _setWrappers();
+      it('should exist', () => {
+        expect(wrapper.exists()).toBe(true);
       });
-      it('should exist', () => { expect(wrapper.exists()).toBe(true); });
+
       it('should have d-toggle class', () => {
         expect(button.classes().includes('d-toggle')).toBe(true);
       });
-      it(
-        'should have type button',
-        () => { expect(button.attributes('type')).toBe('button'); },
-      );
-      it(
-        'should have role switch',
-        () => { expect(button.attributes('role')).toBe('switch'); },
-      );
+
+      it('should have type button', () => {
+        expect(button.attributes('type')).toBe('button');
+      });
+
+      it('should have role switch', () => {
+        expect(button.attributes('role')).toBe('switch');
+      });
 
       it('should show the icon', () => {
         expect(icon.exists()).toBe(true);
@@ -90,108 +67,101 @@ describe('DtToggle Tests', () => {
 
       it('should hide the icon when showIcon prop is false', async () => {
         await wrapper.setProps({ showIcon: false });
-        await wrapper.vm.$nextTick();
-        _setChildWrappers();
+
+        icon = wrapper.find('.d-toggle__inner');
+
         expect(icon.exists()).toBe(false);
       });
 
       describe('disabled behaviour', () => {
-        it(
-          'should set correct disabled attributes when disabled prop is false',
-          () => {
-            expect(button.attributes('aria-disabled')).toBe('false');
-            expect(button.attributes().disabled).toBeUndefined();
-            expect(button.classes().includes('d-toggle--disabled')).toBe(false);
-          },
-        );
+        it('should set correct disabled attributes when disabled prop is false', () => {
+          expect(button.attributes('aria-disabled')).toBe('false');
+          expect(button.attributes().disabled).toBeUndefined();
+          expect(button.classes().includes('d-toggle--disabled')).toBe(false);
+        });
 
-        it(
-          'should set correct disabled attributes when disabled prop is true',
-          async () => {
-            await wrapper.setProps({ disabled: true });
-            expect(button.attributes('aria-disabled')).toBe('true');
-            expect(button.attributes().disabled).toBe('disabled');
-            expect(button.classes().includes('d-toggle--disabled')).toBe(true);
-          },
-        );
+        it('should set correct disabled attributes when disabled prop is true', async () => {
+          await wrapper.setProps({ disabled: true });
+
+          expect(button.attributes('aria-disabled')).toBe('true');
+          expect(button.attributes().disabled).toBe('disabled');
+          expect(button.classes().includes('d-toggle--disabled')).toBe(true);
+        });
 
         it('should set correct size class', async () => {
           await wrapper.setProps({ size: 'sm' });
+
           expect(button.classes().includes('d-toggle--small')).toBe(true);
         });
       });
     });
+
     describe('Unchecked Toggle', () => {
-      // Test Setup
       beforeEach(() => {
-        propsData = {
-          ...basePropsData,
-          checked: false,
-        };
-        _setWrappers();
+        mockProps = { checked: false };
+
+        updateWrapper();
       });
 
-      it('should exist', () => { expect(wrapper.exists()).toBe(true); });
+      it('should exist', () => {
+        expect(wrapper.exists()).toBe(true);
+      });
 
       describe('checked behaviour', () => {
-        it(
-          'should set correct checked attributes when checked prop is false',
-          () => {
-            expect(button.attributes('aria-checked')).toBe('false');
-            expect(button.classes().includes('d-toggle--checked')).toBe(false);
-          },
-        );
+        it('should set correct checked attributes when checked prop is false', () => {
+          expect(button.attributes('aria-checked')).toBe('false');
+          expect(button.classes().includes('d-toggle--checked')).toBe(false);
+        });
       });
 
       describe('label behaviour', () => {
-        it('should exist', () => { expect(label.exists()).toBe(true); });
-        it(
-          'should match provided label prop',
-          () => { expect(label.text()).toBe(slots.default); },
-        );
+        it('should exist', () => {
+          expect(label.exists()).toBe(true);
+        });
+
+        it('should match provided label prop', () => {
+          expect(label.text()).toBe(baseSlot.default);
+        });
       });
     });
 
     describe('Checked Toggle', () => {
-      // Test Setup
       beforeEach(() => {
-        propsData = {
-          ...basePropsData,
+        mockProps = {
           checked: true,
           disabled: false,
         };
-        _setWrappers();
+
+        updateWrapper();
       });
 
-      it('should exist', () => { expect(wrapper.exists()).toBe(true); });
+      it('should exist', () => {
+        expect(wrapper.exists()).toBe(true);
+      });
 
       describe('checked behaviour', () => {
-        it(
-          'should set correct checked attributes when checked prop is true',
-          () => {
-            expect(button.attributes('aria-checked')).toBe('true');
-            expect(button.classes().includes('d-toggle--checked')).toBe(true);
-          },
-        );
+        it('should set correct checked attributes when checked prop is true', () => {
+          expect(button.attributes('aria-checked')).toBe('true');
+          expect(button.classes().includes('d-toggle--checked')).toBe(true);
+        });
       });
 
       describe('label behaviour', () => {
-        it('should exist', () => { expect(label.exists()).toBe(true); });
-        it(
-          'should match provided label prop',
-          () => { expect(label.text()).toBe(slots.default); },
-        );
+        it('should exist', () => {
+          expect(label.exists()).toBe(true);
+        });
+
+        it('should match provided label prop', () => {
+          expect(label.text()).toBe(baseSlot.default);
+        });
       });
     });
 
     describe('Indeterminate Toggle', () => {
-      // Test Setup
       beforeEach(() => {
-        propsData = {
-          ...basePropsData,
-          checked: 'mixed',
-        };
-        _setWrappers();
+        mockProps = { checked: 'mixed' };
+
+        updateWrapper();
       });
 
       it('should set indeterminate state when checked prop is mixed', () => {
@@ -205,45 +175,38 @@ describe('DtToggle Tests', () => {
 
     describe('Accessibility Tests', () => {
       describe('aria-label validations', () => {
-        const warningMessage = 'You must provide an aria-label when there is no label passed';
+        let MOCK_CONSOLE_ERROR_SPY;
+        let MOCK_CONSOLE_WARN_SPY;
 
-        beforeAll(() => {
-          Vue.config.silent = true;
-          vi.spyOn(Vue.util, 'warn').mockClear();
+        beforeEach(() => {
+          MOCK_CONSOLE_ERROR_SPY = vi.spyOn(console, 'error');
+          MOCK_CONSOLE_WARN_SPY = vi.spyOn(console, 'warn');
         });
 
-        afterAll(() => {
-          Vue.util.warn.mockRestore();
-          Vue.config.silent = false;
+        afterEach(() => {
+          MOCK_CONSOLE_ERROR_SPY.mockRestore();
+          MOCK_CONSOLE_WARN_SPY.mockRestore();
         });
 
         describe('should not throw a Vue error if a label is provided', () => {
-          beforeAll(() => {
-            propsData = basePropsData;
-            slots = { default: 'My Label' };
-            _setWrappers();
+          it('should not raise any warnings', () => {
+            mockSlots = { default: 'My Label' };
+
+            updateWrapper();
+
+            expect(console.warn).not.toHaveBeenCalled();
           });
-          itBehavesLikeDoesNotRaiseAnyVueWarnings();
         });
 
         describe('should not throw a Vue error if a label is not provided, but an aria-label attr exists', () => {
-          beforeAll(() => {
-            propsData = { ...basePropsData };
-            attrs = { 'aria-label': 'my label' };
-            slots = {};
-            _setWrappers();
-          });
-          itBehavesLikeDoesNotRaiseAnyVueWarnings();
-        });
+          it('should not raise any warnings', () => {
+            mockSlots = { default: '' };
+            mockAttrs = { 'aria-label': 'my label' };
 
-        describe('When neither ariaLabel attr nor a default slot exists', () => {
-          beforeAll(() => {
-            propsData = { ...basePropsData };
-            attrs = {};
-            slots = {};
-            _setWrappers();
+            updateWrapper();
+
+            expect(console.warn).not.toHaveBeenCalled();
           });
-          itBehavesLikeRaisesSingleVueWarning(warningMessage);
         });
       });
     });
--- components/tooltip/tooltip.test.js
+++ components/tooltip/tooltip.test.js
@@ -5,84 +5,65 @@ import {
   TOOLTIP_DIRECTIONS,
 } from './tooltip_constants';
 
+const MOCK_TRANSITION_STUB = () => ({
+  render: function (h) {
+    return this.$options._renderChildren;
+  },
+});
+
+const baseProps = { delay: false };
+const baseSlots = {
+  default: 'Test message',
+};
+const baseScopedSlots = {
+  anchor: '<button data-qa="dt-button" v-bind="props.attrs">Hover me</button>',
+};
+
+let mockProps = {};
+let mockSlots = {};
+let mockScopedSlots = {};
+const testContext = {};
+
 describe('DtTooltip tests', () => {
-  let testContext;
-
-  beforeAll(() => {
-    testContext = {};
-  });
-
-  // Wrappers
   let wrapper;
   let tooltipContainer;
   let tooltip;
   let anchor;
   let onMount;
 
-  const restoreSpy = function () {
-    onMount.mockRestore();
-  };
+  const updateWrapper = () => {
+    wrapper = mount(DtTooltip, {
+      propsData: { ...baseProps, ...mockProps },
+      slots: { ...baseSlots, ...mockSlots },
+      localVue: testContext.localVue,
+      stubs: {
+        // this gets around transition async problems. See https://v1.test-utils.vuejs.org/guides/common-tips.html
+        transition: MOCK_TRANSITION_STUB(),
+      },
+      scopedSlots: { ...baseScopedSlots, ...mockScopedSlots },
+    });
 
-  const setOnMount = function () {
-    onMount = vi.spyOn(DtTooltip.methods, 'onMount').mockClear();
-  };
-
-  const _clearChildWrappers = () => {
-    tooltipContainer = null;
-    tooltip = null;
-    anchor = null;
-  };
-
-  // Helpers
-  const _setChildWrappers = () => {
     tooltipContainer = wrapper.find('[data-qa="dt-tooltip-container"]');
     tooltip = wrapper.findComponent({ ref: 'content' });
     anchor = wrapper.findComponent({ ref: 'anchor' });
   };
 
-  const transitionStub = () => ({
-    render: function (h) {
-      return this.$options._renderChildren;
-    },
-  });
-
-  const _mountWrapper = () => {
-    wrapper = mount(DtTooltip, {
-      stubs: {
-        // this gets around transition async problems. See https://v1.test-utils.vuejs.org/guides/common-tips.html
-        transition: transitionStub(),
-      },
-      propsData: {
-        delay: false,
-      },
-      slots: {
-        default: 'Test message',
-      },
-      scopedSlots: {
-        anchor: '<button data-qa="dt-button" v-bind="props.attrs">Hover me</button>',
-      },
-      localVue: testContext.localVue,
-    });
-    _setChildWrappers();
-  };
-
   beforeAll(() => {
+    testContext.localVue = createLocalVue();
     // RequestAnimationFrame and cancelAnimationFrame are undefined in the scope
     // Need to mock them to avoid error
     global.requestAnimationFrame = vi.fn();
     global.cancelAnimationFrame = vi.fn();
-    testContext.localVue = createLocalVue();
   });
 
-  beforeEach(async () => {
-    _mountWrapper();
+  beforeEach(() => {
+    updateWrapper();
   });
 
-  afterEach(async () => {
-    // close to unmount tippy
-    await wrapper.setProps({ show: false });
-    wrapper.destroy();
-    _clearChildWrappers();
+  afterEach(() => {
+    mockProps = {};
+    mockSlots = {};
+    mockScopedSlots = {};
   });
 
   afterAll(() => {
@@ -93,136 +74,114 @@ describe('DtTooltip tests', () => {
 
   describe('Presentation Tests', () => {
     describe('when tooltip is open', () => {
-      beforeEach(async () => {
-        await wrapper.setProps({ show: true });
-        _setChildWrappers();
+      beforeEach(() => {
+        mockProps = { show: true };
+
+        updateWrapper();
       });
 
-      it(
-        'should render the component',
-        () => { expect(wrapper.exists()).toBe(true); },
-      );
-      it(
-        'should render the container',
-        () => { expect(tooltipContainer.exists()).toBe(true); },
-      );
-      it(
-        'should render the tooltip',
-        () => { expect(tooltip.exists()).toBe(true); },
-      );
-      it(
-        'should render the anchor',
-        () => { expect(anchor.text()).toBe('Hover me'); },
-      );
+      it('should render the component', () => {
+        expect(wrapper.exists()).toBe(true);
+      });
+
+      it('should render the container', () => {
+        expect(tooltipContainer.exists()).toBe(true);
+      });
+
+      it('should render the tooltip', () => {
+        expect(tooltip.exists()).toBe(true);
+      });
+
+      it('should render the anchor', () => {
+        expect(anchor.text()).toBe('Hover me');
+      });
+
       it('should set default classes', () => {
         expect(tooltip.classes('d-tooltip__arrow-tippy--top')).toBe(true);
       });
-      it('should render the message', async () => {
+
+      it('should render the message', () => {
         expect(tooltip.text()).toBe('Test message');
       });
 
       describe('When inverted is true', () => {
-        beforeEach(async () => {
-          await wrapper.setProps({ inverted: true });
-          _setChildWrappers();
-        });
         it('should have the inverted class set', () => {
+          mockProps = { inverted: true };
+
+          updateWrapper();
+
           expect(tooltip.classes(TOOLTIP_KIND_MODIFIERS.inverted)).toBe(true);
         });
       });
 
       describe('When a placement is provided', () => {
-        TOOLTIP_DIRECTIONS.forEach(placement => describe(`When direction is ${placement}`, () => {
-          beforeEach(async () => {
-            await wrapper.setProps({ placement });
-          });
+        TOOLTIP_DIRECTIONS.forEach(placement =>
+          describe(`When direction is ${placement}`, () => {
+            it('should have correct arrow direction class', () => {
+              mockProps = { placement };
 
-          it('should have correct arrow direction class', async () => {
-            expect(tooltip.classes(`d-tooltip__arrow-tippy--${placement}`)).toBe(true);
-          });
-        }));
+              updateWrapper();
+
+              expect(tooltip.classes(`d-tooltip__arrow-tippy--${placement}`)).toBe(true);
+            });
+          }));
       });
     });
   });
 
   describe('Interactivity Tests', () => {
-    beforeEach(setOnMount);
-    afterEach(restoreSpy);
+    beforeEach(() => {
+      onMount = vi.spyOn(DtTooltip.methods, 'onMount').mockClear();
+    });
+
+    afterEach(() => {
+      onMount.mockRestore();
+    });
 
     describe('When show prop is true', () => {
-      beforeEach(async () => {
-        await wrapper.setProps({ show: true });
-      });
-
       it('should display tooltip', async () => {
+        await wrapper.setProps({ show: true });
+
         expect(tooltip.isVisible()).toBe(true);
       });
     });
 
     describe('When show prop is false', () => {
-      beforeEach(async () => {
-        await wrapper.setProps({ show: false });
-      });
-
       it('should display tooltip', async () => {
+        await wrapper.setProps({ show: false });
+
         expect(tooltip.isVisible()).toBe(false);
       });
     });
 
     describe('When show prop is unset (default behaviour)', () => {
-      beforeEach(async () => {
-        await wrapper.setProps({ show: null });
-        _setChildWrappers();
+      beforeEach(() => {
+        mockProps = { show: null };
+
+        updateWrapper();
       });
 
       describe('When mouseenter tooltip', () => {
-        beforeEach(async () => {
+        it('should display tooltip', async () => {
           await wrapper.setProps({ delay: false });
           await anchor.trigger('mouseenter');
-        });
 
-        it('should display tooltip', () => {
           expect(tooltip.isVisible()).toBe(true);
         });
       });
 
-      // jsdom does not yet support :focus-visible, commenting this out for now...
-      // describe('When focusin tooltip', function () {
-      //   beforeEach(async function () {
-      //     await anchor.trigger('focusin');
-      //   });
-
-      //   it('should display tooltip', function () {
-      //     assert.isTrue(tooltip.isVisible());
-      //   });
-
-      //   describe('When escape is pressed', function () {
-      //     beforeEach(async function () {
-      //       await anchor.trigger('keydown', { key: 'Escape' });
-      //     });
-
-      //     it('should hide tooltip', function () {
-      //       assert.isFalse(tooltip.isVisible());
-      //     });
-      //   });
-      // });
-
       describe('When mouseleave tooltip', () => {
-        beforeEach(async () => {
+        it('should hide tooltip', async () => {
           await anchor.trigger('mouseleave');
-        });
 
-        it('should hide tooltip', () => {
           expect(tooltip.isVisible()).toBe(false);
         });
       });
 
       describe('When focusout tooltip', () => {
-        beforeEach(async () => {
+        it('should display tooltip', async () => {
           await anchor.trigger('focusout');
-        });
 
-        it('should display tooltip', () => {
           expect(tooltip.isVisible()).toBe(false);
         });
       });
