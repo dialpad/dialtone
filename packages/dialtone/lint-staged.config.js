bd5eaeda5bc90c4e338b757665a6f0900beeec59
Julio Ortega <julio.ortega@dialpad.com>
2023-11-01 15:44:02 -0600

    dialtone cleanup

--- packages/dialtone/lint-staged.config.js
+++ packages/dialtone/lint-staged.config.js
@@ -2,9 +2,6 @@ module.exports = {
   '*.{js, vue}': [
     'npm run lint-staged:code',
   ],
-  '*.md': [
-    'npm run lint-staged:docs',
-  ],
   '*.{less, css}': [
     'npm run lint-staged:library',
   ],
