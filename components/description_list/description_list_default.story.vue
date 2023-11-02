9ef361b48062ba654c81be072cf7d7396c83e26e
Nina <ninamrepetto@gmail.com>
2023-11-02 18:17:17 -0300

    feat: add description list component (#1289)
    * feat: add description list component

---
+++ components/description_list/description_list_default.story.vue
@@ -0,0 +1,18 @@
+<template>
+  <dt-description-list
+    :gap="gap"
+    :items="items"
+    :direction="direction"
+    :term-class="termClass"
+    :description-class="descriptionClass"
+  />
+</template>
+
+<script>
+import DtDescriptionList from './description_list.vue';
+
+export default {
+  name: 'DtDescriptionListDefault',
+  components: { DtDescriptionList },
+};
+</script>
