95ea15eab5ad432db80f5eb5f3bdf4ccc8a9f46a
Ignacio Ropolo <89984179+iropolo@users.noreply.github.com>
2023-12-18 14:52:27 -0300

    feat(hovercard): hovercard stub (#58)
    * feat(hovercard): hovercard stub
    
    * feat(hovercard): hovercard clean stub
    
    * feat(hovercard): hovercard stub pr comments
    
    ---------
    
    Co-authored-by: iropolo <ignacio.ropolo@dialpad.com>

---
+++ packages/dialtone-vue3/components/hovercard/hovercard.stories.js
@@ -0,0 +1,140 @@
+import DtHovercard from './hovercard.vue';
+
+import DtHovercardDefaultTemplate from './hovercard_default_story.vue';
+import { createTemplateFromVueFile } from '@/common/storybook_utils';
+import { action } from '@storybook/addon-actions';
+import {
+  POPOVER_DIRECTIONS,
+  POPOVER_PADDING_CLASSES,
+} from '@/components/popover/index.js';
+
+export const argTypesData = {
+  // Slots
+  anchor: {
+    table: {
+      type: {
+        summary: 'VNode',
+      },
+    },
+  },
+  content: {
+    control: 'text',
+    table: {
+      type: {
+        summary: 'VNode',
+      },
+    },
+  },
+  headerContent: {
+    name: 'headerContent',
+    description: 'Slot for popover header content',
+    control: 'text',
+    table: {
+      category: 'slots',
+      type: {
+        summary: 'VNode',
+      },
+    },
+  },
+  footerContent: {
+    name: 'footerContent',
+    description: 'Slot for popover footer content',
+    control: 'text',
+    table: {
+      category: 'slots',
+      type: {
+        summary: 'VNode',
+      },
+    },
+  },
+
+  // Props
+  id: {
+    table: {
+      defaultValue: {
+        summary: 'generated unique ID',
+      },
+    },
+  },
+  padding: {
+    options: Object.keys(POPOVER_PADDING_CLASSES),
+    control: {
+      type: 'select',
+    },
+  },
+  placement: {
+    options: POPOVER_DIRECTIONS,
+    control: {
+      type: 'select',
+    },
+    table: {
+      defaultValue: {
+        summary: 'top-start',
+      },
+    },
+  },
+  transition: {
+    control: {
+      type: 'boolean',
+    },
+  },
+  appendTo: {
+    table: {
+      defaultValue: {
+        summary: 'body',
+      },
+    },
+  },
+
+  // Events
+  onOpened: {
+    table: {
+      disable: true,
+    },
+  },
+
+  opened: {
+    description: `emitted when popover is shown or hidden.`,
+    table: {
+      type: {
+        summary: 'event',
+      },
+    },
+  },
+};
+
+export const argsData = {
+  placement: 'bottom-end',
+  contentWidth: null,
+  sticky: false,
+  offset: [0, 16],
+  appendTo: 'body',
+  onOpened: action('opened'),
+  visuallyHiddenCloseLabel: 'Close popover',
+  transition: false,
+};
+
+const decorator = () => ({
+  template: `<div class="d-wmx264"><story />
+  </div>`,
+});
+
+export default {
+  title: 'Components/Hovercard',
+  component: DtHovercard,
+  args: argsData,
+  argTypes: argTypesData,
+  decorators: [decorator],
+  excludeStories: /.*Data$/,
+};
+
+const DefaultTemplate = (args, { argTypes }) => createTemplateFromVueFile(
+  args,
+  argTypes,
+  DtHovercardDefaultTemplate,
+);
+export const Default = {
+  render: DefaultTemplate,
+
+  args: {},
+};
---
+++ packages/dialtone-vue3/components/hovercard/hovercard.vue
@@ -0,0 +1,171 @@
+<template>
+  <dt-popover
+    :id="id"
+    :placement="placement"
+    :content-class="contentClass"
+    :fallback-placements="fallbackPlacements"
+    :padding="padding"
+    :transition="transition ? 'fade' : null"
+    :offset="offset"
+    :modal="false"
+    initial-focus-element="none"
+    :header-class="headerClass"
+    :footer-class="footerClass"
+    :append-to="appendTo"
+    :hovercard="true"
+    @opened="(e) => ($emit('opened', e))"
+  >
+    <template #anchor>
+      <slot name="anchor" />
+    </template>
+    <template #content>
+      <slot name="content" />
+    </template>
+    <template #headerContent>
+      <slot name="headerContent" />
+    </template>
+
+    <template #footerContent>
+      <slot name="footerContent" />
+    </template>
+  </dt-popover>
+</template>
+
+<script setup>
+import { POPOVER_APPEND_TO_VALUES, POPOVER_PADDING_CLASSES, DtPopover } from '@/components/popover/index.js';
+import { getUniqueString } from '@/common/utils';
+
+defineProps({
+  /**
+     * Fade transition when the content display is toggled.
+     * @type boolean
+     * @values true, false
+     */
+  transition: {
+    type: Boolean,
+    default: false,
+  },
+
+  /**
+     * If the popover does not fit in the direction described by "placement",
+     * it will attempt to change its direction to the "fallbackPlacements".
+     * <a
+     *   class="d-link"
+     *   href="https://popper.js.org/docs/v2/modifiers/flip/#fallbackplacements"
+     *   target="_blank"
+     * >
+     *   Popper.js docs
+     * </a>
+     * */
+  fallbackPlacements: {
+    type: Array,
+    default: () => {
+      return ['auto'];
+    },
+  },
+
+  /**
+     * The direction the popover displays relative to the anchor.
+     * <a
+     *   class="d-link"
+     *   href="https://atomiks.github.io/tippyjs/v6/all-props/#placement"
+     *   target="_blank"
+     * >
+     *   Tippy.js docs
+     * </a>
+     * @values top, top-start, top-end,
+     * right, right-start, right-end,
+     * left, left-start, left-end,
+     * bottom, bottom-start, bottom-end,
+     * auto, auto-start, auto-end
+     */
+  placement: {
+    type: String,
+    default: 'top-start',
+  },
+
+  /**
+     * Padding size class for the popover content.
+     * @values none, small, medium, large
+     */
+  padding: {
+    type: String,
+    default: 'large',
+    validator: (padding) => {
+      return Object.keys(POPOVER_PADDING_CLASSES).some((item) => item === padding);
+    },
+  },
+
+  /**
+     *  Displaces the content box from its anchor element
+     *  by the specified number of pixels.
+     *  <a
+     *    class="d-link"
+     *    href="https://atomiks.github.io/tippyjs/v6/all-props/#offset"
+     *    target="_blank"
+     *  >
+     *    Tippy.js docs
+     *  </a>
+     */
+  offset: {
+    type: Array,
+    default: () => [0, 16],
+  },
+
+  /**
+     * The id of the tooltip
+     */
+  id: {
+    type: String,
+    default () { return getUniqueString(); },
+  },
+
+  /**
+     * Additional class name for the header content wrapper element.
+     */
+  headerClass: {
+    type: [String, Array, Object],
+    default: '',
+  },
+
+  /**
+     * Additional class name for the footer content wrapper element.
+     */
+  footerClass: {
+    type: [String, Array, Object],
+    default: '',
+  },
+
+  /**
+     * Additional class name for the dialog element.
+     */
+  dialogClass: {
+    type: [String, Array, Object],
+    default: '',
+  },
+
+  /**
+     * Additional class name for the content wrapper element.
+     */
+  contentClass: {
+    type: [String, Array, Object],
+    default: '',
+  },
+
+  /**
+     * Sets the element to which the popover is going to append to.
+     * 'body' will append to the nearest body (supports shadow DOM).
+     * @values 'body', 'parent', HTMLElement,
+     */
+  appendTo: {
+    type: [HTMLElement, String],
+    default: 'body',
+    validator: appendTo => {
+      return POPOVER_APPEND_TO_VALUES.includes(appendTo) ||
+            (appendTo instanceof HTMLElement);
+    },
+  },
+});
+
+defineEmits(['opened']);
+</script>
