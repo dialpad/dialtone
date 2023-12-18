20411681519056a001c3330817960586a704f232
Julio Ortega <julio.ortega@dialpad.com>
2023-12-18 14:09:49 -0600

    fix config file

--- packages/dialtone-vue2/percy.config.cjs
+++ packages/dialtone-vue2/percy.config.cjs
@@ -1,9 +1,10 @@
-import baseConfig from '../../percy.base_config.cjs'
+const baseConfig = require('../../percy.base_config.cjs');
 
 module.exports = {
   ...baseConfig,
   storybook: {
     exclude: [
+      ...baseConfig.storybook.exclude
       // Add specific dialtone-vue 2 stories to exclude
     ],
   },
--- packages/dialtone-vue3/percy.config.cjs
+++ packages/dialtone-vue3/percy.config.cjs
@@ -1,9 +1,11 @@
-import baseConfig from '../../percy.base_config.cjs'
+const baseConfig = require('../../percy.base_config.cjs');
 
 module.exports = {
   ...baseConfig,
   storybook: {
     exclude: [
+      ...baseConfig.storybook.exclude,
+      // Add specific dialtone-vue 3 stories to exclude
       'Components/Scroller: Dynamic',
     ],
   },
