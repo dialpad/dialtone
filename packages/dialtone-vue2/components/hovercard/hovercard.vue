dcfb9674dc78dcc0ef71dac7ed099737f1887166
Nina <ninamrepetto@gmail.com>
2024-01-05 13:13:23 -0300

    update hovercard vue2 to options api

--- packages/dialtone-vue2/components/hovercard/hovercard.vue
+++ packages/dialtone-vue2/components/hovercard/hovercard.vue
@@ -34,145 +34,165 @@
   </dt-popover>
 </template>
 
-<script setup>
+<script>
 import { reactive } from 'vue';
 import { POPOVER_APPEND_TO_VALUES, POPOVER_PADDING_CLASSES, DtPopover } from '@/components/popover/index.js';
 import { getUniqueString } from '@/common/utils';
 import useTimer from './timer';
 
-const timer = reactive(useTimer());
+export default {
+  name: 'DtHovercard',
 
-defineProps({
-  /**
-     * Fade transition when the content display is toggled.
-     * @type boolean
-     * @values true, false
-     */
-  transition: {
-    type: Boolean,
-    default: false,
+  components: {
+    DtPopover,
   },
 
+  props: {
   /**
-     * If the popover does not fit in the direction described by "placement",
-     * it will attempt to change its direction to the "fallbackPlacements".
-     * <a
-     *   class="d-link"
-     *   href="https://popper.js.org/docs/v2/modifiers/flip/#fallbackplacements"
-     *   target="_blank"
-     * >
-     *   Popper.js docs
-     * </a>
-     * */
-  fallbackPlacements: {
-    type: Array,
-    default: () => {
-      return ['auto'];
+   * Fade transition when the content display is toggled.
+   * @type boolean
+   * @values true, false
+   */
+    transition: {
+      type: Boolean,
+      default: false,
     },
-  },
 
-  /**
-     * The direction the popover displays relative to the anchor.
-     * <a
-     *   class="d-link"
-     *   href="https://atomiks.github.io/tippyjs/v6/all-props/#placement"
-     *   target="_blank"
-     * >
-     *   Tippy.js docs
-     * </a>
-     * @values top, top-start, top-end,
-     * right, right-start, right-end,
-     * left, left-start, left-end,
-     * bottom, bottom-start, bottom-end,
-     * auto, auto-start, auto-end
-     */
-  placement: {
-    type: String,
-    default: 'top-start',
-  },
-
-  /**
-     * Padding size class for the popover content.
-     * @values none, small, medium, large
-     */
-  padding: {
-    type: String,
-    default: 'large',
-    validator: (padding) => {
-      return Object.keys(POPOVER_PADDING_CLASSES).some((item) => item === padding);
+    /**
+   * If the popover does not fit in the direction described by "placement",
+   * it will attempt to change its direction to the "fallbackPlacements".
+   * <a
+   *   class="d-link"
+   *   href="https://popper.js.org/docs/v2/modifiers/flip/#fallbackplacements"
+   *   target="_blank"
+   * >
+   *   Popper.js docs
+   * </a>
+   * */
+    fallbackPlacements: {
+      type: Array,
+      default: () => {
+        return ['auto'];
+      },
     },
-  },
 
-  /**
-     *  Displaces the content box from its anchor element
-     *  by the specified number of pixels.
-     *  <a
-     *    class="d-link"
-     *    href="https://atomiks.github.io/tippyjs/v6/all-props/#offset"
-     *    target="_blank"
-     *  >
-     *    Tippy.js docs
-     *  </a>
-     */
-  offset: {
-    type: Array,
-    default: () => [0, 16],
-  },
+    /**
+   * The direction the popover displays relative to the anchor.
+   * <a
+   *   class="d-link"
+   *   href="https://atomiks.github.io/tippyjs/v6/all-props/#placement"
+   *   target="_blank"
+   * >
+   *   Tippy.js docs
+   * </a>
+   * @values top, top-start, top-end,
+   * right, right-start, right-end,
+   * left, left-start, left-end,
+   * bottom, bottom-start, bottom-end,
+   * auto, auto-start, auto-end
+   */
+    placement: {
+      type: String,
+      default: 'top-start',
+    },
 
-  /**
-     * The id of the tooltip
-     */
-  id: {
-    type: String,
-    default () { return getUniqueString(); },
-  },
+    /**
+   * Padding size class for the popover content.
+   * @values none, small, medium, large
+   */
+    padding: {
+      type: String,
+      default: 'large',
+      validator: (padding) => {
+        return Object.keys(POPOVER_PADDING_CLASSES).some((item) => item === padding);
+      },
+    },
 
-  /**
-     * Additional class name for the header content wrapper element.
-     */
-  headerClass: {
-    type: [String, Array, Object],
-    default: '',
-  },
+    /**
+   *  Displaces the content box from its anchor element
+   *  by the specified number of pixels.
+   *  <a
+   *    class="d-link"
+   *    href="https://atomiks.github.io/tippyjs/v6/all-props/#offset"
+   *    target="_blank"
+   *  >
+   *    Tippy.js docs
+   *  </a>
+   */
+    offset: {
+      type: Array,
+      default: () => [0, 16],
+    },
 
-  /**
-     * Additional class name for the footer content wrapper element.
-     */
-  footerClass: {
-    type: [String, Array, Object],
-    default: '',
-  },
+    /**
+   * The id of the tooltip
+   */
+    id: {
+      type: String,
+      default () { return getUniqueString(); },
+    },
 
-  /**
-     * Additional class name for the dialog element.
-     */
-  dialogClass: {
-    type: [String, Array, Object],
-    default: '',
-  },
+    /**
+   * Additional class name for the header content wrapper element.
+   */
+    headerClass: {
+      type: [String, Array, Object],
+      default: '',
+    },
 
-  /**
+    /**
+   * Additional class name for the footer content wrapper element.
+   */
+    footerClass: {
+      type: [String, Array, Object],
+      default: '',
+    },
+
+    /**
+   * Additional class name for the dialog element.
+   */
+    dialogClass: {
+      type: [String, Array, Object],
+      default: '',
+    },
+
+    /**
      * Additional class name for the content wrapper element.
      */
-  contentClass: {
-    type: [String, Array, Object],
-    default: '',
-  },
+    contentClass: {
+      type: [String, Array, Object],
+      default: '',
+    },
 
-  /**
-     * Sets the element to which the popover is going to append to.
-     * 'body' will append to the nearest body (supports shadow DOM).
-     * @values 'body', 'parent', HTMLElement,
-     */
-  appendTo: {
-    type: [HTMLElement, String],
-    default: 'body',
-    validator: appendTo => {
-      return POPOVER_APPEND_TO_VALUES.includes(appendTo) ||
+    /**
+   * Sets the element to which the popover is going to append to.
+   * 'body' will append to the nearest body (supports shadow DOM).
+   * @values 'body', 'parent', HTMLElement,
+   */
+    appendTo: {
+      type: [HTMLElement, String],
+      default: 'body',
+      validator: appendTo => {
+        return POPOVER_APPEND_TO_VALUES.includes(appendTo) ||
             (appendTo instanceof HTMLElement);
+      },
     },
   },
-});
 
-defineEmits(['opened']);
+  emits: [
+    /**
+     * Emitted when popover is shown or hidden
+     *
+     * @event opened
+     * @type {Boolean | Array}
+     */
+    'opened',
+  ],
+
+  data () {
+    return {
+      timer: reactive(useTimer()),
+    };
+  },
+};
 </script>
