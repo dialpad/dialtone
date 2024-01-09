b1117c8e79b2286b2d14bd379b1209086209224a
Nina <ninamrepetto@gmail.com>
2024-01-09 12:21:44 -0300

    add visual changes to vue 3

--- packages/dialtone-vue3/components/hovercard/hovercard_default.story.vue
+++ packages/dialtone-vue3/components/hovercard/hovercard_default.story.vue
@@ -16,39 +16,84 @@
     <template #anchor>
       <dt-recipe-contact-row
         name="Jaqueline Nackos"
-        avatar-presence="active"
+        avatar-presence="busy"
         avatar-seed="JN"
         avatar-alt="Avatar person"
         :avatar-src="defaultImage"
-        user-status="Good Morning! :smile:"
+        user-status="Working from SF"
         call-button-tooltip=""
       />
     </template>
     <template #content>
-      <div>
-        <dt-recipe-contact-row
-          name="Jaqueline Nackos"
-          avatar-presence="active"
-          avatar-seed="JN"
-          avatar-alt="Avatar person"
-          :avatar-src="defaultImage"
-          user-status="Good Morning! :smile:"
-          call-button-tooltip="Call"
-          selected
-          :has-call-button="false"
-        />
-        <br>
-        <p>
-          Vice President of Sales Enablement
-          Aerolabs
-        </p>
-        <p>
-          <b>Work:</b> (415) 555-1234
-        </p>
-        <p>
-          <b>Local Time:</b> 12:32 PM
-        </p>
-      </div>
+      <dt-stack
+        direction="column"
+        gap="400"
+      >
+        <dt-stack
+          direction="row"
+          gap="300"
+        >
+          <dt-avatar
+            full-name="Jaqueline Nackos"
+            :image-src="defaultImage"
+            image-alt="Person avatar"
+            seed="JN"
+            size="md"
+            presence="busy"
+          />
+          <dt-stack
+            direction="column"
+            gap="0"
+          >
+            <p class="d-headline-compact-medium">
+              Jaqueline Nackos
+            </p>
+            <p class="d-body-small d-fc-tertiary">
+              <span class="d-fc-critical">In a meeting</span> • Working from SF
+            </p>
+          </dt-stack>
+        </dt-stack>
+        <dt-stack
+          direction="column"
+          gap="300"
+          class="d-fc-secondary"
+        >
+          <p>
+            Vice President of Sales Enablement Aerolabs
+          </p>
+          <dt-stack
+            direction="row"
+            gap="300"
+          >
+            <dt-icon
+              name="clock-4"
+              size="200"
+            />
+            <p class="d-body-small">
+              <b>Local Time:</b> 12:32 PM
+            </p>
+          </dt-stack>
+        </dt-stack>
+        <dt-stack
+          direction="row"
+          gap="400"
+        >
+          <dt-button
+            width="var(--dt-size-100-percent)"
+            importance="outlined"
+            kind="muted"
+          >
+            Call
+          </dt-button>
+          <dt-button
+            width="var(--dt-size-100-percent)"
+            importance="outlined"
+            kind="muted"
+          >
+            Message
+          </dt-button>
+        </dt-stack>
+      </dt-stack>
     </template>
     <template
       v-if="$attrs.headerContent"
@@ -69,4 +114,8 @@
 import DtHovercard from './hovercard.vue';
 import DtRecipeContactRow from '@/recipes/leftbar/contact_row/contact_row.vue';
 import defaultImage from '@/common/assets/avatar2.png';
+import DtStack from '../stack/stack.vue';
+import DtIcon from '../icon/icon.vue';
+import DtButton from '../button/button.vue';
+import DtAvatar from '../avatar/avatar.vue';
 </script>
