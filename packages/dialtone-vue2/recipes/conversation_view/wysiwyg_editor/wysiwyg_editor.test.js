1afefb529d8c9424485f3d68d80c055d68a78dda
Eugenia Mariotti <eugenia.mariotti@dialpad.com>
2024-01-19 10:27:21 -0300

    feat(wysiwyg-editor, rich-text-editor): add wysiwyg component

---
+++ packages/dialtone-vue2/recipes/conversation_view/wysiwyg_editor/wysiwyg_editor.test.js
@@ -0,0 +1,450 @@
+import { mount, createLocalVue } from '@vue/test-utils';
+import DtRecipeWysiwygEditor from './wysiwyg_editor.vue';
+
+// Wrappers
+let wrapper;
+let editor;
+
+let wysiwygEditorEl;
+let linkInputEl;
+
+let boldFormatBtn;
+let italicsFormatBtn;
+let underlineFormatBtn;
+let strikeFormatBtn;
+let listItemsFormatBtn;
+let addLinkBtn;
+let confirmAddLinkBtn;
+let cancelAddLinkBtn;
+
+const testText = 'In the beginning, it was a nice day.';
+
+// Constants
+const baseProps = {
+  value: testText,
+  inputAriaLabel: 'aria-label text',
+  inputClass: 'qa-editor',
+  autoFocus: 'all',
+};
+
+// Test Environment
+let propsData = baseProps;
+let slots;
+let listeners;
+const getClientRectsMock = vi.fn(() => [{}]);
+const getBoundingClientRectMock = vi.fn(() => [{}]);
+const scrollByMock = vi.fn();
+
+const testContext = {};
+
+const _setChildWrappers = () => {
+  editor = wrapper.find('[data-qa="dt-rich-text-editor"]').find('div[contenteditable]');
+
+  // buttons
+  boldFormatBtn = wrapper.find('[data-qa="dt-wysiwyg-editor-bold-btn"]');
+  italicsFormatBtn = wrapper.find('[data-qa="dt-wysiwyg-editor-italics-btn"]');
+  underlineFormatBtn = wrapper.find('[data-qa="dt-wysiwyg-editor-underline-btn"]');
+  strikeFormatBtn = wrapper.find('[data-qa="dt-wysiwyg-editor-strike-btn"]');
+  listItemsFormatBtn = wrapper.find('[data-qa="dt-wysiwyg-editor-list-items-btn"]');
+  addLinkBtn = wrapper.find('[data-qa="dt-wysiwyg-editor-add-link-btn"]');
+
+  // Els
+  wysiwygEditorEl = wrapper.find('[data-qa="dt-wysiwyg-editor"]');
+};
+
+const _mountWrapper = () => {
+  wrapper = mount(DtRecipeWysiwygEditor, {
+    propsData,
+    listeners,
+    slots,
+    localVue: testContext.localVue,
+    attachTo: document.body,
+  });
+};
+
+describe('DtRecipeWysiwygEditor tests', () => {
+  // Test Setup
+  beforeAll(() => {
+    testContext.localVue = createLocalVue();
+    global.Range.prototype.getClientRects = getClientRectsMock;
+    global.Range.prototype.getBoundingClientRect = getBoundingClientRectMock;
+    global.scrollBy = scrollByMock;
+  });
+
+  beforeEach(async () => {
+    _mountWrapper();
+    await wrapper.vm.$nextTick();
+    _setChildWrappers();
+  });
+
+  // Test Teardown
+  afterEach(function () {
+    propsData = baseProps;
+  });
+
+  describe('Presentation Tests', function () {
+    it('should render the component', function () {
+      expect(wrapper.exists()).toBe(true);
+    });
+
+    it('should contain the initial value', function () {
+      expect(editor.text()).toBe(testText);
+    });
+
+    it('should contain bold format button', function () {
+      expect(boldFormatBtn.exists()).toBe(true);
+    });
+
+    it('should contain italics format btn', function () {
+      expect(italicsFormatBtn.exists()).toBe(true);
+    });
+
+    it('should contain underline button', function () {
+      expect(underlineFormatBtn.exists()).toBe(true);
+    });
+
+    it('should contain strike button', function () {
+      expect(strikeFormatBtn.exists()).toBe(true);
+    });
+
+    it('should contain list items button', function () {
+      expect(listItemsFormatBtn.exists()).toBe(true);
+    });
+
+    it('should contain add link button', function () {
+      expect(addLinkBtn.exists()).toBe(true);
+    });
+
+    it('should not have border applied on message-input when not focused', () => {
+      expect(wysiwygEditorEl.classes('d-bc-default')).toBe(true);
+      expect(wysiwygEditorEl.classes('d-bc-black-500')).toBe(false);
+    });
+
+    describe('When we focus anywhere on the wysiwyg editor', () => {
+      it('should focus and add the border for the wysiwyg editor', async () => {
+        await editor.trigger('focus');
+        expect(wysiwygEditorEl.classes('d-bc-black-500')).toBe(true);
+      });
+
+      it('should programmatically focus to input', async () => {
+        wrapper.vm.onFocus();
+        await wrapper.vm.$nextTick();
+        expect(wysiwygEditorEl.classes('d-bc-black-500')).toBe(true);
+      });
+    });
+
+    describe('When rounded edges is disabled', () => {
+      beforeEach(async () => {
+        await wrapper.setProps({
+          roundedEdges: false,
+          value: testText,
+        });
+      });
+
+      it('the editor edges should not be round', async () => {
+        expect(editor.text()).toBe(testText);
+        expect(wysiwygEditorEl.classes('d-bar0')).toBe(true);
+      });
+    });
+
+    describe('When bold button is disabled', () => {
+      beforeEach(async () => {
+        _mountWrapper();
+        await wrapper.setProps({ showBoldButton: false });
+        await wrapper.vm.$nextTick();
+        _setChildWrappers();
+      });
+
+      it('should not appear in the formatting options', () => {
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-bold-btn"]')
+          .exists()).toBe(false);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-italics-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-underline-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-strike-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-list-items-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-add-link-btn"]')
+          .exists()).toBe(true);
+      });
+    });
+
+    describe('When italics button is disabled', () => {
+      beforeEach(async () => {
+        _mountWrapper();
+        await wrapper.setProps({ showItalicsButton: false });
+        await wrapper.vm.$nextTick();
+        _setChildWrappers();
+      });
+
+      it('should not appear in the formatting options', () => {
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-bold-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-italics-btn"]')
+          .exists()).toBe(false);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-underline-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-strike-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-list-items-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-add-link-btn"]')
+          .exists()).toBe(true);
+      });
+    });
+
+    describe('When underline button is disabled', () => {
+      beforeEach(async () => {
+        _mountWrapper();
+        await wrapper.setProps({ showUnderlineButton: false });
+        await wrapper.vm.$nextTick();
+        _setChildWrappers();
+      });
+
+      it('should not appear in the formatting options', () => {
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-bold-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-italics-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-underline-btn"]')
+          .exists()).toBe(false);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-strike-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-list-items-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-add-link-btn"]')
+          .exists()).toBe(true);
+      });
+    });
+
+    describe('When strike button is disabled', () => {
+      beforeEach(async () => {
+        _mountWrapper();
+        await wrapper.setProps({ showStrikeButton: false });
+        await wrapper.vm.$nextTick();
+        _setChildWrappers();
+      });
+
+      it('should not appear in the formatting options', () => {
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-bold-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-italics-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-underline-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-strike-btn"]')
+          .exists()).toBe(false);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-list-items-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-add-link-btn"]')
+          .exists()).toBe(true);
+      });
+    });
+
+    describe('When list items button is disabled', () => {
+      beforeEach(async () => {
+        _mountWrapper();
+        await wrapper.setProps({ showListItemsButton: false });
+        await wrapper.vm.$nextTick();
+        _setChildWrappers();
+      });
+
+      it('should not appear in the formatting options', () => {
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-bold-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-italics-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-underline-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-strike-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-list-items-btn"]')
+          .exists()).toBe(false);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-add-link-btn"]')
+          .exists()).toBe(true);
+      });
+    });
+
+    describe('When add link button is disabled', () => {
+      beforeEach(async () => {
+        _mountWrapper();
+        await wrapper.setProps({ showAddLinkButton: false });
+        await wrapper.vm.$nextTick();
+        _setChildWrappers();
+      });
+
+      it('should not appear in the formatting options', () => {
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-bold-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-italics-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-underline-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-strike-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-list-items-btn"]')
+          .exists()).toBe(true);
+
+        expect(wrapper
+          .find('[data-qa="dt-wysiwyg-editor-add-link-btn"]')
+          .exists()).toBe(false);
+      });
+    });
+  });
+
+  describe('Interactivity tests', function () {
+    beforeEach(async () => {
+      _mountWrapper();
+      await wrapper.vm.$nextTick();
+      _setChildWrappers();
+    });
+
+    describe('When bold font button is clicked', () => {
+      it('editor output should be enclosed in bold html tags', async () => {
+        await boldFormatBtn.trigger('click');
+        await wrapper.vm.$nextTick();
+        expect(editor.html()).toContain('<strong>In the beginning, it was a nice day.</strong>');
+      });
+    });
+
+    describe('When italics font button is clicked', () => {
+      it('editor output should be enclosed in italics html tags', async () => {
+        await italicsFormatBtn.trigger('click');
+        await wrapper.vm.$nextTick();
+        expect(editor.html()).toContain('<em>In the beginning, it was a nice day.</em>');
+      });
+    });
+
+    describe('When underline text button is clicked', () => {
+      it('editor output should be enclosed in underline html tags', async () => {
+        await underlineFormatBtn.trigger('click');
+        await wrapper.vm.$nextTick();
+        expect(editor.html()).toContain('<u>In the beginning, it was a nice day.</u>');
+      });
+    });
+
+    describe('When strike button is clicked', () => {
+      it('editor output should be enclosed in strike html tags', async () => {
+        await strikeFormatBtn.trigger('click');
+        await wrapper.vm.$nextTick();
+        expect(editor.html()).toContain('<s>In the beginning, it was a nice day.</s>');
+      });
+    });
+
+    describe('When list items button is clicked', () => {
+      it('editor output should be enclosed in list item html tags', async () => {
+        const expectedHtmlOutput = '<ul><li><p>In the beginning, it was a nice day.</p></li></ul>';
+        await listItemsFormatBtn.trigger('click');
+        await wrapper.vm.$nextTick();
+
+        // Editor adds spaces and linebreaks for this. So remove them to compare
+        const editorHtmlOutput = editor.html().replaceAll(/[\n\r]/g, '').replaceAll(' ', '');
+        expect(editorHtmlOutput)
+          .toContain(expectedHtmlOutput.replaceAll(' ', ''));
+      });
+    });
+
+    describe('When add link button is clicked', () => {
+      // eslint-disable-next-line vitest/expect-expect
+      it('link input modal should be opened and closed when confirm button is closed', async () => {
+        await addLinkBtn.trigger('click');
+
+        linkInputEl = await wrapper.find('[data-qa="dt-wysiwyg-editor-link-input"]');
+
+        expect(linkInputEl.exists()).toBe(true);
+        expect(linkInputEl.isVisible()).toBe(true);
+
+        linkInputEl.value = 'https://www.youtube.com';
+
+        confirmAddLinkBtn = await wrapper.find('[data-qa="dt-wysiwyg-editor-set-link-confirm-btn"]');
+
+        await confirmAddLinkBtn.trigger('click');
+        await wrapper.vm.$nextTick();
+
+        expect(linkInputEl.isVisible()).toBe(false);
+      });
+
+      it('link input modal should be opened and closed when cancel button is closed', async () => {
+        await addLinkBtn.trigger('click');
+
+        linkInputEl = await wrapper.find('[data-qa="dt-wysiwyg-editor-link-input"]');
+
+        expect(linkInputEl.exists()).toBe(true);
+        expect(linkInputEl.isVisible()).toBe(true);
+
+        cancelAddLinkBtn = await wrapper.find('[data-qa="dt-wysiwyg-editor-set-link-cancel-btn"]');
+
+        await cancelAddLinkBtn.trigger('click');
+        await wrapper.vm.$nextTick();
+
+        expect(linkInputEl.isVisible()).toBe(false);
+      });
+    });
+  });
+});
---
+++ packages/dialtone-vue2/recipes/conversation_view/wysiwyg_editor/wysiwyg_editor_default.story.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <dt-recipe-wysiwyg-editor
+      ref="wysiwygEditor"
+      v-model="value"
+      class="d-mb32"
+      :input-aria-label="$attrs.inputAriaLabel"
+      :auto-focus="$attrs.autoFocus"
+      :editable="$attrs.editable"
+      :rounded-edges="$attrs.roundedEdges"
+      :input-class="$attrs.inputClass"
+      :link="$attrs.link"
+      :placeholder="$attrs.placeholder"
+      :max-height="$attrs.maxHeight"
+      :set-link-placeholder="$attrs.setLinkPlaceholder"
+      :set-link-modal-title="$attrs.setLinkModalTitle"
+      :cancel-set-link-button-label="$attrs.cancelSetLinkButtonLabel"
+      :confirm-set-link-button-label="$attrs.confirmSetLinkButtonLabel"
+      :show-bold-button="$attrs.showBoldButton"
+      :show-italics-button="$attrs.showItalicsButton"
+      :show-strike-button="$attrs.showStrikeButton"
+      :show-underline-button="$attrs.showUnderlineButton"
+      :show-list-items-button="$attrs.showListItemsButton"
+      :show-add-link-button="$attrs.showAddLinkButton"
+      @focus="$attrs.onFocus"
+      @blur="$attrs.onBlur"
+      @input="onInput($event)"
+    />
+    <p><strong>Editor content is:</strong></p>
+    <span>{{ value }}</span>
+  </div>
+</template>
+
+<script>
+import DtRecipeWysiwygEditor from './wysiwyg_editor.vue';
+
+export default {
+  name: 'DtRecipeWysiwygEditorDefault',
+  components: { DtRecipeWysiwygEditor },
+  data () {
+    return {
+      value: this.$attrs.value,
+    };
+  },
+
+  methods: {
+    onInput (updatedInput) {
+      this.value = updatedInput;
+    },
+  },
+};
+</script>
