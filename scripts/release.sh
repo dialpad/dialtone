bc7b65bfdba03e00935d5519e3f0396bd6f5b4d9
Julio Ortega <julio.ortega@dialpad.com>
2024-01-22 15:20:30 -0600

    fix logic

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,22 +7,29 @@ release_branch="${1:-production}"
 if [[ "$release_branch" == "production" && "$current_branch" != "staging" ]]; then
   echo "Releasing production can only be done while on staging branch"
   exit 1;
-else
+elif [[ "$release_branch" == "$current_branch" ]]; then
+  echo "Releasing $release_branch can't be done while on $current_branch branch"
+  echo "Make sure to checkout to your feature branch"
+  exit 1;
+fi
+
+if [[ "$release_branch" == "alpha" || "$release_branch" == "beta" ]]; then
   echo "Deleting local and remote $release_branch branch"
   git branch -d "$release_branch" || true
   git push origin --delete "$release_branch" || true
+
+  echo "Checking out to a clean $release_branch branch"
+  git checkout -b "$release_branch";
 fi
 
-echo "Updating branch"
-git pull;
+echo "Running build in parallel to improve performance"
+pnpm nx affected --verbose --target=build --parallel=5;
 
 echo "Running release-local on affected projects"
 pnpm nx affected --verbose --target=release-local --parallel=false;
 
 echo "Checking out to $release_branch"
-if [[ "$release_branch" == "alpha" || "$release_branch" == "beta" ]]; then
-  git checkout -b "$release_branch";
-else
+if [[ "$release_branch" == "production" && "$current_branch" != "staging" ]]; then
   git checkout "$release_branch";
 
   echo "Updating branch"
