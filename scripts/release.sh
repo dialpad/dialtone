e1735e194109cec6c427cb7d604b3a85b3249a2c
Julio Ortega <julio.ortega@dialpad.com>
2024-01-22 12:17:42 -0600

    add parameters to release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-branch="$(git rev-parse --abbrev-ref HEAD)"
+current_branch="$(git rev-parse --abbrev-ref HEAD)"
+release_branch="${1:-production}"
 
-if ! [[ "$branch" =~ ^staging$ ]]; then
-  echo "This script can only be run while on staging branch"
+if [[ "$release_branch" == "production" && "$current_branch" != "staging" ]]; then
+  echo "Releasing production can only be done while on staging branch"
   exit 1;
 fi
 
 git pull &&
 pnpm nx affected --target=release-local --parallel=false &&
-git checkout production &&
-git merge --ff-only staging &&
-git push -u origin production
+git checkout "$release_branch" &&
+git merge --ff-only "$current_branch" &&
+git push -u origin "$release_branch"
