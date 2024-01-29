fa26e31d5e787d8bd46ef87c386ec791391ec7ad
iropolo <ignacio.ropolo@dialpad.com>
2024-01-29 17:58:08 -0300

    fix: svg id fix

---
+++ packages/dialtone-vue2/components/icon/icon_svg_inject.vue
@@ -0,0 +1,35 @@
+<template>
+  <img
+    :src="`/node_modules/@dialpad/dialtone-icons/dist/svg/${name}.svg`"
+    class="injectable-svg d-icon"
+  >
+</template>
+
+<script>
+import SVGInject from '@iconfu/svg-inject';
+
+export default {
+  props: {
+    name: String,
+  },
+
+  watch: {
+    name (newValue, oldValue) {
+      if (newValue !== oldValue) {
+        console.log('name changed');
+        this.$nextTick(() => {
+          console.log('update svg on name change');
+          SVGInject(document.querySelectorAll('.injectable-svg'));
+        });
+      }
+    },
+  },
+
+  mounted () {
+    this.$nextTick(() => {
+      console.log('update svg on mount');
+      SVGInject(document.querySelectorAll('.injectable-svg'));
+    });
+  },
+};
+</script>
---
+++ packages/dialtone-vue2/components/icon/icon_svg_loader.vue
@@ -0,0 +1,36 @@
+<template>
+  <component
+    :is="svgComponent"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+  },
+
+  data () {
+    return {
+      svgComponent: null,
+    };
+  },
+
+  watch: {
+    name: {
+      immediate: true,
+      handler (value) {
+        this.loadSvgComponent(value);
+      },
+    },
+  },
+
+  methods: {
+    async loadSvgComponent (name) {
+      if (!name) return;
+      const module = await import(`../../../dialtone-vue2/node_modules/@dialpad/dialtone-icons/dist/svg/${name}.svg`);
+      this.svgComponent = module.default;
+    },
+  },
+};
+</script>
