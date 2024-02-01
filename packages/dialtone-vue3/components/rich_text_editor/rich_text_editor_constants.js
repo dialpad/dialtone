bc93fb8b4ebf2aa03ea5079dba0760d550194f69
Euge <emariotti3@users.noreply.github.com>
2024-02-01 14:52:57 -0300

    feat(wysiwyg-editor, rich-text-editor): add wysiwyg component for vue3 (#117)
    Co-authored-by: Eugenia Mariotti <eugenia.mariotti@dialpad.com>

--- packages/dialtone-vue3/components/rich_text_editor/rich_text_editor_constants.js
+++ packages/dialtone-vue3/components/rich_text_editor/rich_text_editor_constants.js
@@ -10,7 +10,17 @@ export const RICH_TEXT_EDITOR_AUTOFOCUS_TYPES = [
   'all',
 ];
 
+export const RICH_TEXT_EDITOR_SUPPORTED_LINK_PROTOCOLS = [
+  'https',
+  'http',
+  'ftp',
+  'ftps',
+  'sftp',
+  'mailto',
+];
+
 export default {
+  RICH_TEXT_EDITOR_SUPPORTED_LINK_PROTOCOLS,
   RICH_TEXT_EDITOR_OUTPUT_FORMATS,
   RICH_TEXT_EDITOR_AUTOFOCUS_TYPES,
 };
