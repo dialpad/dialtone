1212b3da67f34914caa77bbaa9a01e4fbea2941e
Julio Ortega <julio.ortega@dialpad.com>
2024-02-19 12:02:47 -0600

    add rem to sizing and spacing 0 values

--- packages/dialtone-tokens/dialtone-transforms.js
+++ packages/dialtone-tokens/dialtone-transforms.js
@@ -22,10 +22,10 @@ const ANDROID_WEIGHTS = {
   500: 'Medium',
   600: 'SemiBold',
   700: 'Bold',
-}
+};
 
 const pxToRemTransformer = (token, options) => {
-  if (token.value.toString().endsWith('%')) { return token.value };
+  if (token.value.toString().endsWith('%')) { return token.value; }
   const baseFont = (options && options.basePxFontSize) || 16;
   const floatVal = parseFloat(token.value);
 
@@ -34,23 +34,22 @@ const pxToRemTransformer = (token, options) => {
   }
 
   if (floatVal === 0) {
-    return '0';
+    return '0rem';
   }
 
   return `${floatVal / baseFont}rem`;
+};
+
+function throwSizeError (name, value, unitType) {
+  throw new Error(`Invalid Number: '${name}: ${value}' is not a valid number, cannot transform to '${unitType}' \n`);
 }
 
-function throwSizeError(name, value, unitType) {
-  throw `Invalid Number: '${name}: ${value}' is not a valid number, cannot transform to '${unitType}' \n`;
-}
-
-export function registerDialtoneTransforms(styleDictionary) {
-
+export function registerDialtoneTransforms (styleDictionary) {
   styleDictionary.registerTransform({
     name: 'dt/size/pxToRem',
     type: 'value',
-    matcher: function(token) {
-      return SIZE_IDENTIFIERS.includes(token.type)
+    matcher: function (token) {
+      return SIZE_IDENTIFIERS.includes(token.type);
     },
     transformer: pxToRemTransformer,
   });
@@ -58,7 +57,7 @@ export function registerDialtoneTransforms(styleDictionary) {
   styleDictionary.registerTransform({
     name: 'dt/space/pxToRem',
     type: 'value',
-    matcher: function(token) {
+    matcher: function (token) {
       return SPACING_IDENTIFIERS.includes(token.type);
     },
     transformer: pxToRemTransformer,
@@ -67,62 +66,60 @@ export function registerDialtoneTransforms(styleDictionary) {
   styleDictionary.registerTransform({
     name: 'dt/android/xml/color',
     type: 'value',
-    matcher: function(token) {
-      return ['color'].includes(token.type)
+    matcher: function (token) {
+      return ['color'].includes(token.type);
     },
     transformer: function (token) {
-      if (token.value === 'transparent') { return '#00ffffff'}
-      var str = Color(token.value).toHex8();
-      return '#' + str.slice(6) + str.slice(0,6);
-    }
+      if (token.value === 'transparent') { return '#00ffffff'; }
+      const str = Color(token.value).toHex8();
+      return '#' + str.slice(6) + str.slice(0, 6);
+    },
   });
 
   styleDictionary.registerTransform({
     name: 'dt/android/compose/color',
     type: 'value',
-    matcher: function(token) {
-      return ['color'].includes(token.type)
+    matcher: function (token) {
+      return ['color'].includes(token.type);
     },
-    transformer: (token, options) => {
+    transformer: (token) => {
       const hex8 = Color(token.value).toHex8();
       return `Color(0x${hex8.slice(6) + hex8.slice(0, 6)})`;
-    }
+    },
   });
 
   styleDictionary.registerTransform({
     name: 'dt/android/compose/fonts/transformToStack',
     type: 'value',
-    matcher: function(token) {
-      return FONT_FAMILY_IDENTIFIERS.includes(token.type)
+    matcher: function (token) {
+      return FONT_FAMILY_IDENTIFIERS.includes(token.type);
     },
-    transformer: (token, options) => {
+    transformer: (token) => {
       if (token.name === 'mono' || token.path.includes('code')) {
         return 'FontFamily.Monospace';
       }
       return 'FontFamily.Default';
-    }
+    },
   });
 
   styleDictionary.registerTransform({
     name: 'dt/android/compose/fonts/weight',
     type: 'value',
-    matcher: function(token) {
-      return WEIGHT_IDENTIFIERS.includes(token.type)
+    matcher: function (token) {
+      return WEIGHT_IDENTIFIERS.includes(token.type);
     },
-    transformer: (token, options) => {
-      return `FontWeight.${ANDROID_WEIGHTS[token.value]}`
-    }
+    transformer: (token) => {
+      return `FontWeight.${ANDROID_WEIGHTS[token.value]}`;
+    },
   });
-
 
   styleDictionary.registerTransform({
     name: 'dt/android/compose/size/pxToDp',
     type: 'value',
-    matcher: function(token) {
-      return [...SPACING_IDENTIFIERS, ...SIZE_IDENTIFIERS].includes(token.type)
+    matcher: function (token) {
+      return [...SPACING_IDENTIFIERS, ...SIZE_IDENTIFIERS].includes(token.type);
     },
-    transformer: (token, options) => {
-
+    transformer: (token) => {
       const floatVal = parseFloat(token.value);
 
       if (isNaN(floatVal)) {
@@ -130,17 +127,17 @@ export function registerDialtoneTransforms(styleDictionary) {
       }
 
       return `${floatVal}.dp`;
-    }
+    },
   });
 
-  //Sp is for font sizes only
+  // Sp is for font sizes only
   styleDictionary.registerTransform({
     name: 'dt/android/compose/size/pxToSp',
     type: 'value',
-    matcher: function(token) {
-      return [...FONT_SIZE_IDENTIFIERS].includes(token.type)
+    matcher: function (token) {
+      return [...FONT_SIZE_IDENTIFIERS].includes(token.type);
     },
-    transformer: (token, options) => {
+    transformer: (token) => {
       const floatVal = parseFloat(token.value);
 
       if (isNaN(floatVal)) {
@@ -148,17 +145,16 @@ export function registerDialtoneTransforms(styleDictionary) {
       }
 
       return `${floatVal}.sp`;
-    }
+    },
   });
-
 
   styleDictionary.registerTransform({
     name: 'dt/android/compose/lineHeight/percentToDecimal',
     type: 'value',
-    matcher: function(token) {
-      return LINE_HEIGHT_IDENTIFIERS.includes(token.type)
+    matcher: function (token) {
+      return LINE_HEIGHT_IDENTIFIERS.includes(token.type);
     },
-    transformer: (token, options) => {
+    transformer: (token) => {
       const floatVal = parseFloat(token.value);
 
       if (isNaN(floatVal)) {
@@ -170,17 +166,17 @@ export function registerDialtoneTransforms(styleDictionary) {
       }
 
       return `${floatVal / 100}.em`;
-    }
+    },
   });
 
-  //Sp is for font sizes only
+  // Sp is for font sizes only
   styleDictionary.registerTransform({
     name: 'dt/android/compose/opacity/percentToFloat',
     type: 'value',
-    matcher: function(token) {
-      return ['opacity'].includes(token.type)
+    matcher: function (token) {
+      return ['opacity'].includes(token.type);
     },
-    transformer: (token, options) => {
+    transformer: (token) => {
       const floatVal = parseFloat(token.value);
 
       if (isNaN(floatVal)) {
@@ -188,56 +184,56 @@ export function registerDialtoneTransforms(styleDictionary) {
       }
 
       return `${floatVal}F`;
-    }
+    },
   });
 
   styleDictionary.registerTransform({
     name: 'dt/stringify',
     type: 'value',
-    matcher: function(token) {
-      return ['type', 'textCase'].includes(token.type)
+    matcher: function (token) {
+      return ['type', 'textCase'].includes(token.type);
     },
-    transformer: (token, options) => {
+    transformer: (token) => {
       return `"${token.value}"`;
-    }
+    },
   });
 
   styleDictionary.registerTransform({
     name: 'dt/ios/fonts/transformToStack',
     type: 'value',
-    matcher: function(token) {
-      return FONT_FAMILY_IDENTIFIERS.includes(token.type)
+    matcher: function (token) {
+      return FONT_FAMILY_IDENTIFIERS.includes(token.type);
     },
-    transformer: (token, options) => {
+    transformer: (token) => {
       if (token.name === 'mono' || token.path.includes('code')) {
         return 'UIFont.monospacedSystemFont(ofSize: 15, weight: .regular)';
       }
       return 'UIFont.systemFont(ofSize: 15)';
-    }
+    },
   });
 
   styleDictionary.registerTransform({
     name: 'dt/ios/color',
     type: 'value',
-    matcher: function(token) {
-      return ['color'].includes(token.type)
+    matcher: function (token) {
+      return ['color'].includes(token.type);
     },
-    transformer: (token, options) => {
+    transformer: (token) => {
       const { r, g, b, a } = Color(token.value).toRgb();
       const rFixed = (r / 255.0).toFixed(3);
       const gFixed = (g / 255.0).toFixed(3);
       const bFixed = (b / 255.0).toFixed(3);
       return `UIColor(red: ${rFixed}, green: ${gFixed}, blue: ${bFixed}, alpha: ${a})`;
-    }
+    },
   });
 
   styleDictionary.registerTransform({
     name: 'dt/ios/size/pxToCGFloat',
     type: 'value',
-    matcher: function(token) {
-      return [...SPACING_IDENTIFIERS, ...SIZE_IDENTIFIERS].includes(token.type)
+    matcher: function (token) {
+      return [...SPACING_IDENTIFIERS, ...SIZE_IDENTIFIERS].includes(token.type);
     },
-    transformer: (token, options) => {
+    transformer: (token) => {
       const floatVal = parseFloat(token.value);
 
       if (isNaN(floatVal)) {
@@ -245,16 +241,16 @@ export function registerDialtoneTransforms(styleDictionary) {
       }
 
       return `CGFloat(${(floatVal).toFixed(2)})`;
-    }
+    },
   });
 
   styleDictionary.registerTransform({
     name: 'dt/ios/lineHeight/percentToDecimal',
     type: 'value',
-    matcher: function(token) {
-      return ['opacity', ...LINE_HEIGHT_IDENTIFIERS].includes(token.type)
+    matcher: function (token) {
+      return ['opacity', ...LINE_HEIGHT_IDENTIFIERS].includes(token.type);
     },
-    transformer: (token, options) => {
+    transformer: (token) => {
       const floatVal = parseFloat(token.value);
 
       if (isNaN(floatVal)) {
@@ -262,16 +258,16 @@ export function registerDialtoneTransforms(styleDictionary) {
       }
 
       return `CGFloat(${(floatVal).toFixed(2)})`;
-    }
+    },
   });
 
   styleDictionary.registerTransform({
     name: 'dt/lineHeight/percentToDecimal',
     type: 'value',
-    matcher: function(token) {
-      return LINE_HEIGHT_IDENTIFIERS.includes(token.type)
+    matcher: function (token) {
+      return LINE_HEIGHT_IDENTIFIERS.includes(token.type);
     },
-    transformer: (token, options) => {
+    transformer: (token) => {
       const floatVal = parseFloat(token.value);
 
       if (isNaN(floatVal)) {
@@ -283,33 +279,32 @@ export function registerDialtoneTransforms(styleDictionary) {
       }
 
       return `${floatVal / 100}`;
-    }
+    },
   });
 
   styleDictionary.registerTransform({
     name: 'dt/fonts/transformToStack',
     type: 'value',
-    matcher: function(token) {
-      return FONT_FAMILY_IDENTIFIERS.includes(token.type)
+    matcher: function (token) {
+      return FONT_FAMILY_IDENTIFIERS.includes(token.type);
     },
-    transformer: (token, options) => {
+    transformer: (token) => {
       if (token.name === 'body' || token.name === 'expressive') {
-        return `${token.value}, ${FALLBACK_FONTS.join(', ')}`
-      }
-      else if (token.name === 'mono') {
-        return `${token.value}, ${FALLBACK_FONTS_MONO.join(', ')}`
+        return `${token.value}, ${FALLBACK_FONTS.join(', ')}`;
+      } else if (token.name === 'mono') {
+        return `${token.value}, ${FALLBACK_FONTS_MONO.join(', ')}`;
       }
       return token.value;
-    }
+    },
   });
 
   styleDictionary.registerTransform({
     name: 'dt/lineHeight/percentToDecimal',
     type: 'value',
-    matcher: function(token) {
-      return LINE_HEIGHT_IDENTIFIERS.includes(token.type)
+    matcher: function (token) {
+      return LINE_HEIGHT_IDENTIFIERS.includes(token.type);
     },
-    transformer: (token, options) => {
+    transformer: (token) => {
       const floatVal = parseFloat(token.value);
 
       if (isNaN(floatVal)) {
@@ -321,6 +316,6 @@ export function registerDialtoneTransforms(styleDictionary) {
       }
 
       return `${floatVal / 100}`;
-    }
+    },
   });
 }
