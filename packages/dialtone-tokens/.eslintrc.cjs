3cb3581dcbd87ada48e76032094c38faf07a663a
Julio Ortega <julio.ortega@dialpad.com>
2024-02-22 14:01:17 -0600

    feat: transform Figma variables to sd format (#154)
    Co-authored-by: Yorbi Barriento <yorbi.barriento@dialpad.com>

---
+++ packages/dialtone-tokens/.eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint"
+  ],
+  extends: [
+    'standard',
+    'semistandard',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  env: {
+    browser: true,
+    node: true,
+    amd: true,
+  },
+  rules: {
+    camelcase: ['error', {
+      properties: 'never', // Ignore snake_case in JSON properties, which are often params.
+      allow: ['^opt_'], // Allow opt_varname arguments.
+    }],
+    'comma-dangle': ['error', 'always-multiline'],
+    'max-len': ['error', {
+      code: 120,
+      tabWidth: 2,
+      ignoreComments: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+    }],
+    'prefer-promise-reject-errors': ['error', {
+      allowEmptyReject: true,
+    }],
+    'max-lines': ['warn', { max: 300, skipBlankLines: true, skipComments: true }],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    complexity: ['warn', 5],
+  },
+};
