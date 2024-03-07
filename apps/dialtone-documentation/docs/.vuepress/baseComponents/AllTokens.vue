08a814b298ee1514b686f0a979ec60b26f3cb56e
Nina <ninamrepetto@gmail.com>
2024-03-07 11:24:33 -0300

    filter categories and add no results message

--- apps/dialtone-documentation/docs/.vuepress/baseComponents/AllTokens.vue
+++ apps/dialtone-documentation/docs/.vuepress/baseComponents/AllTokens.vue
@@ -2,7 +2,6 @@
   <div class="tokens-bar">
     <dt-input
       id="search-input"
-      ref="searchRef"
       v-model="searchInput"
       aria-label="Search tokens"
       placeholder="Search Tokens / Value / Keyword"
@@ -45,24 +44,36 @@
       @change="setTheme"
     />
   </div>
-  <div v-for="category in categories" :key="category">
+  <div v-for="category in Object.keys(filteredTokens)" :key="category">
     <slot :name="category">
       <!-- slot for anchor header -->
     </slot>
-    <token-table :category="category" :format="format" :theme="theme" :search-criteria="searchCriteria" />
+    <token-table :category="category" :tokens="filteredTokens[category]" />
+  </div>
+  <div
+    v-if="!hasSearchResults"
+    class="d-d-flex d-fl-center d-p16 d-gg4 d-fc-tertiary d-fs-300"
+  >
+    <span>No results found for</span>
+    <strong class="d-fw-semibold">
+      &OpenCurlyDoubleQuote;{{ searchCriteria }}&CloseCurlyDoubleQuote;
+    </strong>
   </div>
 </template>
 
 <script setup>
+import * as tokensJson from '@dialpad/dialtone-tokens/dist/doc.json';
+import { getComposedTypographyTokens, getComposedShadowTokens } from '../common/token-utilities';
 import TokenTable, { CATEGORY_MAP, FORMAT_MAP, THEMES } from '@baseComponents/TokenTable.vue';
-import { computed, ref } from 'vue';
+import { computed, ref, onBeforeMount } from 'vue';
 import { debounce } from '../common/utilities';
 
 const format = ref('CSS');
 const theme = ref('light');
-const categories = Object.keys(CATEGORY_MAP);
 const searchInput = ref(null);
 const searchCriteria = ref(null);
+const processedTokens = {}; // is set beforeMount and never changes
+const filteredTokens = ref({}); // same as processedTokens but filtered by format, theme and search
 
 const formatSelectMenuOptions = computed(() => {
   return Object.keys(FORMAT_MAP).map((item) => {
@@ -78,7 +89,25 @@ const setTheme = (newTheme) => {
   theme.value = newTheme;
 };
 
-const setSearchCriteria = () => (searchCriteria.value = searchInput.value);
+const setSearchCriteria = () => {
+  searchCriteria.value = searchInput.value;
+
+  if (!searchCriteria.value) {
+    filteredTokens.value = structuredClone(processedTokens[format.value][theme.value]);
+    return;
+  }
+  const newTokens = {};
+  const regex = new RegExp(searchCriteria.value, 'i');
+  Object.keys(CATEGORY_MAP).forEach((category) => {
+    const results = processedTokens[format.value][theme.value][category].filter((token) => {
+      if (regex.test(category)) return true;
+      const { name, tokenValue, description } = token;
+      return regex.test(name) || regex.test(tokenValue) || regex.test(description);
+    });
+    if (results.length) newTokens[category] = results;
+  });
+  filteredTokens.value = structuredClone(newTokens);
+};
 
 const searchToken = () => {
   debounce(() => setSearchCriteria());
@@ -90,6 +119,95 @@ const resetSearch = () => {
 };
 
 const isSearchEmpty = computed(() => !searchInput.value || searchInput.value.trim().length === 0);
+
+const hasSearchResults = computed(() => Object.keys(filteredTokens.value).length > 0);
+
+/*
+* Before mount process the file tokensJson and fill processedTokens with the data we want to show.
+* This data is static, and contains the tokens names, values, description, exampleValue,
+* exampleName for the tokens in every format, theme and category.
+*
+* processedTokens: {
+*    CSS: {
+*      light: {
+*        color: [
+*          {
+*            name: ...,
+*            tokenValue: ...,
+*            description: ...,
+*            exampleValue: ...,
+*            exampleName: ...,
+*          },
+*          ...
+*        ],
+*        typography: [...],
+*        shadow: [...],
+*        size: [...],
+*        space: [...],
+*        component: [...]
+*       },
+*       dark: {...},
+*    },
+*    Android: {
+*      light: {...},
+*      dark: {...},
+*    },
+*    iOS: {
+*      light: {...},
+*      dark: {...},
+*    },
+*  },
+*/
+onBeforeMount(() => {
+  addTokens();
+  addComposedTokens();
+  filteredTokens.value = structuredClone(processedTokens[format.value][theme.value]);
+});
+
+const addTokens = () => {
+  Object.keys(FORMAT_MAP).forEach(format => {
+    for (const theme of THEMES) {
+      Object.keys(CATEGORY_MAP).forEach(category => {
+        const tokens = [];
+        Object.entries(tokensJson[theme.value])
+          .filter(([key, value]) => CATEGORY_MAP[category].includes(key.split('/')[0]) && value[FORMAT_MAP.CSS])
+          .forEach(([_, value]) => {
+            const { name, value: tokenValue, description } = value[FORMAT_MAP[format]] || {};
+            // exclude base tokens
+            if (name && !isBaseToken(name)) {
+              const { value: exampleValue, name: exampleName } = value[FORMAT_MAP.CSS];
+              tokens.push({ exampleValue, exampleName, name, tokenValue, description });
+            }
+          });
+        if (!processedTokens[format]) processedTokens[format] = {};
+        if (!processedTokens[format][theme.value]) processedTokens[format][theme.value] = {};
+        processedTokens[format][theme.value][category] = [...tokens];
+      });
+    }
+  });
+};
+
+/*
+* Tokens that are a combination of other tokens.
+* Only apply for the categories typography and shadow, and only in CSS format.
+*/
+const addComposedTokens = () => {
+  const composedTypographyTokens = getComposedTypographyTokens();
+  THEMES.forEach((themeObj) => {
+    const theme = themeObj.value;
+    const composedShadowTokens = getComposedShadowTokens(theme);
+    processedTokens.CSS[theme].typography = [
+      ...composedTypographyTokens,
+      ...processedTokens.CSS[theme].typography,
+    ];
+    processedTokens.CSS[theme].shadow = [
+      ...composedShadowTokens,
+      ...processedTokens.CSS[theme].shadow,
+    ];
+  });
+};
+
+const isBaseToken = (name) => name.endsWith('base)') || name.endsWith('root)');
 </script>
 
 <style scoped>
--- apps/dialtone-documentation/docs/.vuepress/baseComponents/TokenTable.vue
+++ apps/dialtone-documentation/docs/.vuepress/baseComponents/TokenTable.vue
@@ -30,7 +30,7 @@
     </thead>
     <tbody>
       <tr
-        v-for="({ exampleValue, exampleName, name, tokenValue, description }) in filteredTokens"
+        v-for="({ exampleValue, exampleName, name, tokenValue, description }) in tokens"
         :key="name"
       >
         <td>
@@ -77,8 +77,6 @@
 </template>
 
 <script>
-import * as tokensJson from '@dialpad/dialtone-tokens/dist/doc.json';
-import { getComposedTypographyTokens, getComposedShadowTokens } from '../common/token-utilities';
 import CopyButton from './CopyButton.vue';
 import TokenExample from './TokenExample.vue';
 
@@ -111,27 +109,15 @@ export default {
   },
 
   props: {
+    tokens: {
+      type: Array,
+      default: () => [],
+    },
+
     category: {
       type: String,
       default: 'color',
       validator: (v) => Object.keys(CATEGORY_MAP).includes(v),
-    },
-
-    format: {
-      type: String,
-      default: 'CSS',
-      validator: (v) => Object.keys(FORMAT_MAP).includes(v),
-    },
-
-    searchCriteria: {
-      type: String,
-      default: null,
-    },
-
-    theme: {
-      type: String,
-      default: 'light',
-      validator: (v) => THEMES.find(theme => theme.value === v),
     },
 
     tokenList: {
@@ -140,109 +126,10 @@ export default {
     },
   },
 
-  data () {
-    return {
-      processedTokens: {
-        CSS: {
-          light: [],
-          dark: [],
-        },
-
-        Android: {
-          light: [],
-          dark: [],
-        },
-
-        iOS: {
-          light: [],
-          dark: [],
-        },
-      },
-    };
-  },
-
-  computed: {
-    tokensProcessed () {
-      return this.processedTokens[this.format][this.theme];
-    },
-
-    /**
-     * Search tokens by name, value, description or category name
-    */
-    filteredTokens () {
-      if (!this.searchCriteria) return this.tokensProcessed;
-      const regex = new RegExp(this.searchCriteria, 'i');
-      return this.tokensProcessed.filter((token) => {
-        if (regex.test(this.category)) return true;
-        const { name, tokenValue, description } = token;
-        return regex.test(name) || regex.test(tokenValue) || regex.test(description);
-      });
-    },
-
-    formatSelectMenuOptions () {
-      return Object.keys(FORMAT_MAP).map((item) => {
-        return { value: item, label: item };
-      });
-    },
-  },
-
-  beforeMount () {
-    this.addTokens();
-    this.addComposedTokens();
-  },
-
   methods: {
     remToPixels (value) {
       if (this.category !== 'size' && this.category !== 'space') return;
       return `${parseFloat(value.replace('rem', '')) * 10}px`;
-    },
-
-    isBaseToken (name) {
-      return name.endsWith('base)') || name.endsWith('root)');
-    },
-
-    tokenInList (name) {
-      return this.tokenList && this.tokenList[name];
-    },
-
-    addTokens () {
-      for (const format in this.processedTokens) {
-        for (const theme of THEMES) {
-          const tokens = [];
-          Object.entries(tokensJson[theme.value])
-            .filter(([key, value]) => CATEGORY_MAP[this.category].includes(key.split('/')[0]) &&
-          value[FORMAT_MAP.CSS] &&
-          (!this.tokenList || this.tokenList[value[FORMAT_MAP.CSS].name]))
-            .forEach(([_, value]) => {
-              const { name, value: tokenValue, description } = value[FORMAT_MAP[format]] || {};
-              const tokenDescription = this.tokenInList(name) ? this.tokenList[name].description : description;
-              // exclude base tokens
-              if (name && !this.isBaseToken(name)) {
-                const { value: exampleValue, name: exampleName } = value[FORMAT_MAP.CSS];
-                tokens.push({ exampleValue, exampleName, name, tokenValue, description: tokenDescription });
-              }
-            });
-          this.processedTokens[format][theme.value] = [...tokens];
-        }
-      }
-    },
-
-    addComposedTokens () {
-      if (this.category === 'typography') {
-        for (const theme of THEMES) {
-          this.processedTokens.CSS[theme.value] = [
-            ...getComposedTypographyTokens(),
-            ...this.processedTokens.CSS[theme.value],
-          ];
-        }
-      } else if (this.category === 'shadow') {
-        for (const theme of THEMES) {
-          this.processedTokens.CSS[theme.value] = [
-            ...getComposedShadowTokens(theme.value),
-            ...this.processedTokens.CSS[theme.value],
-          ];
-        }
-      }
     },
   },
 };
