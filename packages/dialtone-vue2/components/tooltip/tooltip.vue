878e03b58257468757a90091202831c00d1630c7
Brad Paugh <brad.paugh@dialpad.com>
2024-03-13 13:56:23 -0700

    fix(tooltip): appendTo initialization (#204)

--- packages/dialtone-vue2/components/tooltip/tooltip.vue
+++ packages/dialtone-vue2/components/tooltip/tooltip.vue
@@ -485,6 +485,7 @@ export default {
         this.tip.setProps({
           ...this.tippyProps,
           zIndex: this.calculateAnchorZindex(),
+          appendTo: this.appendTo === 'body' ? this.anchor?.getRootNode()?.querySelector('body') : this.appendTo,
         });
       }
     },
@@ -497,7 +498,6 @@ export default {
       return {
         contentElement: this.$refs.content.$el,
         allowHTML: true,
-        appendTo: this.appendTo === 'body' ? this.anchor?.getRootNode()?.querySelector('body') : this.appendTo,
         zIndex: this.calculateAnchorZindex(),
         onMount: this.onMount,
         ...this.tippyProps,
