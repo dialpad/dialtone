2da1f289ce516256d13b76b47c644af4f1f2e089
Julio Ortega <julio.ortega@dialpad.com>
2024-04-01 19:20:46 -0600

    remove includes

--- packages/dialtone-vue2/percy.config.cjs
+++ packages/dialtone-vue2/percy.config.cjs
@@ -3,11 +3,6 @@ const baseConfig = require('../../percy.base_config.cjs');
 module.exports = {
   ...baseConfig,
   storybook: {
-    include: [
-      'Components/Avatar: Variants',
-      'Components/Icon: Variants',
-      'Components/Modal: Default',
-    ],
     exclude: [
       ...baseConfig.storybook.exclude
       // Add specific dialtone-vue 2 stories to exclude
