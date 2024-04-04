62d73c81b2ecffe72135e2083e04e10a6ddbe82d
Julio Ortega <julio.ortega@dialpad.com>
2024-04-03 18:43:55 -0600

    release icons and emoji on every change type

--- packages/dialtone-icons/release-local.config.cjs
+++ packages/dialtone-icons/release-local.config.cjs
@@ -16,7 +16,13 @@ module.exports = {
     ['@semantic-release/commit-analyzer', {
       preset: 'angular',
       releaseRules: [
-        { type: 'refactor', release: 'patch' },
+        {type: 'build', release: 'patch'},
+        {type: 'chore', release: 'patch'},
+        {type: 'ci', release: 'patch'},
+        {type: 'docs', release: 'patch'},
+        {type: 'refactor', release: 'patch'},
+        {type: 'style', release: 'patch'},
+        {type: 'test', release: 'patch'},
       ],
     }],
     ['@semantic-release/release-notes-generator', {
