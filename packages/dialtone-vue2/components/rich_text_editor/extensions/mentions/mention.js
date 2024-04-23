3f7c9d25191a69c1359496c6dd49ceb157094ba2
Julio Ortega <julio.ortega@dialpad.com>
2024-04-23 17:17:11 -0600

    parse mention on paste

--- packages/dialtone-vue2/components/rich_text_editor/extensions/mentions/mention.js
+++ packages/dialtone-vue2/components/rich_text_editor/extensions/mentions/mention.js
@@ -1,11 +1,31 @@
 import Mention from '@tiptap/extension-mention';
+import { mergeAttributes, nodePasteRule } from '@tiptap/core';
 import { VueNodeViewRenderer } from '@tiptap/vue-2';
 
 // Mention component
 import MentionComponent from './MentionComponent.vue';
-import { mergeAttributes } from '@tiptap/core';
 
+const mentionPasteMatch = (text, suggestions) => {
+  const matches = [...text.matchAll(/@([\w.-]+)/g)];
+
+  return matches
+    .filter(match => suggestions.some(({ id }) => id === match[1].trim()))
+    .map(match => {
+      let text = match[1];
+      if (!text.endsWith(' ')) text += ' ';
+      return {
+        index: match.index,
+        text,
+        match,
+      };
+    });
+};
+
+// Always the Padawan, never the Jedi. @2
 export const MentionPlugin = Mention.extend({
+  name: 'mention',
+  group: 'inline',
+  inline: true,
 
   addNodeView () {
     return VueNodeViewRenderer(MentionComponent);
@@ -38,6 +58,19 @@ export const MentionPlugin = Mention.extend({
   },
 
   renderHTML ({ HTMLAttributes }) {
-    return ['mention-component', mergeAttributes(this.options.HTMLAttributes, HTMLAttributes), 0];
+    return ['mention-component', mergeAttributes(this.options.HTMLAttributes, HTMLAttributes)];
+  },
+
+  addPasteRules () {
+    const suggestions = this.options.suggestion?.items({ query: '' });
+    return [
+      nodePasteRule({
+        find: (text) => mentionPasteMatch(text, suggestions),
+        type: this.type,
+        getAttributes (attrs) {
+          return suggestions.find(({ id }) => id === attrs[0].trim());
+        },
+      }),
+    ];
   },
 });
