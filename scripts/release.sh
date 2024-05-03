786f3cd1829b75be83210fe2ce1bc61d2e2d9f5f
Julio Ortega <julio.ortega@dialpad.com>
2024-05-03 17:22:18 -0600

    refactor: scape quotes

--- scripts/release.sh
+++ scripts/release.sh
@@ -24,9 +24,9 @@ git merge --ff-only "$current_branch";
 
 echo "Running release-local on affected projects";
 if [[ "$release_branch" == "alpha" || "$release_branch" == "beta" ]]; then
-  pnpm nx affected --target=release-local --base=staging --parallel=false;
+  pnpm nx release-local --base=staging dialtone;
 else
-  pnpm nx affected --target=release-local --base=production --parallel=false;
+  pnpm nx release-local --base=production dialtone;
 fi
 
 echo "Pushing changes to $release_branch";
