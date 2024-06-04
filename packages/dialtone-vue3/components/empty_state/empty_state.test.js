bd41d50508f9e2a0f7799c25c05a0d72d2ebd0a6
Ignacio Ropolo <89984179+iropolo@users.noreply.github.com>
2024-06-04 15:09:03 -0300

    fix: NO-JIRA empty state update (#348)
    Co-authored-by: iropolo <ignacio.ropolo@dialpad.com>

--- packages/dialtone-vue3/components/empty_state/empty_state.test.js
+++ packages/dialtone-vue3/components/empty_state/empty_state.test.js
@@ -65,7 +65,7 @@ describe('DtIllustration Tests', () => {
     });
 
     it('Should render size classes in wrapper', () => {
-      expect(wrapper.classes().includes('d-empty-state', 'd-empty-state--size--lg')).toBe(true);
+      expect(wrapper.classes().includes('d-empty-state', 'd-empty-state--size-lg')).toBe(true);
     });
 
     it('Should render correct headline classes in header text', () => {
@@ -82,7 +82,7 @@ describe('DtIllustration Tests', () => {
       it('Should update size classes in wrapper', () => {
         wrapper.setProps({ size: 'md' });
 
-        expect(wrapper.classes().includes('d-empty-state', 'd-empty-state--size--md')).toBe(true);
+        expect(wrapper.classes().includes('d-empty-state', 'd-empty-state--size-md')).toBe(true);
       });
 
       it('Should update headline classes in header text', () => {
