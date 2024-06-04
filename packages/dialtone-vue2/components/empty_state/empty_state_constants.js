4c5e1ddb6bca5b13b936383f1ede621531751063
iropolo <ignacio.ropolo@dialpad.com>
2024-06-04 14:33:44 -0300

    fix: no-jira empty state update

--- packages/dialtone-vue2/components/empty_state/empty_state_constants.js
+++ packages/dialtone-vue2/components/empty_state/empty_state_constants.js
@@ -1,7 +1,7 @@
 export const EMPTY_STATE_SIZE_MODIFIERS = {
-  sm: 'd-empty-state--size--sm',
-  md: 'd-empty-state--size--md',
-  lg: 'd-empty-state--size--lg',
+  sm: 'd-empty-state--size-sm',
+  md: 'd-empty-state--size-md',
+  lg: 'd-empty-state--size-lg',
 };
 
 export default {
