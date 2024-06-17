c6ae17ea1057c5be49ab7fcf962e0ea34db831cb
Julio Ortega <julio.ortega@dialpad.com>
2024-06-17 12:16:19 -0600

    fix section header

--- packages/dialtone-vue3/prototypes/dialpad/leftbar/components/leftbar_section.vue
+++ packages/dialtone-vue3/prototypes/dialpad/leftbar/components/leftbar_section.vue
@@ -1,22 +1,64 @@
 <template>
   <dt-collapsible
-    :anchor-text="title"
-    anchor-class="d-bar-pill d-w100p d-truncate d-lh12 d-headline--sm d-fc-tertiary d-us-none"
+    class="leftbar-section"
+    :open="isOpen"
   >
+    <template #anchor="{ attrs }">
+      <div class="leftbar-section-header__wrapper">
+        <dt-button
+          v-bind="attrs"
+          class="d-bar-pill d-w100p d-lh12 d-headline--sm d-fc-tertiary d-us-none"
+          label-class="d-jc-flex-start d-w100p"
+          importance="clear"
+          kind="muted"
+          @click="defaultToggleOpen"
+        >
+          <dt-icon
+            :name=" isOpen ? 'chevron-down' : 'chevron-right'"
+            size="300"
+          />
+          <span class="d-truncate d-w128 d-ta-left">{{ title }}</span>
+        </dt-button>
+        <div class="leftbar-section-header__action">
+          <slot name="action" />
+        </div>
+      </div>
+    </template>
     <template #content>
       <slot name="items" />
-    </template>
-    <template #action>
-      <slot name="action" />
     </template>
   </dt-collapsible>
 </template>
 
 <script setup>
 import { DtCollapsible } from '@/components/collapsible';
-import { defineProps } from 'vue';
+import { DtButton } from '@/components/button';
+import { DtIcon } from '@/components/icon';
+import { defineProps, ref } from 'vue';
 
-defineProps({
+const props = defineProps({
   title: { type: String, required: true },
+  open: { type: Boolean, default: false },
 });
+
+const isOpen = ref(props.open);
+
+const defaultToggleOpen = () => {
+  isOpen.value = !isOpen.value;
+};
 </script>
+
+<style lang="less" scoped>
+.leftbar-section-header {
+  &__wrapper {
+    position: relative;
+  }
+
+  &__action {
+    position: absolute;
+    right: var(--dt-size-300);
+    top: 50%;
+    transform: translateY(-50%);
+  }
+}
+</style>
