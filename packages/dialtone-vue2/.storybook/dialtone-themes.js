6536125f9a0071a0b4f06ba76b6e5433756f8fc1
Nina <ninamrepetto@gmail.com>
2024-06-24 18:04:31 -0300

    docs: DLT-1764 update to new Dialpad logo in doc site header (#377)

--- packages/dialtone-vue2/.storybook/dialtone-themes.js
+++ packages/dialtone-vue2/.storybook/dialtone-themes.js
@@ -7,13 +7,13 @@ import { create } from '@storybook/theming/create';
 const _baseThemeVariables = {
   brandTitle: 'Dialpad storybook',
   brandUrl: 'https://dialtone.dialpad.com',
-  brandImage: 'https://static.dialpadcdn.com/dialtone/dialpad_logo.svg',
   fontBase: '-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif',
 }
 
 export const dialtoneDarkTheme = create({
   base: 'dark',
   ..._baseThemeVariables,
+  brandImage: 'https://static.dialpadcdn.com/dialtone/dialpad-logo-white.svg',
   appBg: '#1B1B1B', // --dt-color-surface-secondary
   appContentBg: '#080808', // --dt-color-surface-primary
 
@@ -27,6 +27,7 @@ export const dialtoneDarkTheme = create({
 export const dialtoneLightTheme = create({
   base: 'light',
   ..._baseThemeVariables,
+  brandImage: 'https://static.dialpadcdn.com/dialtone/dialpad-logo-black.svg',
   appBg: '#F9F9F9', // --dt-color-surface-secondary
   appContentBg: '#FFFFFF', // --dt-color-surface-primary
 
