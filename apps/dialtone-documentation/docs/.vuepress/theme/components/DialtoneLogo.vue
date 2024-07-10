8e020ff36cb6fd5aebf819b1d294618b95676b96
Joshua Hynes <jhynes@dialpad.com>
2024-07-09 22:25:15 -0400

    Update logo in navbar to say Dialtone instead of Dialpad

--- apps/dialtone-documentation/docs/.vuepress/theme/components/DialtoneLogo.vue
+++ apps/dialtone-documentation/docs/.vuepress/theme/components/DialtoneLogo.vue
@@ -1,10 +1,17 @@
 <template>
   <router-link
-    class="d-btn h:d-bgc-transparent d-py6 d-w216 d-jc-flex-start"
+    class="d-btn d-btn--muted h:d-bgc-transparent d-py6 d-px12 d-w216 d-jc-flex-start"
     title="Go back to the homepage"
     to="/"
   >
-    <svg-loader :illustration="true" name="dialpad-logo" />
+    <dt-stack gap="450" direction="row">
+      <dt-icon
+        name="dialpad-ai-color-reversed"
+        size="500"
+      />
+      <span class="d-fs-300 d-fw-bold">Dialtone</span>
+      <!-- <svg-loader :illustration="true" name="dialpad-logo" /> -->
+    </dt-stack>
   </router-link>
 </template>
 
