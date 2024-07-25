bab8f168774a6018482152137e9c55aece9dd073
Brad Paugh <brad.paugh@dialpad.com>
2024-07-25 09:34:27 -0700

    add postcss on correct branch

--- packages/dialtone-tokens/build.js
+++ packages/dialtone-tokens/build.js
@@ -3,7 +3,32 @@
  */
 
 import { writeDocs } from './build-docs.js';
-import { run as runSdTranforms } from './build-sd-transforms.js';
+import { run as runSdTransforms } from './build-sd-transforms.js';
+import postcss from 'postcss';
+import fs from 'fs';
+import dialtoneTokensPlugin from './postcss/dialtone-tokens.cjs';
+import path from 'path';
+const TOKENS_OUTPUT_DIR = './dist/css/';
 
-await runSdTranforms();
+await runSdTransforms();
+
+// Run postcss rather than via command line since we need to run it before writeDocs but after runSdTransforms.
+await runPostCss(TOKENS_OUTPUT_DIR);
 writeDocs();
+
+/**
+ * Runs postcss on all the files in the tokens output directory with the dialtone-tokens plugin.
+ */
+async function runPostCss (tokensOutputDir) {
+  const postCss = postcss([dialtoneTokensPlugin]);
+  const files = fs.readdirSync(tokensOutputDir);
+  for (const file of files) {
+    const css = fs.readFileSync(path.join(tokensOutputDir, file));
+    const result = await postCss
+      .process(css, { from: path.join(tokensOutputDir, file), to: path.join(tokensOutputDir, file) });
+    fs.writeFile(path.join(tokensOutputDir, file), result.css, () => true);
+    if (result.map) {
+      fs.writeFile(path.join(tokensOutputDir, file), result.map.toString(), () => true);
+    }
+  }
+}
