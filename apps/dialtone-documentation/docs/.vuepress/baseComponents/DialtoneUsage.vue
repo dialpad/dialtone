c9edccd4bd3db2cb94c2a6ecf73707f2f7da3f26
Francis Rupert <francisrupert@gmail.com>
2024-08-01 09:21:45 -0500

    do dont title spacing

--- apps/dialtone-documentation/docs/.vuepress/baseComponents/DialtoneUsage.vue
+++ apps/dialtone-documentation/docs/.vuepress/baseComponents/DialtoneUsage.vue
@@ -3,7 +3,9 @@
     <div class="dialtone-usage__item dialtone-usage__item--do">
       <h3 class="dialtone-usage__hd dialtone-usage__hd--do">
         <dt-icon name="check" size="200" />
-        Do
+        <div class="dialtone-usage__label">
+          Do
+        </div>
       </h3>
       <div class="dialtone-usage__bd">
         <slot name="do" />
@@ -12,7 +14,9 @@
     <div class="dialtone-usage__item dialtone-usage__item--dont">
       <h3 class="dialtone-usage__hd dialtone-usage__hd--dont">
         <dt-icon name="close" size="200" />
-        Don’t
+        <div class="dialtone-usage__label">
+          Don’t
+        </div>
       </h3>
       <div class="dialtone-usage__bd">
         <slot name="dont" />
