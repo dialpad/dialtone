317679a52b11faa617ef5332d0201d5ff50ecec4
Francis Rupert <francis.rupert@dialpad.com>
2024-08-19 13:01:32 -0500

    docs(split-button): DLT-1821 split button usage (#458)
    Co-authored-by: Nina <ninamrepetto@gmail.com>

--- apps/dialtone-documentation/docs/.vuepress/theme/components/DialtoneLogo.vue
+++ apps/dialtone-documentation/docs/.vuepress/theme/components/DialtoneLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link
-    class="d-pl8 d-w216 d-fc-secondary d-td-none"
+    class="d-pl8 d-w216 d-fc-secondary d-td-none h:d-fc-primary"
     title="Go back to the homepage"
     to="/"
   >
@@ -9,7 +9,7 @@
         name="dialpad-ai-color-reversed"
         size="500"
       />
-      <span class="d-fs-300 d-fw-bold">Dialtone</span>
+      <span class="d-headline--lg">Dialtone</span>
     </dt-stack>
   </router-link>
 </template>
