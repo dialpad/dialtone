7a5f9449de141e9b569425b7e969c59359b2592e
Julio Ortega <julio.ortega@dialpad.com>
2024-08-19 19:24:24 -0600

    add types to themes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import path, { resolve } from 'path';
 import { fileURLToPath } from 'url';
+import dts from 'vite-plugin-dts';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,18 +15,30 @@ export default defineConfig({
         'themes/config': resolve(__dirname, './common/themes/config.js'),
         'themes/dp-light': resolve(__dirname, './common/themes/dp-light.js'),
         'themes/dp-dark': resolve(__dirname, './common/themes/dp-dark.js'),
-        'themes/expressive-light': resolve(__dirname, './common/themes/expressive-light.js'),
-        'themes/expressive-dark': resolve(__dirname, './common/themes/expressive-dark.js'),
+        'themes/expressive-light': resolve(
+          __dirname,
+          './common/themes/expressive-light.js',
+        ),
+        'themes/expressive-dark': resolve(
+          __dirname,
+          './common/themes/expressive-dark.js',
+        ),
         'themes/tmo-light': resolve(__dirname, './common/themes/tmo-light.js'),
         'themes/tmo-dark': resolve(__dirname, './common/themes/tmo-dark.js'),
-        'themes/expressive-sm-light': resolve(__dirname, './common/themes/expressive-sm-light.js'),
-        'themes/expressive-sm-dark': resolve(__dirname, './common/themes/expressive-sm-dark.js'),
+        'themes/expressive-sm-light': resolve(
+          __dirname,
+          './common/themes/expressive-sm-light.js',
+        ),
+        'themes/expressive-sm-dark': resolve(
+          __dirname,
+          './common/themes/expressive-sm-dark.js',
+        ),
       },
       formats: ['es', 'cjs'],
     },
     rollupOptions: {
       output: {
-        chunkFileNames: () => 'chunks/[name]-[hash].js',
+        chunkFileNames: () => 'themes/chunks/[name]-[hash].js',
       },
     },
     minify: true,
@@ -35,4 +48,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('.', import.meta.url)),
     },
   },
+  plugins: [dts({ outDir: 'dist/themes/types' })],
 });
