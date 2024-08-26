7b07aef1a0ede8840de69db71b10dbd878ac2599
Julio Ortega <julio.ortega@dialpad.com>
2024-08-26 13:43:11 -0600

    update lint-staged to include .eslintignore

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,7 +1,23 @@
+const { ESLint } = require('eslint');
+
+const removeIgnoredFiles = async (files) => {
+  const eslint = new ESLint();
+  const isIgnored = await Promise.all(
+    files.map((file) => {
+      return eslint.isPathIgnored(file);
+    }),
+  );
+  const filteredFiles = files.filter((_, i) => !isIgnored[i]);
+  return filteredFiles.join(' ');
+};
+
 module.exports = {
-  '*.{js, mjs, cjs, vue}': [
-    'pnpm exec eslint --fix --max-warnings=0 --no-warn-ignored',
-  ],
+  '**/*.{js, mjs, cjs, vue}': async (files) => {
+    const filesToLint = await removeIgnoredFiles(files);
+    return [
+      `pnpm exec eslint --fix --max-warnings=0 ${filesToLint}`,
+    ];
+  },
   '*.less': [
     'pnpm exec stylelint --fix',
   ],
