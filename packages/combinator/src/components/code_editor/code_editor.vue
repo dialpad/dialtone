3c7418274cd182211fa35cfe33415035f10f2fd0
Julio Ortega <julio.ortega@dialpad.com>
2024-09-17 16:58:32 -0600

    migrate to vite
    Signed-off-by: Julio Ortega <julio.ortega@dialpad.com>

--- packages/combinator/src/components/code_editor/code_editor.vue
+++ packages/combinator/src/components/code_editor/code_editor.vue
@@ -75,6 +75,9 @@ import DtcCodeEditorTagAttributes from './code_editor_tag_attributes.vue';
 import DtcCodeEditorElement from './code_editor_element.vue';
 import DtcCodeEditorSlot from './code_editor_slot.vue';
 import { DtIconCopy } from '@dialpad/dialtone-icons/vue3';
+import DtcCodeEditorTagAttributes from './code_editor_tag_attributes.vue';
+import DtcCodeEditorElement from './code_editor_element.vue';
+import DtcCodeEditorSlot from './code_editor_slot.vue';
 import { DtButton, DtPopover } from '@dialpad/dialtone-vue';
 
 import { OPTIONS_UPDATE_EVENT, SETTINGS_INDENT_KEY } from '@/src/lib/constants';
