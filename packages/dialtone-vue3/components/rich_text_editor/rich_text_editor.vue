4a61ec9edafa946ed477fb6d197c14e5b0d5cb77
Brad Paugh <brad.paugh@dialpad.com>
2024-10-04 14:12:19 -0700

    vue 3

--- packages/dialtone-vue3/components/rich_text_editor/rich_text_editor.vue
+++ packages/dialtone-vue3/components/rich_text_editor/rich_text_editor.vue
@@ -507,7 +507,7 @@ export default {
 
   mounted () {
     warnIfUnmounted(this.$el, this.$options.name);
-    this.processValue(this.modelValue);
+    this.processValue(this.modelValue, false);
   },
 
   methods: {
@@ -568,13 +568,13 @@ export default {
       this.addEditorListeners();
     },
 
-    processValue (newValue) {
+    processValue (newValue, returnIfEqual = true) {
       let currentValue = this.getOutput();
       if (this.outputFormat === 'json') {
         newValue = JSON.stringify(newValue);
         currentValue = JSON.stringify(currentValue);
       }
-      if (newValue === currentValue) {
+      if (returnIfEqual && newValue === currentValue) {
         // The new value came from this component and was passed back down
         // through the parent, so don't do anything here.
         return;
