511b1357580f2d8ed0116e58aaea8cf99459233d
Nina Repetto <ninamrepetto@gmail.com>
2024-10-08 18:02:04 -0300

    docs: DLT-1797 move SB to docs - Popover to Root Layout (#521)

--- apps/dialtone-documentation/docs/.vuepress/exampleComponents/ExamplePopover.vue
+++ apps/dialtone-documentation/docs/.vuepress/exampleComponents/ExamplePopover.vue
@@ -2,13 +2,14 @@
   <dt-popover
     :modal="modal"
     max-width="264px"
-    placement="bottom"
+    :placement="placement"
+    :fallback-placements="fallbackPlacements"
   >
     <template #anchor="{ attrs }">
       <dt-button
         v-bind="attrs"
       >
-        View Popover
+        {{ buttonText }}
       </dt-button>
     </template>
     <template
@@ -58,6 +59,21 @@ export default {
     footer: {
       type: Boolean,
       default: false,
+    },
+
+    placement: {
+      type: String,
+      default: 'bottom',
+    },
+
+    fallbackPlacements: {
+      type: Array,
+      default: () => ['auto'],
+    },
+
+    buttonText: {
+      type: String,
+      default: 'View Popover',
     },
   },
 };
