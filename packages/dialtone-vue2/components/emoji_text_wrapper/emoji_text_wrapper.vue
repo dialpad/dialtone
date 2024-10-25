3adaaae678c8eec38bc9f5a43b71173a8f6d97bc
Nina <ninamrepetto@gmail.com>
2024-10-25 17:02:31 -0300

    fix(emoji-text-wrapper): DLT-2176 invalid regular expression error

--- packages/dialtone-vue2/components/emoji_text_wrapper/emoji_text_wrapper.vue
+++ packages/dialtone-vue2/components/emoji_text_wrapper/emoji_text_wrapper.vue
@@ -1,7 +1,8 @@
 <script>
 import { DtEmoji } from '../emoji';
-import { findEmojis, findShortCodes } from '@/common/emoji';
+import { findShortCodes } from '@/common/emoji';
 import { ICON_SIZE_MODIFIERS } from '@/components/icon/icon_constants';
+import { emojiPattern } from 'regex-combined-emojis';
 
 /**
  * Wrapper to find and replace shortcodes like :smile: or unicode chars such as 😄 with our custom Emojis implementation.
@@ -57,7 +58,7 @@ export default {
       return items
         .filter(item => item.trim() !== '')
         .map((item) => {
-          if (replaceList.includes(item)) {
+          if (replaceList.includes(item) || regexp.test(item)) {
             return this.$createElement(DtEmoji, {
               props: { code: item, size: this.size },
             });
@@ -93,9 +94,7 @@ export default {
      */
     searchCodes (textContent) {
       const shortcodes = findShortCodes(textContent);
-      const emojis = findEmojis(textContent);
-
-      const replaceList = [...shortcodes, ...emojis];
+      const replaceList = [...shortcodes, emojiPattern];
       return this.replaceDtEmojis(replaceList, textContent);
     },
   },
