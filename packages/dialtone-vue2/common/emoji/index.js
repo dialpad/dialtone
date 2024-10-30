fa903f87e2ae4e1bde7df8e443f6381c07944ccb
Brad Paugh <brad.paugh@dialpad.com>
2024-10-30 14:41:46 -0700

    fix(emoji): NO-JIRA revert back to emoji-toolkit

--- packages/dialtone-vue2/common/emoji/index.js
+++ packages/dialtone-vue2/common/emoji/index.js
@@ -1,5 +1,5 @@
 import { emojiPattern } from 'regex-combined-emojis';
-import { emojisIndexed } from '@dialpad/dialtone-emojis';
+import emojiJsonLocal from 'emoji-toolkit/emoji_strategy.json' with { type: 'json' };
 
 export const emojiRegex = new RegExp(emojiPattern, 'g');
 export const emojiVersion = '8.0';
@@ -14,7 +14,7 @@ export let emojiFileExtensionSmall = '.png';
 export let emojiImageUrlLarge = defaultEmojiAssetUrl;
 export let emojiFileExtensionLarge = '.png';
 
-export const emojiJson = emojisIndexed;
+export const emojiJson = emojiJsonLocal;
 
 export const emojiShortCodeRegex = /(^| |(?<=:))(:\w+:)/g;
 
@@ -175,6 +175,7 @@ export function codeToEmojiData (code) {
     return shortcodeToEmojiData(code);
   } else {
     const unicodeString = unicodeToString(code);
+    console.log(unicodeString);
     const result = emojiJson[unicodeString];
     if (result) result.key = unicodeString;
     return result;
