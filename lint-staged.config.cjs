61cbe5fb73761a340467350a3a9c85c4228d3e99
Brad Paugh <brad.paugh@dialpad.com>
2024-11-28 09:13:50 -0800

    support all keywords files

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -3,7 +3,8 @@ module.exports = {
     'eslint --fix',
     'vitest related --run',
   ],
-  '**/dialtone-icons/src/keywords-icons.json': (filenames) => 'nx run dialtone-icons:build --skip-nx-cache',
+  // using function syntax here so we can run the command without lint staged automatically passing filenames to it.
+  '**/dialtone-icons/src/keywords-*.json': (filenames) => 'nx run dialtone-icons:build --skip-nx-cache',
   '*.json': [
     'eslint --fix',
   ],
