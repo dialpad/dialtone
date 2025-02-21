253e0d4fe8b2f4934101ec10a396b15d61d3f0c0
Nina <ninamrepetto@gmail.com>
2025-02-21 15:15:36 -0300

    fix: NO-JIRA not show unmounted warning in dev or prod environments

--- packages/dialtone-vue2/common/utils/index.js
+++ packages/dialtone-vue2/common/utils/index.js
@@ -377,7 +377,9 @@ export function capitalizeFirstLetter (str, locale = 'en-US') {
  * @param {HTMLElement} componentRef - the component reference
  * @param {string} componentName - the component name
  */
+// eslint-disable-next-line complexity
 export function warnIfUnmounted (componentRef, componentName) {
+  if (typeof process === 'undefined') return;
   if (typeof process !== 'undefined' && process.env.NODE_ENV !== 'test') return;
   if (!componentRef || !(componentRef instanceof HTMLElement) || !document?.body) return;
   if (!document.body.contains(componentRef)) {
