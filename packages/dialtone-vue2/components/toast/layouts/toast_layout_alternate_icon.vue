3e64e88b80cd6a7081831da17f217740f9084225
Vishal Bhardwaj <117891012+vishaldialpad@users.noreply.github.com>
2025-04-17 09:59:24 +0530

    DLT-2385: new layout for Toast component in vue2 (#716)

---
+++ packages/dialtone-vue2/components/toast/layouts/toast_layout_alternate_icon.vue
@@ -0,0 +1,70 @@
+<template>
+  <div
+    aria-hidden="true"
+    class="d-toast-layout-alternate__icon"
+  >
+    <slot>
+      <component
+        :is="defaultIcon"
+        v-if="defaultIcon"
+        :size="size"
+      />
+    </slot>
+  </div>
+</template>
+
+<script>
+import {
+  DtIconInfo,
+  DtIconAlertTriangle,
+  DtIconBell,
+  DtIconSparkle,
+} from '@dialpad/dialtone-icons/vue2';
+import { TOAST_ALTERNATE_KINDS } from '../toast_constants.js';
+import { ICON_SIZE_MODIFIERS } from '@/components/icon/icon_constants.js';
+
+const kindToIcon = new Map([
+  ['info', DtIconInfo],
+  ['success', DtIconInfo],
+  ['warning', DtIconAlertTriangle],
+  ['error', DtIconInfo],
+  ['base', DtIconBell],
+  ['gradient', DtIconSparkle],
+]);
+
+export default {
+  name: 'DtToastLayoutAlternateIcon',
+  components: {
+    DtIconInfo,
+    DtIconAlertTriangle,
+    DtIconBell,
+    DtIconSparkle,
+  },
+
+  props: {
+    /**
+     * Kind of icon
+     * @values base, error, info, success, warning
+     */
+    kind: {
+      type: String,
+      default: 'base',
+      validator (kind) {
+        return TOAST_ALTERNATE_KINDS.includes(kind);
+      },
+    },
+
+    size: {
+      type: String,
+      default: '400',
+      validator: (s) => Object.keys(ICON_SIZE_MODIFIERS).includes(s),
+    },
+  },
+
+  computed: {
+    defaultIcon () {
+      return kindToIcon.get(this.kind);
+    },
+  },
+};
+</script>
--- packages/dialtone-vue2/components/toast/toast_constants.js
+++ packages/dialtone-vue2/components/toast/toast_constants.js
@@ -1,3 +1,5 @@
+import { NOTICE_KINDS } from '@/components/notice';
+export const TOAST_ALTERNATE_KINDS = ['gradient', ...NOTICE_KINDS];
 export const TOAST_ROLES = ['status', 'alert'];
 export const TOAST_MIN_DURATION = 6000;
 export const TOAST_LAYOUTS = ['default', 'alternate'];
@@ -6,4 +8,5 @@ export default {
   TOAST_ROLES,
   TOAST_MIN_DURATION,
   TOAST_LAYOUTS,
+  TOAST_ALTERNATE_KINDS,
 };
