0e307264ab6ec9c91584becc842e8a871013dacb
Julio Ortega <julio.ortega@dialpad.com>
2025-04-17 15:07:17 -0600

    add localization to tests
    Signed-off-by: Julio Ortega <julio.ortega@dialpad.com>

--- packages/dialtone-vue2/tests/setupTests.js
+++ packages/dialtone-vue2/tests/setupTests.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
+import { DialtoneLocalizationPlugin } from '@/localization/index.js';
 
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
+Vue.use(DialtoneLocalizationPlugin);
 
 // Mock IntersectionObserver
 class MockObserver {
