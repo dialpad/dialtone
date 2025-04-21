9661b4f7cbe4c42ad11426d01789fe922a9a6468
Nina <ninamrepetto@gmail.com>
2025-04-21 16:29:52 -0300

    fix(checkbox, radio): NO-JIRA description slot not being reactive

--- packages/dialtone-vue3/components/radio/radio.vue
+++ packages/dialtone-vue3/components/radio/radio.vue
@@ -25,12 +25,12 @@
       </div>
     </label>
     <div
-      v-if="hasDescriptionOrMessages"
+      v-if="$slots.description || description || hasMessages"
       class="d-radio__messages"
       data-qa="radio-description-messages"
     >
       <div
-        v-if="hasDescription"
+        v-if="$slots.description || description"
         :class="['d-description', descriptionClass]"
         v-bind="descriptionChildProps"
         data-qa="radio-description"
@@ -156,14 +156,6 @@ export default {
         focusout: event => this.$emit('focusout', event),
         change: event => this.emitValue(event.target.value),
       };
-    },
-
-    hasDescription () {
-      return !!(this.$slots.description || this.description);
-    },
-
-    hasDescriptionOrMessages () {
-      return this.hasDescription || this.hasMessages;
     },
 
     hasMessages () {
