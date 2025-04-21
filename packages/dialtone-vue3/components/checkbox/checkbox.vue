7a29e67fd3f8efb9b4036dd98a716903aa7ad3d1
Nina Repetto <ninamrepetto@gmail.com>
2025-04-21 16:44:00 -0300

    fix(checkbox, radio): NO-JIRA description slot not being reactive (#725)

--- packages/dialtone-vue3/components/checkbox/checkbox.vue
+++ packages/dialtone-vue3/components/checkbox/checkbox.vue
@@ -27,12 +27,12 @@
       </div>
     </label>
     <div
-      v-if="hasDescriptionOrMessages"
+      v-if="$slots.description || description || hasMessages"
       class="d-checkbox__messages"
       data-qa="checkbox-description-messages"
     >
       <div
-        v-if="hasDescription"
+        v-if="$slots.description || description"
         :class="['d-description', descriptionClass]"
         v-bind="descriptionChildProps"
         data-qa="checkbox-description"
@@ -117,14 +117,6 @@ export default {
 
     hasLabel () {
       return !!(this.$slots.default || this.label);
-    },
-
-    hasDescription () {
-      return !!(this.$slots.description || this.description);
-    },
-
-    hasDescriptionOrMessages () {
-      return this.hasDescription || this.hasMessages;
     },
 
     hasMessages () {
