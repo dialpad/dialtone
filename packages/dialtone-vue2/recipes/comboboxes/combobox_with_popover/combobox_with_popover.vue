2aa52b27aef9865bfb0ab91d05b76b21b892bb08
Julio Ortega <julio.ortega@dialpad.com>
2025-04-21 13:31:20 -0600

    fix label sizes
    Signed-off-by: Julio Ortega <julio.ortega@dialpad.com>

--- packages/dialtone-vue2/recipes/comboboxes/combobox_with_popover/combobox_with_popover.vue
+++ packages/dialtone-vue2/recipes/comboboxes/combobox_with_popover/combobox_with_popover.vue
@@ -117,7 +117,7 @@
 <script>
 import ComboboxLoadingList from '@/components/combobox/combobox_loading-list.vue';
 import ComboboxEmptyList from '@/components/combobox/combobox_empty-list.vue';
-import { DtCombobox, LABEL_SIZES } from '@/components/combobox';
+import { DtCombobox, COMBOBOX_LABEL_SIZES } from '@/components/combobox';
 import { DtPopover, POPOVER_APPEND_TO_VALUES, POPOVER_CONTENT_WIDTHS } from '@/components/popover';
 import { getUniqueString } from '@/common/utils';
 import { DROPDOWN_PADDING_CLASSES } from '@/components/dropdown';
@@ -160,7 +160,7 @@ export default {
     size: {
       type: String,
       default: null,
-      validator: (t) => Object.values(LABEL_SIZES).includes(t),
+      validator: (t) => Object.values(COMBOBOX_LABEL_SIZES).includes(t),
     },
 
     /**
