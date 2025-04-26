42d53d92fd79df4f321ce4e46e1b0a974781f39d
Francis Rupert <francisrupert@gmail.com>
2025-04-26 17:44:45 -0500

    toc style housekeeping

--- apps/dialtone-documentation/docs/.vuepress/theme/components/TocItem.vue
+++ apps/dialtone-documentation/docs/.vuepress/theme/components/TocItem.vue
@@ -2,7 +2,7 @@
   <RouterLink
     :class="[
       'd-btn d-btn--xs d-btn--muted d-w100p d-jc-flex-start',
-      { 'd-fw-bold d-bgc-moderate': active },
+      { 'd-btn--active': active },
       { 'd-fw-normal': !active },
     ]"
     :to="to"
