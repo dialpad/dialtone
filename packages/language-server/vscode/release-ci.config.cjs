b37092ce860cacbd889c106a1c4a904ce49afc6d
Julio Ortega <julio.ortega@dialpad.com>
2025-05-08 23:55:09 -0600

    update release config
    Signed-off-by: Julio Ortega <julio.ortega@dialpad.com>

--- packages/language-server/vscode/release-ci.config.cjs
+++ packages/language-server/vscode/release-ci.config.cjs
@@ -12,7 +12,13 @@ module.exports = {
     ['@semantic-release/commit-analyzer', {
       preset: 'angular',
       releaseRules: [
+        { type: 'build', release: 'patch' },
+        { type: 'chore', release: 'patch' },
+        { type: 'ci', release: 'patch' },
+        { type: 'docs', release: 'patch' },
         { type: 'refactor', release: 'patch' },
+        { type: 'style', release: 'patch' },
+        { type: 'test', release: 'patch' },
       ],
     }],
     ['@semantic-release/release-notes-generator', {
@@ -41,15 +47,9 @@ module.exports = {
     }],
   ],
   branches: [
-    'staging',
-    'next',
-    {
-      name: 'beta',
-      prerelease: true,
-    },
-    {
-      name: 'alpha',
-      prerelease: true,
-    },
+    { name: 'staging', prerelease: true },
+    { name: 'next', prerelease: true },
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true },
   ],
 };
