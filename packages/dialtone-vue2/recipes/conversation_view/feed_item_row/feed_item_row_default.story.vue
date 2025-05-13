2cf210be08b5a8b18476cf2ced7ff34aacfd4ca1
Julio Ortega <julio.ortega@dialpad.com>
2025-05-12 18:40:23 -0600

    refactor feed_item_row
    Signed-off-by: Julio Ortega <julio.ortega@dialpad.com>

--- packages/dialtone-vue2/recipes/conversation_view/feed_item_row/feed_item_row_default.story.vue
+++ packages/dialtone-vue2/recipes/conversation_view/feed_item_row/feed_item_row_default.story.vue
@@ -153,22 +153,21 @@ export default {
         {
           emojiUnicodeOrShortname: '😀',
           isSelected: true,
-          ariaLabel: 'Emoji aria label',
-          tooltip: 'You reacted with 😀',
           num: 1,
         },
         {
           emojiUnicodeOrShortname: '😒',
           isSelected: false,
-          ariaLabel: 'Emoji aria label',
-          tooltip: 'You reacted with 😒',
           num: 1,
+        },
+        {
+          emojiUnicodeOrShortname: '😌',
+          isSelected: true,
+          num: 99,
         },
         {
           emojiUnicodeOrShortname: '🙃',
           isSelected: false,
-          ariaLabel: 'Emoji aria label',
-          tooltip: 'You reacted with 🙃',
           num: 99,
         },
       ],
