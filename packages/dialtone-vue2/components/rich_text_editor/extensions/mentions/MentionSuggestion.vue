48889727f5dfc1bf6ef07761e9ffcb405d22bb2f
Bianca Artola <bianca.artola@dialpad.com>
2025-05-20 16:02:49 -0300

    feat(mention-suggestion): DP-134987 add presence and status to mention suggestions

--- packages/dialtone-vue2/components/rich_text_editor/extensions/mentions/MentionSuggestion.vue
+++ packages/dialtone-vue2/components/rich_text_editor/extensions/mentions/MentionSuggestion.vue
@@ -7,24 +7,44 @@
       :full-name="name"
       :image-src="avatarSrc"
       :image-alt="name"
+      :show-presence="showDetails"
       size="xs"
     />
     <!-- eslint-disable-next-line vue/no-restricted-class -->
     <span class="d-mention-suggestion__name">
       {{ name }}
     </span>
+
+   <div v-if="showDetails">
+        <span
+          v-if="presence"
+          data-qa="dt-recipe-leftbar-row-presence-text"
+          :class="['d-recipe-leftbar-row__meta-context', presenceFontColorClass]"
+        >
+          {{ presence }}
+        </span>
+        <dt-emoji-text-wrapper
+          v-if="status"
+          size="100"
+          element-type="span"
+        >
+          {{ status }}
+        </dt-emoji-text-wrapper>
+      </div>
   </dt-stack>
 </template>
 
 <script>
 import { DtAvatar } from '@/components/avatar';
 import { DtStack } from '@/components/stack';
+import DtEmojiTextWrapper from '@/components/emoji_text_wrapper/emoji_text_wrapper.vue';
 
 export default {
   name: 'MentionSuggestion',
   components: {
     DtAvatar,
     DtStack,
+    DtEmojiTextWrapper
   },
 
   props: {
@@ -32,6 +52,11 @@ export default {
       type: Object,
       required: true,
     },
+
+    showDetails: {
+      type: Boolean,
+      default: false
+    }
   },
 
   computed: {
@@ -41,6 +66,24 @@ export default {
 
     avatarSrc () {
       return this.item.avatarSrc;
+    },
+
+    presence () {
+      return this.item.presence;
+    },
+
+    status () {
+      return this.item.status;
+    },
+
+    presenceFontColorClass () {
+      const presenceFontColors = {
+        active: 'd-recipe-contact-row--active',
+        busy: 'd-recipe-contact-row--busy',
+        away: 'd-recipe-contact-row--away',
+      };
+
+      return presenceFontColors[this.avatarPresence];
     },
   },
 };
