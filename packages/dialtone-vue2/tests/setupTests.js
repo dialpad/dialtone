13df38d04f43ed37b25be5cfb02e5b1e78ec7769
Julio Ortega <julio.ortega@dialpad.com>
2025-05-26 18:27:36 -0600

    fix tests
    Signed-off-by: Julio Ortega <julio.ortega@dialpad.com>

--- packages/dialtone-vue2/tests/setupTests.js
+++ packages/dialtone-vue2/tests/setupTests.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
+import { DtTooltipDirective } from '@/directives/tooltip_directive';
 
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
+Vue.use(DtTooltipDirective);
 
 // Mock IntersectionObserver
 class MockObserver {
