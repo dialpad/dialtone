68b855f2c42918b2ce15e385de55ccb0491ff66d
Tico Ortega <julio.ortega@dialpad.com>
2025-07-21 16:20:41 -0600

    fix missing strings

--- packages/dialtone-vue3/recipes/buttons/callbar_button_with_popover/callbar_button_with_popover.vue
+++ packages/dialtone-vue3/recipes/buttons/callbar_button_with_popover/callbar_button_with_popover.vue
@@ -47,10 +47,11 @@
           :active="open"
           :class="['d-recipe-callbar-button-with-popover__arrow',
                    { 'd-recipe-callbar-button-with-popover__arrow--large': !isCompactMode }]"
-          circle
+          :circle="true"
           importance="clear"
           size="lg"
-          v-bind="arrowButtonLabel"
+          :aria-label="arrowButtonLabel"
+          :title="arrowButtonLabel"
           width="2rem"
           @click="arrowClick"
         >
@@ -346,7 +347,7 @@ export default {
     },
 
     arrowButtonLabel () {
-      return this.i18n.$ta('DIALTONE_CALLBAR_BUTTON_WITH_POPOVER_ARROW_BUTTON_LABEL');
+      return this.i18n.$t('DIALTONE_CALLBAR_BUTTON_WITH_POPOVER_ARROW_BUTTON_ARIA_LABEL');
     },
   },
 
