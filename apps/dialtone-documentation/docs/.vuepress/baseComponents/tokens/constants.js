01efb9e3e809d966ac2041b12d7a33398a65a9d8
Tico Ortega <julio.ortega@dialpad.com>
2025-08-18 14:24:26 -0600

    add themes
    Signed-off-by: Tico Ortega <julio.ortega@dialpad.com>

--- apps/dialtone-documentation/docs/.vuepress/baseComponents/tokens/constants.js
+++ apps/dialtone-documentation/docs/.vuepress/baseComponents/tokens/constants.js
@@ -14,11 +14,19 @@ export const BRANDS = [
   { value: 'tmo', label: 'T-Mobile' },
   { value: 'expressive', label: 'Expressive' },
   { value: 'expressive-sm', label: 'Expressive Small' },
-  // { value: 'deca', label: 'Deca' },
+  { value: 'aegean', label: 'Aegean' },
+  { value: 'botany', label: 'Botany' },
+  { value: 'buttercream', label: 'Buttercream' },
+  { value: 'ceruleo', label: 'Ceruleo' },
+  { value: 'high-desert', label: 'High Desert' },
+  { value: 'melon', label: 'Melon' },
+  { value: 'plum', label: 'Plum' },
+  { value: 'sunflower', label: 'Sunflower' },
+  { value: 'verdant-haze', label: 'Verdant Haze' },
 ];
 
 export const CATEGORY_MAP = {
-  color: ['color', 'opacity', 'theme'],
+  color: ['color', 'opacity', 'shell', 'theme'],
   typography: ['typography', 'font'],
   size: ['size'],
   space: ['space'],
