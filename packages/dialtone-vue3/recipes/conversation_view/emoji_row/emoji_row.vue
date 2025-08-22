761cd663b76acf9f27e3b51ede288a69e73025c1
Tico Ortega <julio.ortega@dialpad.com>
2025-08-22 14:33:39 -0600

    improve emoji row tooltip - vue 3

--- packages/dialtone-vue3/recipes/conversation_view/emoji_row/emoji_row.vue
+++ packages/dialtone-vue3/recipes/conversation_view/emoji_row/emoji_row.vue
@@ -7,12 +7,22 @@
       <dt-tooltip
         class="d-recipe-emoji-row__tooltip"
         content-class="d-recipe-emoji-row__tooltip-content"
+        :fallback-placements="['top', 'bottom']"
         sticky="popper"
         @shown="(shown) => emojiHovered(reaction, shown)"
       >
         <span aria-hidden="true">
-          <dt-emoji-text-wrapper size="200">
-            {{ reactionLabel(reaction) }}
+          <dt-emoji-text-wrapper size="800">
+            <p class="d-recipe-emoji-row__tooltip-emoji">
+              {{ reaction.emojiUnicodeOrShortname }}
+            </p>
+            <p class="d-recipe-emoji-row__tooltip-names">
+              {{ reaction.names }}
+              <span
+                class="d-recipe-emoji-row__tooltip-label"
+                v-text="reactionLabel(reaction)"
+              />
+            </p>
           </dt-emoji-text-wrapper>
         </span>
         <template #anchor="{ attrs }">
@@ -30,7 +40,7 @@
           >
             <span class="d-recipe-emoji-row__emoji">
               <dt-emoji
-                size="200"
+                size="300"
                 :code="reaction.emojiUnicodeOrShortname"
               />
             </span>
@@ -54,6 +64,7 @@ import { DtTooltip } from '@/components/tooltip';
 import { DtEmoji } from '@/components/emoji';
 import { DtEmojiTextWrapper } from '@/components/emoji_text_wrapper';
 import { DialtoneLocalization } from '@/localization';
+import { emojiToShortcode } from '@/common/emoji';
 
 export default {
   compatConfig: { MODE: 3 },
@@ -103,8 +114,7 @@ export default {
 
     reactionLabel (reaction) {
       return this.i18n.$t('DIALTONE_EMOJI_ROW_REACTION_LABEL', {
-        names: reaction.names,
-        reaction: reaction.emojiUnicodeOrShortname,
+        reaction: emojiToShortcode(reaction.emojiUnicodeOrShortname),
       });
     },
   },
