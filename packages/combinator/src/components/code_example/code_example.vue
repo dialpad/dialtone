1a48c07897bcca726338f55e826928be645b951b
Julio Ortega <julio.ortega@dialpad.com>
2025-09-01 18:37:40 -0600

    update combinator styles
    Signed-off-by: Julio Ortega <julio.ortega@dialpad.com>

---
+++ packages/combinator/src/components/code_example/code_example.vue
@@ -0,0 +1,190 @@
+<template>
+  <dt-tab-group
+    class="code-example-tab-group"
+    @change="selectedPanelId = $event.selected"
+  >
+    <template #tabs>
+      <div class="d-d-flex d-jc-space-between d-ai-flex-start d-w100p">
+        <div>
+          <dt-tab
+            :id="vueTabId"
+            label="Vue code"
+            :panel-id="vuePanelId"
+            selected
+          >
+            Vue
+          </dt-tab>
+          <dt-tab
+            :id="htmlTabId"
+            label="HTML code"
+            :panel-id="htmlPanelId"
+          >
+            HTML
+          </dt-tab>
+        </div>
+        <!-- aria label blank so no tooltip displays since it would be redundant to the "Copy code" text -->
+        <copy-button
+          text=""
+          aria-label=""
+        >
+          Copy Code
+        </copy-button>
+      </div>
+    </template>
+    <dt-tab-panel
+      :id="vuePanelId"
+      :tab-id="vueTabId"
+    >
+      <div
+        v-dt-scrollbar
+        class="language-html d-hmx332"
+        data-ext="html"
+      >
+        <!--        <pre-->
+        <!--          class="language-html"-->
+        <!--          v-html="highlightedVue"-->
+        <!--        />-->
+      </div>
+    </dt-tab-panel>
+    <!--    <dt-tab-panel-->
+    <!--      v-if="!!htmlCode"-->
+    <!--      :id="htmlPanelId"-->
+    <!--      :tab-id="htmlTabId"-->
+    <!--    >-->
+    <!--      <dt-banner-->
+    <!--        v-if="showHtmlWarning"-->
+    <!--        class="d-ps-static"-->
+    <!--        kind="warning"-->
+    <!--        hide-close-->
+    <!--      >-->
+    <!--        Raw HTML renders visuals only. You may need to add JS to replicate its functionality.-->
+    <!--      </dt-banner>-->
+    <!--      <div-->
+    <!--        v-dt-scrollbar-->
+    <!--        class="language-html d-hmx332"-->
+    <!--        data-ext="html"-->
+    <!--      >-->
+    <!--        <pre-->
+    <!--          class="language-html"-->
+    <!--          v-html="highlightedHtml"-->
+    <!--        />-->
+    <!--      </div>-->
+    <!--    </dt-tab-panel>-->
+  </dt-tab-group>
+</template>
+
+<script setup>
+import { computed, onMounted, ref } from 'vue';
+import { DtTab, DtTabPanel, DtBanner, DtTabGroup } from '@dialpad/dialtone-vue';
+import Prism from 'prismjs';
+import prettier from 'prettier/standalone';
+import htmlParser from 'prettier/plugins/html.mjs';
+import CopyButton from './copy_button.vue';
+import { getUniqueString } from '@workspaceRoot/common/utils/client.mjs';
+
+const props = defineProps({
+  /**
+   * The HTML code to be displayed in the HTML tab if the component reference is not provided or
+   * a function that retrieves the reference to the example component to get the HTML code.
+   */
+  // htmlCode: {
+  //   type: [String, Function],
+  //   default: null,
+  // },
+  /**
+   * The Vue code to be displayed in the Vue tab.
+   */
+  // vueCode: {
+  //   type: String,
+  //   required: true,
+  // },
+  /**
+   * Indicates whether to show a warning for HTML code.
+   */
+  showHtmlWarning: {
+    type: Boolean,
+    default: true,
+  },
+
+  component: {
+    type: Object,
+    required: true,
+  },
+});
+
+const formattedHTML = ref(null);
+
+// const trimmedHtmlCode = computed(() => {
+//   if (formattedHTML.value) {
+//     return formattedHTML.value;
+//   }
+//   return typeof props.htmlCode === 'string' ? props.htmlCode.replace(/^\n/gm, '') : '';
+// });
+
+// const highlightedHtml = computed(() => {
+//   if (formattedHTML.value) {
+//     return Prism.highlight(
+//       formattedHTML.value,
+//       Prism.languages.html,
+//       'html',
+//     );
+//   }
+//   return typeof props.htmlCode === 'string' ? Prism.highlight(props.htmlCode.trim(), Prism.languages.html, 'html') : '';
+// });
+
+// const trimmedVueCode = props.vueCode.replace(/^\n/gm, '');
+// const highlightedVue = Prism.highlight(props.vueCode.trim(), Prism.languages.html, 'html');
+
+const vueTabId = getUniqueString();
+const vuePanelId = getUniqueString();
+const htmlTabId = getUniqueString();
+const htmlPanelId = getUniqueString();
+
+const selectedPanelId = ref(vuePanelId);
+
+onMounted(async () => {
+  console.log(props.component)
+  // if (typeof props.htmlCode === 'function') {
+  //   const componentRef = props.htmlCode();
+  //   const el = componentRef.$el ?? componentRef;
+  //   formattedHTML.value = await formatHTML(el.outerHTML);
+  // }
+});
+
+/**
+ * Transforms a single-line HTML string to an indented multiline HTML string.
+ * Removes comments, id and data-qa attributes, and simplifies svg tags.
+ * Also, adds a new line before each svg, img, and span tag because prettier
+ * doesn't do it.
+ * @param elementHTML - The HTML code to be formatted.
+ * @returns The formatted HTML code.
+ */
+// const formatHTML = async (elementHTML) => {
+//   const normalizedHTML = elementHTML
+//     .replace(/<!--.*?-->/g, '')
+//     .replace(/id=".*?"/g, '')
+//     .replace(/data-qa=".*?"/g, '')
+//     .replace(/<svg.*?>.*?<\/svg>/g, '<svg>...</svg>')
+//     .replace(/<svg/g, '\n<svg')
+//     .replace(/<img/g, '\n<img')
+//     .replace(/<span/g, '\n<span');
+//   const prettyHTML = await prettier.format(normalizedHTML,
+//     {
+//       parser: 'html',
+//       plugins: [htmlParser],
+//       htmlWhitespaceSensitivity: 'ignore',
+//     });
+//   return prettyHTML;
+// };
+</script>
+
+<style scoped lang="less">
+.code-example-tab-group {
+  margin-top: var(--dt-space-500);
+
+  .language-html {
+    margin-top: 0;
+    position: relative;
+  }
+}
+</style>
--- packages/combinator/src/components/option_bar/option_bar.vue
+++ packages/combinator/src/components/option_bar/option_bar.vue
@@ -1,67 +1,259 @@
 <template>
-  <div class="dtc-option-bar d-as-stretch d-of-y-auto d-w100p">
-    <ul class="d-ls-reset">
-      <dtc-section
-        v-if="info.slots"
-        heading="Slots"
-        content-class="d-px12"
+  <section class="d-divide-y d-divide-default">
+    <dt-stack
+      gap="500"
+      direction="row"
+    >
+      <h2
+        class="d-fl1"
+        :class="isFullscreen ? 'd-headline--lg' : 'd-headline--md'"
       >
-        <dtc-option-bar-member-group
-          :component="component"
-          :members="info.slots"
-          :values="options.slots"
-          :control-selector="getSlotControls"
-          @update:member="updateSlots"
-        />
-      </dtc-section>
-      <dtc-section
-        v-if="info.props"
-        heading="Props"
-        content-class="d-px12"
+        Button
+      </h2>
+      <dt-button
+        v-dt-tooltip="`Fullscreen`"
+        kind="muted"
+        importance="clear"
+        size="sm"
+        @click="toggleFullScreen"
       >
-        <dtc-option-bar-member-group
-          :component="component"
-          :members="info.props"
-          :values="options.props"
-          :control-selector="(prop, value) => getBindingControls(prop, value, 'null')"
-          @update:member="updateProps"
-        />
-      </dtc-section>
-      <dtc-section
-        v-if="info.attributes"
-        heading="Native HTML Attributes"
-        content-class="d-px12"
+        <template #icon="{ iconSize }">
+          <dt-icon-minimize
+            v-if="isFullscreen"
+            :size="iconSize"
+          />
+          <dt-icon-expand
+            v-else
+            :size="iconSize"
+          />
+        </template>
+      </dt-button>
+    </dt-stack>
+    <div
+      v-dt-scrollbar
+      class="dialtone-playground__controls"
+    >
+      <dt-stack
+        gap="500"
       >
-        <dtc-option-bar-member-group
-          :component="component"
-          :members="info.attributes"
-          :values="options.attributes"
-          :control-selector="(attribute, value) => getBindingControls(attribute, value)"
-          @update:member="updateAttributes"
+        <dt-select-menu
+          id="buttonSize"
+          v-model="buttonSize"
+          :disabled="isLink"
+          size="sm"
+          :options="[
+            { value: `xs`, label: `xs` },
+            { value: `sm`, label: `sm` },
+            { value: `md`, label: `md` },
+            { value: `lg`, label: `lg` },
+            { value: `xl`, label: `xl` },
+          ]"
+          label="Size"
         />
-      </dtc-section>
-      <dtc-section
-        v-if="info.events"
-        heading="Events"
-        content-class="d-px12"
-      >
-        <dtc-option-bar-member-group
-          :component="component"
-          :members="info.events"
-          :values="options.events"
-          :control-selector="getEventControls"
+        <dt-select-menu
+          v-model="buttonImportance"
+          :disabled="isLink"
+          size="sm"
+          :options="[
+            { value: `clear`, label: `clear` },
+            { value: `outlined`, label: `outlined` },
+            { value: `primary`, label: `primary` },
+          ]"
+          label="Importance"
         />
-      </dtc-section>
-    </ul>
-  </div>
+        <dt-select-menu
+          v-model="buttonKind"
+          :disabled="isLink"
+          size="sm"
+          :options="[
+            { value: `default`, label: `default` },
+            { value: `muted`, label: `muted` },
+            { value: `danger`, label: `danger` },
+            { value: `positive`, label: `positive` },
+            { value: `inverted`, label: `inverted` },
+            { value: `unstyled`, label: `unstyled` },
+          ]"
+          label="Kind"
+        />
+        <dt-stack gap="400">
+          <dt-toggle
+            v-model="hasIcon"
+            :disabled="isLink"
+            label-class="d-label--sm d-fc-secondary"
+            size="sm"
+            wrapper-class="d-jc-space-between"
+          >
+            Icon
+          </dt-toggle>
+          <dt-select-menu
+            v-show="hasIcon"
+            v-model="iconName"
+            :disabled="isLink"
+            size="sm"
+            label-class="d-vi-visible-sr"
+            :options="[
+              { value: `activity`, label: `activity` },
+              { value: `add-task`, label: `add-task` },
+              { value: `agent-assist`, label: `agent-assist` },
+              { value: `ai-notes`, label: `ai-notes` },
+              { value: `ai-write`, label: `ai-write` },
+              { value: `airplay`, label: `airplay` },
+              { value: `airtable`, label: `airtable` },
+              { value: `alarm-check`, label: `alarm-check` },
+              { value: `alarm-clock-off`, label: `alarm-clock-off` },
+              { value: `alarm-minus`, label: `alarm-minus` },
+              { value: `alarm-plus`, label: `alarm-plus` },
+              { value: `album`, label: `album` },
+              { value: `alert-circle`, label: `alert-circle` },
+              { value: `alert-triangle`, label: `alert-triangle` },
+              { value: `align-center`, label: `align-center` },
+              { value: `align-justify`, label: `align-justify` },
+              { value: `align-left`, label: `align-left` },
+              { value: `align-right`, label: `align-right` },
+              { value: `amex`, label: `amex` },
+              { value: `app-store-badge`, label: `app-store-badge` },
+              { value: `apple`, label: `apple` },
+              { value: `archive`, label: `archive` },
+              { value: `archive-restore`, label: `archive-restore` },
+              { value: `arrow-down`, label: `arrow-down` },
+              { value: `arrow-down-left`, label: `arrow-down-left` },
+              { value: `arrow-down-right`, label: `arrow-down-right` },
+              { value: `arrow-left`, label: `arrow-left` },
+              { value: `arrow-left-right`, label: `arrow-left-right` },
+              { value: `arrow-right`, label: `arrow-right` },
+              { value: `arrow-up`, label: `arrow-up` },
+              { value: `...`, label: `...` },
+              { value: `zoom-in`, label: `zoom-in` },
+              { value: `zoom-out`, label: `zoom-out` },
+            ]"
+            label="Icon name"
+          />
+        </dt-stack>
+        <dt-select-menu
+          v-show="hasIcon"
+          v-model="iconPosition"
+          :disabled="isLink"
+          size="sm"
+          :options="[
+            { value: `left`, label: `left` },
+            { value: `right`, label: `right` },
+            { value: `top`, label: `top` },
+            { value: `bottom`, label: `bottom` },
+          ]"
+          label="Icon Position"
+        />
+        <dt-toggle
+          v-show="hasIcon"
+          v-model="isIconOnly"
+          :disabled="isLink"
+          label-class="d-label--sm d-fc-secondary"
+          size="sm"
+          wrapper-class="d-jc-space-between"
+        >
+          Icon only
+        </dt-toggle>
+        <dt-toggle
+          v-show="isIconOnly"
+          v-model="isCircle"
+          :disabled="isLink"
+          label-class="d-label--sm d-fc-secondary"
+          size="sm"
+          wrapper-class="d-jc-space-between"
+        >
+          Circle
+        </dt-toggle>
+        <dt-toggle
+          v-show="!hasIcon"
+          v-model="isLink"
+          label-class="d-label--sm d-fc-secondary"
+          size="sm"
+          wrapper-class="d-jc-space-between"
+        >
+          Link
+        </dt-toggle>
+        <dt-select-menu
+          v-show="isLink && !hasIcon"
+          v-model="linkKind"
+          size="sm"
+          :options="[
+            { value: `default`, label: `default` },
+            { value: `warning`, label: `warning` },
+            { value: `danger`, label: `danger` },
+            { value: `success`, label: `success` },
+            { value: `muted`, label: `muted` },
+            { value: `mention`, label: `mention` },
+          ]"
+          label="Link Kind"
+        />
+        <dt-toggle
+          v-show="isLink && !hasIcon"
+          v-model="isLinkInverted"
+          label-class="d-label--sm d-fc-secondary"
+          size="sm"
+          wrapper-class="d-jc-space-between"
+        >
+          Link Inverted
+        </dt-toggle>
+        <dt-toggle
+          v-model="isActive"
+          :disabled="isLink"
+          label-class="d-label--sm d-fc-secondary"
+          size="sm"
+          wrapper-class="d-jc-space-between"
+        >
+          Active
+        </dt-toggle>
+        <dt-toggle
+          v-model="isLoading"
+          :disabled="isLink"
+          label-class="d-label--sm d-fc-secondary"
+          size="sm"
+          wrapper-class="d-jc-space-between"
+        >
+          Loading
+        </dt-toggle>
+        <dt-toggle
+          v-model="isDisabled"
+          label-class="d-label--sm d-fc-secondary"
+          size="sm"
+          wrapper-class="d-jc-space-between"
+        >
+          Disabled
+        </dt-toggle>
+        <dt-input
+          v-model="buttonLabel"
+          label="Label"
+          type="text"
+          size="sm"
+        />
+        <dt-input
+          v-model="buttonLabelClass"
+          label="Label class"
+          type="text"
+          size="sm"
+        />
+      </dt-stack>
+    </div>
+  </section>
 </template>
 
 <script setup>
-import DtcOptionBarMemberGroup from './option_bar_member_group.vue';
-import DtcSection from '../tools/section.vue';
+// import DtcOptionBarMemberGroup from './option_bar_member_group.vue';
+// import DtcSection from '../tools/section.vue';
+
+import { ref } from 'vue';
 
 import { OPTIONS_UPDATE_EVENT } from '@/src/lib/constants';
 import { getControlByMemberType, getControlByValue } from '@/src/lib/control';
+import {
+  DtStack,
+  DtSelectMenu,
+  DtToggle,
+  DtInput,
+  DtButton,
+} from '@dialpad/dialtone-vue';
+import DtIconMinimize from '@dialpad/dialtone-icons/vue3/minimize';
+import DtIconExpand from '@dialpad/dialtone-icons/vue3/expand';
 
 defineProps({
   /**
@@ -87,7 +279,31 @@ defineProps({
   },
 });
 
-const emit = defineEmits([OPTIONS_UPDATE_EVENT]);
+const emit = defineEmits([OPTIONS_UPDATE_EVENT, 'toggle-full-screen']);
+
+const hasIcon = ref(false);
+const isLink = ref(false);
+const isFullscreen = ref(false);
+const buttonLabel = ref('Place Call');
+const buttonLabelClass = ref('');
+const buttonSize = ref('xs');
+const buttonImportance = ref('primary');
+const buttonKind = ref('default');
+const iconName = ref('activity');
+const iconPosition = ref('left');
+const linkKind = ref('default');
+const isActive = ref(false);
+const isIconOnly = ref(false);
+const isCircle = ref(false);
+const isLinkInverted = ref(false);
+const isLoading = ref(false);
+const isDisabled = ref(false);
+
+
+const toggleFullScreen = () => {
+  isFullscreen.value = !isFullscreen.value;
+  emit('toggle-full-screen', isFullscreen.value);
+}
 
 /**
  * Gets an array of controls for a binding.
@@ -149,9 +365,17 @@ function updateMember (memberGroup, { member, value }) {
   });
 }
 
-function updateSlots (e) { updateMember('slots', e); }
-function updateProps (e) { updateMember('props', e); }
-function updateAttributes (e) { updateMember('attributes', e); }
+function updateSlots (e) {
+  updateMember('slots', e);
+}
+
+function updateProps (e) {
+  updateMember('props', e);
+}
+
+function updateAttributes (e) {
+  updateMember('attributes', e);
+}
 </script>
 
 <script>
@@ -163,10 +387,3 @@ export default {
   name: 'DtcOptionBar',
 };
 </script>
-
-<style>
-  .dtc-option-bar {
-    background-color: #FCFCFC;
-    color-scheme: light;
-  }
-</style>
