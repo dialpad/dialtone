44db5814f0e39543b133f0f6fad91aa39d8939cd
Brad Paugh <brad.paugh@dialpad.com>
2025-09-18 17:49:08 -0700

    vue3

--- packages/dialtone-vue3/components/rich_text_editor/extensions/channels/ChannelComponent.vue
+++ packages/dialtone-vue3/components/rich_text_editor/extensions/channels/ChannelComponent.vue
@@ -6,13 +6,24 @@
     <dt-link
       kind="mention"
     >
-      {{ text }}
+      <dt-stack
+        direction="row"
+        gap="0"
+      >
+        <dt-icon-lock
+          v-if="$props.node.attrs.locked"
+          size="200"
+        />
+        <span>{{ text }}</span>
+      </dt-stack>
     </dt-link>
   </node-view-wrapper>
 </template>
 
 <script>
 import { nodeViewProps, NodeViewWrapper } from '@tiptap/vue-3';
+import { DtIconLock } from '@dialpad/dialtone-icons/vue3';
+import { DtStack } from '@/components/stack';
 
 import { DtLink } from '@/components/link';
 
@@ -22,12 +33,18 @@ export default {
   components: {
     NodeViewWrapper,
     DtLink,
+    DtIconLock,
+    DtStack,
   },
 
   props: nodeViewProps,
 
   computed: {
     text () {
+      console.log(nodeViewProps)
+      if (this.$props.node.attrs.locked) {
+        return this.$props.node.attrs.name;
+      }
       return '#' + this.$props.node.attrs.name;
     },
   },
