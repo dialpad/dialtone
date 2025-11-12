ced90e2ec166ab7e8c25afc683a89fc949083be9
francisrupert <francis.rupert@dialpad.com>
2025-11-11 23:14:29 -0600

    .

--- apps/dialtone-documentation/docs/.vuepress/theme/components/Navbar.vue
+++ apps/dialtone-documentation/docs/.vuepress/theme/components/Navbar.vue
@@ -119,6 +119,7 @@
     </a>
     <dt-dropdown
       id="theme-toggle-dropdown"
+      hidden
       navigation-type="arrow-keys"
       placement="bottom-start"
       class="theme-toggle-dropdown"
