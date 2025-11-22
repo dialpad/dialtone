812f73290a5b4a4e362b340e9170dc9972f15053
francisrupert <francis.rupert@dialpad.com>
2025-11-22 16:54:05 -0600

    add notice encouraging dt-stack use instead of flex css utilities

---
+++ apps/dialtone-documentation/docs/.vuepress/baseComponents/FlexStackNotice.vue
@@ -0,0 +1,21 @@
+<template>
+  <dt-notice
+    kind="warning"
+    class="d-wmx100p d-mt24"
+    hide-close
+    title="Consider DtStack instead"
+  >
+    Use the
+    <router-link class="d-link d-link--muted" to="/components/stack">
+      DtStack
+    </router-link>
+    component for most Flex-based layout implementations.
+    Only resort to using Flex utilities when the Stack component is insufficient for your layout needs.
+  </dt-notice>
+</template>
+
+<script>
+export default {
+  name: 'FlexStackNotice',
+};
+</script>
