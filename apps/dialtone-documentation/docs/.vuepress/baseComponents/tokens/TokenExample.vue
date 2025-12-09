35ddc7f0cc94ef4658c2b66233d57178210c87b3
francisrupert <francis.rupert@dialpad.com>
2025-12-09 12:52:42 -0600

    add text category support to TokenExample component

--- apps/dialtone-documentation/docs/.vuepress/baseComponents/tokens/TokenExample.vue
+++ apps/dialtone-documentation/docs/.vuepress/baseComponents/tokens/TokenExample.vue
@@ -12,7 +12,7 @@
     </div>
   </dt-stack>
   <dt-stack
-    v-if="category === 'typography'"
+    v-if="category === 'typography' || category === 'text'"
     direction="row"
     justify="center"
     align="center"
@@ -65,6 +65,7 @@ const TYPOGRAPHY_KEY_MAP = {
 const SHADOW_COMPOSITION_TOKENS = ['small', 'medium', 'large', 'extra-large', 'card', 'focus', 'focus-inset'];
 
 const isTypography = (name, key) => name.includes('--dt-typography') && name.includes(key);
+const isText = (name, key) => name.includes('--dt-text') && name.includes(key);
 const isFont = (name, key) => name.includes(`--dt-font-${key}`);
 const getRectSizeStyle = (value) => {
   if (value.endsWith('%')) return { 'inline-size': value };
@@ -142,11 +143,11 @@ const foregroundBackgroundColor = computed(() => {
 
 const getTypographyStyle = computed(() => {
   for (const key in TYPOGRAPHY_KEY_MAP) {
-    if (isFont(props.name, key) || isTypography(props.name, key)) {
+    if (isFont(props.name, key) || isTypography(props.name, key) || isText(props.name, key)) {
       return { [TYPOGRAPHY_KEY_MAP[key]]: props.value };
     }
   }
-  if (props.name.startsWith('var(--dt-typography')) {
+  if (props.name.startsWith('var(--dt-typography') || props.name.startsWith('var(--dt-text')) {
     return `font: ${props.value}`;
   }
   return null;
