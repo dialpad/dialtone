24f6c14434cea1b3259ed935e06726dd6d08287d
francisrupert <francis.rupert@dialpad.com>
2025-12-12 13:28:13 -0600

    migrate to dt-text component

--- apps/dialtone-documentation/docs/.vuepress/baseComponents/FontUtilitiesNotice.vue
+++ apps/dialtone-documentation/docs/.vuepress/baseComponents/FontUtilitiesNotice.vue
@@ -3,14 +3,14 @@
     kind="warning"
     hide-close
     class="d-wmx100p d-my16"
+    title="Use DtText in favor CSS Utilities"
   >
     <template #default>
-      <dt-text as="p" kind="body" wrap="pretty">
-        Before considering any typography utility, initially reach for the
-        <router-link class="d-fw-semibold d-link d-link--muted" to="/components/text">
-          DtText
-        </router-link> component.
-      </dt-text>
+      Reach for the
+      <router-link class="d-fw-semibold d-link d-link--muted" to="/components/text">
+        DtText
+      </router-link>
+      component before considering any typography utility.
     </template>
   </dt-notice>
 </template>
