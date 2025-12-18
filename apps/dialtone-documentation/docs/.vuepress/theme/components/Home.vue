510d9fda99797a80523e2df39a767b16e8c6cb4e
francisrupert <francis.rupert@dialpad.com>
2025-12-18 13:18:14 -0600

    doc site headers use new text styles

--- apps/dialtone-documentation/docs/.vuepress/theme/components/Home.vue
+++ apps/dialtone-documentation/docs/.vuepress/theme/components/Home.vue
@@ -8,7 +8,7 @@
       class="hero__inner-wrapper d-mx-auto d-wmx1340 d-pl24 d-pr32"
     >
       <dt-stack justify="center" class="hero__content">
-        <dt-text as="h1" kind="headline" size="xxxl" strength="normal" class="d-mb12 d-ff-marketing">
+        <dt-text as="h1" kind="headline" size="xxxl" strength="normal" class="d-mb12">
           Improve your UI's reception with Dialtone
         </dt-text>
         <dt-text as="p" kind="body" size="lg" tone="secondary" class="d-mb16">
@@ -68,7 +68,7 @@
         <router-link class="d-fc-primary h:d-td-underline d-td-none d-bar8 d-pt4" to="/design/">
           <dt-stack gap="600">
             <svg-loader class="d-h96" name="home-design-language" />
-            <dt-text as="h2" kind="headline" size="xxl" strength="normal" class="d-ff-marketing">
+            <dt-text as="h2" kind="headline" size="xxl" strength="normal">
               Design Language
             </dt-text>
           </dt-stack>
@@ -86,7 +86,7 @@
         <router-link class="d-fc-primary h:d-td-underline d-td-none d-bar8 d-pt4" to="/components/">
           <dt-stack gap="600">
             <svg-loader class="d-h96" name="home-components" />
-            <dt-text as="h2" kind="headline" size="xxl" strength="normal" class="d-ff-marketing">
+            <dt-text as="h2" kind="headline" size="xxl" strength="normal">
               Component
             </dt-text>
           </dt-stack>
@@ -110,7 +110,7 @@
         <router-link class="d-fc-primary h:d-td-underline d-td-none d-bar8 d-pt4" to="/utilities/">
           <dt-stack gap="600">
             <svg-loader class="d-h96" name="home-utilities" />
-            <dt-text as="h2" kind="headline" size="xxl" strength="normal" class="d-ff-marketing">
+            <dt-text as="h2" kind="headline" size="xxl" strength="normal">
               Utilities
             </dt-text>
           </dt-stack>
@@ -128,7 +128,7 @@
         <router-link class="d-fc-primary h:d-td-underline d-td-none d-bar8 d-pt4" to="/guides/">
           <dt-stack gap="600">
             <svg-loader class="d-h96" name="home-guides" />
-            <dt-text as="h2" kind="headline" size="xxl" strength="normal" class="d-ff-marketing">
+            <dt-text as="h2" kind="headline" size="xxl" strength="normal">
               Guides
             </dt-text>
           </dt-stack>
