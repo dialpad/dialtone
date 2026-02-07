65c1a2b20d1951f4a523db134b9d4fbd9d4b1c5e
francisrupert <francis.rupert@dialpad.com>
2026-02-06 22:11:12 -0600

    copy markdown ui

--- apps/dialtone-documentation/docs/.vuepress/theme/components/PageHeader.vue
+++ apps/dialtone-documentation/docs/.vuepress/theme/components/PageHeader.vue
@@ -21,44 +21,15 @@
         </dt-stack>
         <dt-stack direction="row" gap="200">
           <a
-            v-if="githubUrl"
-            class="d-btn d-btn--muted"
-            :href="githubUrl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <span class="d-btn__icon d-btn__icon--left">
-              <svg
-                class="d-icon d-icon--size-300"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-              >
-                <path
-                  d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.17 6.839 9.49.5.092.682-.217.682-.482
-                  0-.237-.008-.866-.013-1.7-2.782.604-3.369-1.34-3.369-1.34-.454-1.156-1.11-1.464-1.11-1.464
-                  -.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.087 2.91.831
-                  .092-.646.35-1.086.636-1.337-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683
-                  -.103-.253-.446-1.27.098-2.647 0 0 .84-.269 2.75 1.025A9.578 9.578 0 0 1 12 6.836c.85.004
-                  1.705.115 2.504.337 1.909-1.294 2.747-1.025 2.747-1.025.546 1.377.203 2.394.1 2.647.64.699
-                  1.028 1.592 1.028 2.683 0 3.842-2.339 4.687-4.566 4.935.359.309.678.919.678 1.852 0 1.336
-                  -.012 2.415-.012 2.743 0 .267.18.578.688.48C19.138 20.167 22 16.418 22 12c0-5.523-4.477-10-10-10Z"
-                  fill="currentColor"
-                />
-              </svg>
-            </span>
-            <span class="d-btn__label">GitHub</span>
-          </a>
-          <a
             v-if="$frontmatter.storybook && $frontmatter.storybook !== 'planned'"
-            class="d-btn d-btn--muted d-btn--muted"
+            class="d-btn d-btn--muted d-btn--muted d-btn--sm"
             :href="$frontmatter.storybook"
             target="_blank"
             rel="noopener noreferrer"
           >
             <span class="d-btn__icon d-btn__icon--left">
               <svg
-                class="d-icon d-icon--size-300"
+                class="d-icon d-icon--size-200"
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
                 aria-hidden="true"
@@ -92,14 +63,14 @@
           </a>
           <a
             v-if="$frontmatter.figma_url"
-            class="d-btn d-btn--muted"
+            class="d-btn d-btn--muted d-btn--sm"
             :href="$frontmatter.figma_url"
             target="_blank"
             rel="noopener noreferrer"
           >
             <span class="d-btn__icon d-btn__icon--left">
               <svg
-                class="d-icon d-icon--size-300"
+                class="d-icon d-icon--size-200"
                 viewBox="0 0 24 24"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
@@ -140,6 +111,79 @@
             </span>
             <span class="d-btn__label">Figma</span>
           </a>
+          <a
+            v-if="githubUrl"
+            class="d-btn d-btn--muted d-btn--sm"
+            :href="githubUrl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span class="d-btn__icon d-btn__icon--left">
+              <svg
+                class="d-icon d-icon--size-200"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path
+                  d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.17 6.839 9.49.5.092.682-.217.682-.482
+                  0-.237-.008-.866-.013-1.7-2.782.604-3.369-1.34-3.369-1.34-.454-1.156-1.11-1.464-1.11-1.464
+                  -.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.087 2.91.831
+                  .092-.646.35-1.086.636-1.337-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683
+                  -.103-.253-.446-1.27.098-2.647 0 0 .84-.269 2.75 1.025A9.578 9.578 0 0 1 12 6.836c.85.004
+                  1.705.115 2.504.337 1.909-1.294 2.747-1.025 2.747-1.025.546 1.377.203 2.394.1 2.647.64.699
+                  1.028 1.592 1.028 2.683 0 3.842-2.339 4.687-4.566 4.935.359.309.678.919.678 1.852 0 1.336
+                  -.012 2.415-.012 2.743 0 .267.18.578.688.48C19.138 20.167 22 16.418 22 12c0-5.523-4.477-10-10-10Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </span>
+            <span class="d-btn__label">GitHub</span>
+          </a>
+          <span class="d-pl8">
+            <dt-dropdown navigation-type="arrow-keys" placement="bottom-end">
+              <template #anchor="{ attrs }">
+                <dt-button v-bind="attrs" size="sm" importance="outlined" kind="muted" icon-position="right">
+                  <dt-stack direction="row" gap="350">
+                    <dt-icon
+                      :name="showCopiedIcon ? 'check' : 'copy'"
+                      size="200"
+                      :class="{ 'd-fc-success': showCopiedIcon }"
+                    />
+                    Copy
+                  </dt-stack>
+                  <template #icon>
+                    <dt-icon
+                      name="chevron-down"
+                      size="200"
+                    />
+                  </template>
+                </dt-button>
+              </template>
+              <template #list="{ close }">
+                <dt-list-item-group>
+                  <dt-list-item role="menuitem" navigation-type="arrow-keys" @click="onCopyMarkdownLink(close)">
+                    Copy as Markdown
+                  </dt-list-item>
+                  <dt-list-item role="menuitem" navigation-type="arrow-keys" @click="onCopyMarkdownLink(close)">
+                    Copy Markdown link
+                  </dt-list-item>
+                </dt-list-item-group>
+                <dt-dropdown-separator />
+                <dt-list-item-group>
+                  <dt-list-item role="menuitem" navigation-type="arrow-keys">
+                    View as Markdown
+                  </dt-list-item>
+                  <dt-list-item role="menuitem" navigation-type="arrow-keys">
+                    Open in Claude.ai
+                  </dt-list-item>
+                  <dt-list-item role="menuitem" navigation-type="arrow-keys">
+                    Open in ChatGPT
+                  </dt-list-item>
+                </dt-list-item-group>
+              </template>
+            </dt-dropdown>
+          </span>
         </dt-stack>
       </dt-stack>
       <dt-text
@@ -168,10 +212,20 @@
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { usePageData } from 'vuepress/client';
 
 const page = usePageData();
+
+const showCopiedIcon = ref(false);
+let copiedTimeout;
+
+function onCopyMarkdownLink (close) {
+  close();
+  showCopiedIcon.value = true;
+  clearTimeout(copiedTimeout);
+  copiedTimeout = setTimeout(() => { showCopiedIcon.value = false; }, 2000);
+}
 
 const SLUG_OVERRIDES = { tabs: 'tab' };
 const EXCLUDED_SLUGS = new Set(['scrollbar', 'table']);
