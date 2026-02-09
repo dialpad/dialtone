425d44659de4ce6c468ed83208607b71858c1d6a
Joshua Hynes <jhynes@dialpad.com>
2026-02-09 11:03:41 -0500

    Remove AVATAR_COLORS from story files

--- packages/dialtone-vue/recipes/item_layout/contact_info/contact_info.stories.js
+++ packages/dialtone-vue/recipes/item_layout/contact_info/contact_info.stories.js
@@ -5,7 +5,7 @@ import DtRecipeContactInfo from './contact_info.vue';
 
 import DtRecipeContactInfoDefaultTemplate from './contact_info_default.story.vue';
 import DtRecipeContactInfoVariantsTemplate from './contact_info_variants.story.vue';
-import { AVATAR_SIZE_MODIFIERS, AVATAR_COLORS } from '@/components/avatar';
+import { AVATAR_SIZE_MODIFIERS } from '@/components/avatar';
 import { PRESENCE_STATES_LIST } from '@/components/presence';
 
 import avatarImage from '@/common/assets/avatar1.png?url';
@@ -49,16 +49,10 @@ export const argTypesData = {
   },
 
   avatarColor: {
-    options: [
-      undefined,
-      ...AVATAR_COLORS,
-    ],
     control: {
-      type: 'select',
-      labels: {
-        undefined: '(empty)',
-      },
+      type: 'text',
     },
+    description: 'Legacy color prop. Use avatarSeed instead for auto-generated colors.',
   },
 
   avatarLabelledBy: {
