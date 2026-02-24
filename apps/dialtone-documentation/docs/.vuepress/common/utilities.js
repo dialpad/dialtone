98ff1c82fcb3ca0d5819dabd68e7d394cbe62002
francisrupert <francis.rupert@dialpad.com>
2026-02-23 19:41:01 -0600

    css  variable name extraction supports oklch

--- apps/dialtone-documentation/docs/.vuepress/common/utilities.js
+++ apps/dialtone-documentation/docs/.vuepress/common/utilities.js
@@ -71,9 +71,10 @@ export function extractUtilityClasses (utilityClassDocs, prefix) {
 }
 
 export function extractCSSVariableName (propValue) {
-  const variable = Object.values(propValue.values)[0].value;
-  if (!variable.startsWith('var(')) return;
-  return variable.replace('var(', '').replace(/(-(h|s|c|l|a|hsl|hsla|oklch|oklcha))?\).*/, '');
+  const value = Object.values(propValue.values)[0].value;
+  const match = value.match(/var\((--[\w-]+)\)/);
+  if (!match) return;
+  return match[1].replace(/(-(h|s|c|l|a|hsl|hsla|oklch|oklcha))$/, '');
 }
 
 /**
