dc5d5a6666111dfd09c42def21a259f55ca3221e
francisrupert <francis.rupert@dialpad.com>
2026-03-12 15:31:32 -0500

    add caret position preservation to code editor input and restructure option bar layout with sticky section headers

--- packages/combinator/src/components/code_editor/code_editor_input.vue
+++ packages/combinator/src/components/code_editor/code_editor_input.vue
@@ -3,14 +3,51 @@
     class="dtc-code-editor-input d-ws-pre-wrap"
     spellcheck="false"
     contenteditable
-    @input="e => emit('input', e)"
+    @input="onInput"
   >
     <slot />
   </span>
 </template>
 
 <script setup>
+import { nextTick } from 'vue';
+
 const emit = defineEmits(['input']);
+
+function getCaretOffset (element) {
+  const sel = window.getSelection();
+  if (!sel.rangeCount) return 0;
+  const range = sel.getRangeAt(0).cloneRange();
+  range.selectNodeContents(element);
+  range.setEnd(sel.getRangeAt(0).startContainer, sel.getRangeAt(0).startOffset);
+  return range.toString().length;
+}
+
+function setCaretOffset (element, offset) {
+  const sel = window.getSelection();
+  const range = document.createRange();
+  const walker = document.createTreeWalker(element, NodeFilter.SHOW_TEXT);
+  let currentOffset = 0;
+  let node;
+  while ((node = walker.nextNode())) {
+    if (currentOffset + node.textContent.length >= offset) {
+      range.setStart(node, offset - currentOffset);
+      range.collapse(true);
+      sel.removeAllRanges();
+      sel.addRange(range);
+      return;
+    }
+    currentOffset += node.textContent.length;
+  }
+}
+
+function onInput (e) {
+  const offset = getCaretOffset(e.target);
+  emit('input', e);
+  nextTick(() => {
+    setCaretOffset(e.target, offset);
+  });
+}
 </script>
 
 <script>
