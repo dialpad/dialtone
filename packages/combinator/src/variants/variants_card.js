90dc44f6ced510f27131c9ef8c6a7a592fab67ab
francisrupert <francis.rupert@dialpad.com>
2026-03-14 20:53:05 -0500

    add targeted styling variant to card and update empty state variant presets

--- packages/combinator/src/variants/variants_card.js
+++ packages/combinator/src/variants/variants_card.js
@@ -25,4 +25,17 @@ export default {
       footer: { initialValue: '<dt-button importance="outlined" size="sm">Button</dt-button>' },
     },
   },
+  'Targeted styling': {
+    props: {
+      containerClass: { initialValue: 'd-bar0 d-baw0' },
+      contentClass: { initialValue: 'd-p8 d-by d-bgc-critical' },
+      headerClass: { initialValue: 'd-p8 d-bgc-info' },
+      footerClass: { initialValue: 'd-p8 d-bgc-warning' },
+    },
+    slots: {
+      header: { initialValue: '<dt-text as="p" kind="headline" size="md">Lorem ipsum</dt-text>' },
+      content: { initialValue: '<dt-text as="p" kind="body" size="sm"> Content slot. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum molestie semper. Morbi finibus nulla turpis, nec molestie mi rutrum.Content slot. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum molestie semper. Morbi finibus nulla turpis, nec molestie mi rutrum.Content slot. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum molestie semper. Morbi finibus nulla turpis, nec molestie mi rutrum. </dt-text>' },
+      footer: { initialValue: '<dt-text as="p" kind="body" size="sm">Footer</dt-text>' },
+    },
+  },
 };
