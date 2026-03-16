56b5475ef4bb1f273286ccee069bae000a98ea77
francisrupert <francis.rupert@dialpad.com>
2026-03-16 17:09:50 -0500

    variant presets for filter pill

--- packages/combinator/src/variants/variants_filter_pill.js
+++ packages/combinator/src/variants/variants_filter_pill.js
@@ -1,9 +1,84 @@
- 
+
 export default {
   default: {
     props: {
       label: {
-        initialValue: 'Filter label',
+        initialValue: 'Active example',
+      },
+      modelValue: {
+        initialValue: [
+          { name: 'Option 1' },
+          { name: 'Option 2' },
+          { name: 'Option 3', active: true },
+        ],
+      },
+    },
+  },
+
+  'no selection': {
+    props: {
+      label: {
+        initialValue: 'Simple example',
+      },
+      modelValue: {
+        initialValue: [
+          { name: 'Option 1' },
+          { name: 'Option 2' },
+          { name: 'Option 3' },
+        ],
+      },
+    },
+  },
+
+  'non clearable': {
+    props: {
+      label: {
+        initialValue: 'Non Clearable example',
+      },
+      modelValue: {
+        initialValue: [
+          { name: 'Option 1', active: true },
+          { name: 'Option 2' },
+          { name: 'Option 3' },
+        ],
+      },
+      hideClear: {
+        initialValue: true,
+      },
+    },
+  },
+
+  disabled: {
+    props: {
+      label: {
+        initialValue: 'Disabled filter',
+      },
+      modelValue: {
+        initialValue: [
+          { name: 'Option 1' },
+          { name: 'Option 2' },
+          { name: 'Option 3' },
+        ],
+      },
+      disabled: {
+        initialValue: true,
+      },
+    },
+  },
+
+  'content mode invert': {
+    props: {
+      label: {
+        initialValue: 'Inverted',
+      },
+      modelValue: {
+        initialValue: [
+          { name: 'Orange', active: true },
+          { name: 'Apple' },
+        ],
+      },
+      contentMode: {
+        initialValue: 'invert',
       },
     },
   },
