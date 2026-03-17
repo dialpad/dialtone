3d8ec4f12c132fb0fe95a457c3e0e28badd972d8
francisrupert <francis.rupert@dialpad.com>
2026-03-17 09:41:35 -0500

    remove data-qa attribute from loader track svg

--- packages/dialtone-vue/components/loader/loader.vue
+++ packages/dialtone-vue/components/loader/loader.vue
@@ -6,7 +6,6 @@
   >
     <svg
       :class="['d-icon', `d-icon--size-${size}`, 'd-loader__icon-track']"
-      data-qa="dt-loader-icon-track"
       viewBox="0 0 24 24"
       fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
