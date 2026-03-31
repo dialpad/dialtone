2d0009f8fca92e7c5d7ef1940c9999d828ed25be
Joshua Hynes <jhynes@dialpad.com>
2026-03-31 16:16:19 -0400

    fix(resizable): DLT-2097 move close button into sidebar, show open in content

--- apps/dialtone-documentation/docs/.vuepress/exampleComponents/ExampleResizableCollapsible.vue
+++ apps/dialtone-documentation/docs/.vuepress/exampleComponents/ExampleResizableCollapsible.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-w100p" style="height: 300px;">
-    <dt-resizable>
+    <dt-resizable @panel-collapse="onPanelCollapse">
       <dt-resizable-panel
         id="exc-sidebar"
         initial-size="25p"
@@ -8,16 +8,26 @@
         collapsible
         :collapsed="isCollapsed"
       >
-        <div class="d-d-flex d-ai-center d-jc-center d-w100p d-h100p d-bgc-purple-100">
-          <span class="d-fs-200 d-fw-bold d-fc-purple-400">Sidebar</span>
+        <div class="d-d-flex d-fd-column d-w100p d-h100p d-bgc-purple-100">
+          <div class="d-d-flex d-ai-center d-jc-flex-end d-px16 d-py8 d-bb d-bc-default">
+            <button class="d-btn d-btn--sm" @click="isCollapsed = true">
+              Close sidebar
+            </button>
+          </div>
+          <div class="d-d-flex d-ai-center d-jc-center d-fl1">
+            <span class="d-fs-200 d-fw-bold d-fc-purple-400">Sidebar</span>
+          </div>
         </div>
       </dt-resizable-panel>
       <dt-resizable-handle />
       <dt-resizable-panel id="exc-content">
         <div class="d-d-flex d-fd-column d-w100p d-h100p d-bgc-gold-100">
-          <div class="d-d-flex d-ai-center d-px16 d-py8 d-bb d-bc-default">
-            <button class="d-btn d-btn--sm" @click="isCollapsed = !isCollapsed">
-              {{ isCollapsed ? 'Show sidebar' : 'Hide sidebar' }}
+          <div
+            v-if="isCollapsed"
+            class="d-d-flex d-ai-center d-px16 d-py8 d-bb d-bc-default"
+          >
+            <button class="d-btn d-btn--sm" @click="isCollapsed = false">
+              Open sidebar
             </button>
           </div>
           <div class="d-d-flex d-ai-center d-jc-center d-fl1">
@@ -34,6 +44,14 @@ export default {
   name: 'ExampleResizableCollapsible',
   data () {
     return { isCollapsed: false };
+  },
+
+  methods: {
+    onPanelCollapse (panelId, collapsed) {
+      if (panelId === 'exc-sidebar') {
+        this.isCollapsed = collapsed;
+      }
+    },
   },
 };
 </script>
