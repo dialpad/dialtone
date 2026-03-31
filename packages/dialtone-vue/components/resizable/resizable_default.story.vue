8685b1ef7e08a7f7214cc105f8296c9964e804c0
Joshua Hynes <jhynes@dialpad.com>
2026-03-31 12:05:40 -0400

    fix(resizable): DLT-2097 use Top/Bottom labels for column direction story

--- packages/dialtone-vue/components/resizable/resizable_default.story.vue
+++ packages/dialtone-vue/components/resizable/resizable_default.story.vue
@@ -2,15 +2,15 @@
   <!-- eslint-disable vue/no-static-inline-styles -->
   <div style="height: 400px; border: 1px solid var(--dt-color-border-default);">
     <dt-resizable :direction="$attrs.direction">
-      <dt-resizable-panel id="left">
+      <dt-resizable-panel :id="isColumn ? 'top' : 'left'">
         <div class="d-d-flex d-ai-center d-jc-center d-w100p d-h100p d-bgc-purple-100">
-          <span class="d-fs-200 d-fw-bold d-fc-purple-400">Left Panel</span>
+          <span class="d-fs-200 d-fw-bold d-fc-purple-400">{{ isColumn ? 'Top' : 'Left' }} Panel</span>
         </div>
       </dt-resizable-panel>
       <dt-resizable-handle />
-      <dt-resizable-panel id="right">
+      <dt-resizable-panel :id="isColumn ? 'bottom' : 'right'">
         <div class="d-d-flex d-ai-center d-jc-center d-w100p d-h100p d-bgc-gold-100">
-          <span class="d-fs-200 d-fw-bold d-fc-gold-400">Right Panel</span>
+          <span class="d-fs-200 d-fw-bold d-fc-gold-400">{{ isColumn ? 'Bottom' : 'Right' }} Panel</span>
         </div>
       </dt-resizable-panel>
     </dt-resizable>
@@ -28,6 +28,12 @@ export default {
     DtResizable,
     DtResizablePanel,
     DtResizableHandle,
+  },
+
+  computed: {
+    isColumn () {
+      return this.$attrs.direction === 'column';
+    },
   },
 };
 </script>
