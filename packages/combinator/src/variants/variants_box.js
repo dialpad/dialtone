3a265eced34a9025baedd755fa2f66bd66e21de0
francisrupert <francis.rupert@dialpad.com>
2026-04-16 21:30:53 -0500

    add token category defaults and update variant examples

--- packages/combinator/src/variants/variants_box.js
+++ packages/combinator/src/variants/variants_box.js
@@ -1,5 +1,33 @@
 /* eslint-disable max-len */
 export default {
+  defaults: {
+    props: {
+      surface: { tokenCategory: 'color:d-box--surface-:--box-surface' },
+      borderColor: { tokenCategory: 'color:d-box--bc-:--box-bc' },
+      padding: { tokenCategory: 'spacing' },
+      paddingInline: { tokenCategory: 'spacing' },
+      paddingInlineStart: { tokenCategory: 'spacing' },
+      paddingInlineEnd: { tokenCategory: 'spacing' },
+      paddingBlock: { tokenCategory: 'spacing' },
+      paddingBlockStart: { tokenCategory: 'spacing' },
+      paddingBlockEnd: { tokenCategory: 'spacing' },
+      borderWidth: { tokenCategory: 'border-width' },
+      borderWidthInline: { tokenCategory: 'border-width' },
+      borderWidthInlineStart: { tokenCategory: 'border-width' },
+      borderWidthInlineEnd: { tokenCategory: 'border-width' },
+      borderWidthBlock: { tokenCategory: 'border-width' },
+      borderWidthBlockStart: { tokenCategory: 'border-width' },
+      borderWidthBlockEnd: { tokenCategory: 'border-width' },
+      borderRadius: { tokenCategory: 'border-radius' },
+      inlineSize: { tokenCategory: 'layout' },
+      blockSize: { tokenCategory: 'layout' },
+      minInlineSize: { tokenCategory: 'layout' },
+      maxInlineSize: { tokenCategory: 'layout' },
+      minBlockSize: { tokenCategory: 'layout' },
+      maxBlockSize: { tokenCategory: 'layout' },
+    },
+  },
+
   default: {
     slots: {
       default: { initialValue: 'Box content' },
@@ -40,20 +68,24 @@ export default {
     },
   },
 
-  'semantic nav element': {
+  'combined with DtStack and DtText': {
     props: {
-      as: { initialValue: 'nav' },
       padding: { initialValue: '200' },
-      surface: { initialValue: 'secondary' },
-
-      borderWidth: { initialValue: '100' },
-      borderRadius: { initialValue: '200' },
+      surface: { initialValue: 'moderate-opaque' },
+      borderRadius: { initialValue: '450' },
+      shadow: { initialValue: 'card' },
     },
     slots: {
       default: { initialValue: `<dt-stack direction="row" gap="200">
-  <dt-text kind="body" size="sm">Home</dt-text>
-  <dt-text kind="body" size="sm">About</dt-text>
-  <dt-text kind="body" size="sm">Contact</dt-text>
+  <dt-box surface="moderate-opaque" border-radius="400" padding="100" padding-inline="150">
+    <dt-text kind="body" size="sm">First</dt-text>
+  </dt-box>
+  <dt-box surface="moderate-opaque" border-radius="400" padding="100" padding-inline="150">
+    <dt-text kind="body" size="sm">Second</dt-text>
+  </dt-box>
+  <dt-box surface="moderate-opaque" border-radius="400" padding="100" padding-inline="150">
+    <dt-text kind="body" size="sm">Third</dt-text>
+  </dt-box>
 </dt-stack>` },
     },
   },
@@ -102,6 +134,7 @@ export default {
       padding: { initialValue: '200' },
       surface: { initialValue: 'primary' },
       borderWidth: { initialValue: '100' },
+      borderColor: { initialValue: 'subtle' },
       borderRadius: { initialValue: '400' },
     },
     slots: {
@@ -112,21 +145,21 @@ export default {
       <template #endIcon="{ iconSize }">
         <dt-icon name="chevron-down" :size="iconSize" />
       </template>
-      Add thing
+      Add Box
     </dt-button>
   </dt-stack>
   <dt-stack direction="row" align="start" gap="200">
     <dt-box class="d-fl1" padding="200" surface="secondary" border-width="100" border-color="subtle" border-radius="300">
-      <dt-text as="p" align="center" tone="muted">Box 1</dt-text>
+      <dt-text as="p" kind="body" size="200" align="center" tone="muted">Box 1</dt-text>
     </dt-box>
     <dt-box class="d-fl1" padding="200" surface="secondary" border-width="100" border-color="subtle" border-radius="300">
-      <dt-text as="p" align="center" tone="muted">Box 2</dt-text>
+      <dt-text as="p" kind="body" size="200" align="center" tone="muted">Box 2</dt-text>
     </dt-box>
     <dt-box class="d-fl1" padding="200" surface="secondary" border-width="100" border-color="subtle" border-radius="300">
-      <dt-text as="p" align="center" tone="muted">Box 3</dt-text>
+      <dt-text as="p" kind="body" size="200" align="center" tone="muted">Box 3</dt-text>
     </dt-box>
     <dt-box class="d-fl1" padding="200" surface="secondary" border-width="100" border-color="subtle" border-radius="300">
-      <dt-text as="p" align="center" tone="muted">Box 4</dt-text>
+      <dt-text as="p" kind="body" size="200" align="center" tone="muted">Box 4</dt-text>
     </dt-box>
   </dt-stack>
 </dt-stack>` },
