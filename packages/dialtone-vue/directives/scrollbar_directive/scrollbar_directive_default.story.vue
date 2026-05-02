8a1af0d826aa6e9ffe9ca07f7a339197ceb9617f
Brad Paugh <brad.paugh@dialpad.com>
2026-05-01 17:12:06 -0700

    fix specificity

--- packages/dialtone-vue/directives/scrollbar_directive/scrollbar_directive_default.story.vue
+++ packages/dialtone-vue/directives/scrollbar_directive/scrollbar_directive_default.story.vue
@@ -60,6 +60,46 @@
         </div>
       </div>
     </div>
+    <div>
+      <p class="d-label--md-compact d-mbe-100">
+        Offset — block-start &amp; block-end (2rem each)
+      </p>
+      <div
+        v-dt-scrollbar="{ showScrollbar: 'always', offset: { block: '2rem' } }"
+        class="d-h-250 d-w-400 d-bar-400 d-bc-default d-ba"
+      >
+        <div class="d-p-200">
+          <p
+            v-for="i in 20"
+            :key="i"
+            class="d-body--md"
+          >
+            Scrollable content line {{ i }}
+          </p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <p class="d-label--md-compact d-mbe-100">
+        Offset — individual sides (blockStart 3rem, blockEnd 1rem, inlineEnd 1rem)
+      </p>
+      <div
+        v-dt-scrollbar="{
+          showScrollbar: 'always', offset: { blockStart: '3rem', blockEnd: '1rem', inlineEnd: '1rem' },
+        }"
+        class="d-h-250 d-w-400 d-bar-400 d-bc-default d-ba"
+      >
+        <div class="d-p-200">
+          <p
+            v-for="i in 20"
+            :key="i"
+            class="d-body--md"
+          >
+            Scrollable content line {{ i }}
+          </p>
+        </div>
+      </div>
+    </div>
   </dt-stack>
 </template>
 
