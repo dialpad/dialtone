4790fbf4f27e178f66c726aba4050fb97095a8df
francisrupert <francis.rupert@dialpad.com>
2026-06-13 17:19:40 -0500

    prep for combinator right side target

--- apps/dialtone-documentation/docs/.vuepress/theme/components/Page.vue
+++ apps/dialtone-documentation/docs/.vuepress/theme/components/Page.vue
@@ -1,120 +1,129 @@
 <template>
-  <dt-stack gap="300">
-    <DtStack direction="row" align="start" gap="250" class="d-100p">
+  <DtStack direction="row" align="start" gap="250" class="d-100p">
+    <DtBox
+      class="d-fl1 d-mx-auto"
+      min-inline-size="0"
+      max-inline-size="1600"
+    >
       <DtBox
-        class="d-fl1 d-mx-auto"
-        min-inline-size="0"
-        max-inline-size="1600"
+        id="page-sticky-header"
+        padding-block-start="250"
+        surface="primary"
+        class="d-ps-sticky d-ibs-0 d-zi-navigation-fixed"
       >
-        <DtBox
-          id="page-sticky-header"
-          padding-block-start="250"
-          surface="primary"
-          class="d-ps-sticky d-ibs-0 d-zi-navigation-fixed"
-        >
-          <DtBox border-width-block-end="100">
-            <page-header />
-            <dt-tab-group
-              v-if="$frontmatter.status"
-              selected="panel-1"
-              borderless
-            >
-              <template #tabs>
-                <dt-tab id="tab-1" panel-id="panel-1" selected>
-                  <template #startIcon="{ iconSize }">
-                    <dt-icon name="file" :size="iconSize" />
-                  </template>
-                  Documentation
-                </dt-tab>
-                <dt-tab id="tab-2" panel-id="panel-2">
-                  <template #startIcon="{ iconSize }">
-                    <dt-icon name="code" :size="iconSize" />
-                  </template>
-                  Properties
-                </dt-tab>
-              </template>
-            </dt-tab-group>
-          </DtBox>
-        </DtBox>
-        <DtBox padding-block-start="250">
-          <!-- eslint-disable-next-line vue/no-undef-components -->
-          <component-combinator
-            v-if="componentCombinatorName"
-            :component-name="componentCombinatorName"
-          />
-        </DtBox>
-        <!-- eslint-disable-next-line vue/no-undef-components -->
-        <content class="d-docsite-article" />
-        <DtStack gap="300">
-          <DtStack
-            as="nav"
-            direction="row"
-            :justify="prev ? 'between' : 'end'"
-            align="center"
-            class="d-pbs-400"
-            gap="400"
+        <DtBox border-width-block-end="100">
+          <page-header />
+          <dt-tab-group
+            v-if="$frontmatter.status"
+            selected="panel-1"
+            borderless
           >
-            <dt-button
-              v-if="prev"
-              :to="prev.link"
-              class="d-wmn40p"
-              label-class="d-jc-space-between"
-              importance="outlined"
-              kind="muted"
-              :size="400"
-            >
-              <template #startIcon>
-                <dt-icon name="arrow-left" />
-              </template>
-              <dt-stack as="span" class="d-p-100">
-                <dt-text as="span" kind="body" :size="300" tone="muted">
-                  Previous
-                </dt-text>
-                <span>{{ prev.text }}</span>
-              </dt-stack>
-            </dt-button>
-            <dt-button
-              v-if="next"
-              :to="next.link"
-              class="d-wmn40p"
-              label-class="d-jc-space-between"
-              importance="outlined"
-              kind="muted"
-              :size="400"
-            >
-              <template #endIcon>
-                <dt-icon name="arrow-right" />
-              </template>
-              <dt-stack as="span" class="d-p-100">
-                <dt-text as="span" kind="body" :size="300" tone="muted">
-                  Next
-                </dt-text>
-                <span>{{ next.text }}</span>
-              </dt-stack>
-            </dt-button>
-          </DtStack>
-          <DtBox>
-            <dt-text as="p" kind="body" :size="300" tone="muted">
-              <dt-text v-if="$frontmatter.title">
-                {{ $frontmatter.title }}
+            <template #tabs>
+              <dt-tab id="tab-1" panel-id="panel-1" selected>
+                <template #startIcon="{ iconSize }">
+                  <dt-icon name="file" :size="iconSize" />
+                </template>
+                Documentation
+              </dt-tab>
+              <dt-tab id="tab-2" panel-id="panel-2">
+                <template #startIcon="{ iconSize }">
+                  <dt-icon name="code" :size="iconSize" />
+                </template>
+                Properties
+              </dt-tab>
+            </template>
+          </dt-tab-group>
+        </DtBox>
+      </DtBox>
+      <DtBox padding-block-start="250">
+        <!-- eslint-disable-next-line vue/no-undef-components -->
+        <component-combinator
+          v-if="componentCombinatorName"
+          :component-name="componentCombinatorName"
+        />
+      </DtBox>
+      <!-- eslint-disable-next-line vue/no-undef-components -->
+      <content class="d-docsite-article" />
+      <DtStack gap="300">
+        <DtStack
+          as="nav"
+          direction="row"
+          :justify="prev ? 'between' : 'end'"
+          align="center"
+          class="d-pbs-400"
+          gap="400"
+        >
+          <dt-button
+            v-if="prev"
+            :to="prev.link"
+            class="d-wmn40p"
+            label-class="d-jc-space-between"
+            importance="outlined"
+            kind="muted"
+            :size="400"
+          >
+            <template #startIcon>
+              <dt-icon name="arrow-left" />
+            </template>
+            <dt-stack as="span" class="d-p-100">
+              <dt-text as="span" kind="body" :size="300" tone="muted">
+                Previous
               </dt-text>
-              documentation last updated
-              <dt-text>{{ lastUpdated }}</dt-text>
-            </dt-text>
-          </DtBox>
+              <span>{{ prev.text }}</span>
+            </dt-stack>
+          </dt-button>
+          <dt-button
+            v-if="next"
+            :to="next.link"
+            class="d-wmn40p"
+            label-class="d-jc-space-between"
+            importance="outlined"
+            kind="muted"
+            :size="400"
+          >
+            <template #endIcon>
+              <dt-icon name="arrow-right" />
+            </template>
+            <dt-stack as="span" class="d-p-100">
+              <dt-text as="span" kind="body" :size="300" tone="muted">
+                Next
+              </dt-text>
+              <span>{{ next.text }}</span>
+            </dt-stack>
+          </dt-button>
         </DtStack>
-      </DtBox>
-      <DtBox
-        v-if="!isMobile && includeToc"
-        max-inline-size="300"
-        min-inline-size="300"
-        padding-block-start="200"
-        class="d-ps-sticky d-ibs-450 d-d-none xl:d-d-block"
-      >
-        <page-toc :headers="headers" />
-      </DtBox>
-    </DtStack>
-  </dt-stack>
+        <DtBox>
+          <dt-text as="p" kind="body" :size="300" tone="muted">
+            <dt-text v-if="$frontmatter.title">
+              {{ $frontmatter.title }}
+            </dt-text>
+            documentation last updated
+            <dt-text>{{ lastUpdated }}</dt-text>
+          </dt-text>
+        </DtBox>
+      </DtStack>
+    </DtBox>
+    <DtBox
+      v-if="!isMobile && includeToc"
+      max-inline-size="300"
+      min-inline-size="300"
+      padding-block-start="200"
+      class="d-ps-sticky d-ibs-450 d-d-none xl:d-d-block"
+    >
+      <page-toc :headers="headers" />
+    </DtBox>
+    <!--
+    <DtBox
+      id="combinator-side-target"
+      surface="moderate"
+      padding-block-start="250"
+      block-size="800"
+      class="d-fl1 d-ps-sticky d-ibs-0"
+    >
+      asdf
+    </DtBox>
+    -->
+  </DtStack>
 </template>
 
 <script setup>
