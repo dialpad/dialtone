42e1bc9b8bba6540fb51fe007a0078b57bddd619
Paulo Reis <paulo.reis@dialpad.com>
2026-06-16 10:33:11 +0100

    refactor(combinator): NO-JIRA reuse control-selection for icon slot controls

--- packages/combinator/src/components/controls/control_icon_slot.vue
+++ packages/combinator/src/components/controls/control_icon_slot.vue
@@ -1,81 +1,25 @@
 <template>
-  <dt-recipe-combobox-with-popover
-    ref="combobox"
-    label="Icon"
-    max-height="320px"
-    list-class="d-w-250 d-hmx-350"
-    :size="100"
-    append-to="body"
-    @select="e => onSelect(filteredIcons[e])"
-    @opened="onOpen"
-    @focusout="onFocusOut"
+  <dtc-control-selection
+    :value="iconName"
+    :valid-values="iconNames"
+    :default-value="null"
+    :disabled="disabled"
+    @update:value="onSelect"
   >
-    <template #input="{ inputProps, onInput }">
-      <dtc-control-string
-        v-bind="inputProps"
-        :value="searchText"
-        :disabled="disabled"
-        @update:value="e => onInputInternal(e, onInput)"
-      >
-        <template #default>
-          <slot />
-        </template>
-        <template #icon="{ iconSize }">
-          <dt-button
-            v-if="isModified"
-            kind="muted"
-            importance="clear"
-            :size="100"
-            class="d-p-25"
-            @click.stop="onReset"
-          >
-            <template #icon>
-              <dt-icon-close size="100" />
-            </template>
-          </dt-button>
-          <component
-            :is="DtIconChevronsUpDown"
-            v-else
-            :size="iconSize"
-            class="d-fc-muted"
-          />
-        </template>
-      </dtc-control-string>
-    </template>
-    <template #list="{ listProps }">
-      <ul
-        class="d-p-50"
-        v-bind="listProps"
-      >
-        <dt-list-item
-          v-for="(item, i) in filteredIcons"
-          :key="item"
-          navigation-type="arrow-keys"
-          role="option"
-          @click="onSelectInternal(i)"
-        >
-          {{ item }}
-        </dt-list-item>
-      </ul>
-    </template>
-  </dt-recipe-combobox-with-popover>
+    <slot />
+  </dtc-control-selection>
 </template>
 
 <script setup>
-import DtcControlString from './control_string.vue';
-import { DtButton, DtRecipeComboboxWithPopover, DtListItem } from '@dialpad/dialtone-vue';
-import { DtIconChevronsUpDown, DtIconClose } from '@dialpad/dialtone-icons/vue';
-
-import { VALUE_UPDATE_EVENT } from '@/src/lib/constants';
-import { computed, ref, watch } from 'vue';
+import DtcControlSelection from './control_selection.vue';
 import {
   iconNames,
   iconNameToTemplate,
   templateToIconName,
   hasIconSizeBinding,
 } from '@/src/lib/icons';
-
-const NONE_OPTION = '(none)';
+import { VALUE_UPDATE_EVENT } from '@/src/lib/constants';
+import { computed } from 'vue';
 
 const props = defineProps({
   value: {
@@ -98,66 +42,15 @@ const props = defineProps({
 
 const emit = defineEmits([VALUE_UPDATE_EVENT]);
 
-const searchText = ref(templateToIconName(props.value) ?? '');
+const iconName = computed(() => templateToIconName(props.value) ?? null);
 
-watch(() => props.value, (newVal) => {
-  searchText.value = templateToIconName(newVal) ?? '';
-});
-
-const isModified = computed(() => props.value !== props.defaultValue);
-
-const allIcons = computed(() => [NONE_OPTION, ...iconNames]);
-
-const filteredIcons = computed(() => {
-  const query = searchText.value.toLowerCase();
-  if (!query) return allIcons.value;
-  return allIcons.value.filter(name =>
-    name.toLowerCase().includes(query),
-  );
-});
-
-const combobox = ref();
-const open = ref(false);
-
-function onOpen (e) {
-  open.value = e;
-}
-
-function onFocusOut (e) {
-  if (e.relatedTarget?.closest('[data-tippy-root]')) return;
-  if (combobox.value?.$el?.contains(e.relatedTarget)) return;
-  if (open.value) {
-    combobox.value?.closeComboboxList();
-  }
-}
-
-function onSelectInternal (i) {
-  combobox.value.onSelect(i);
-  combobox.value.closeComboboxList();
-}
-
-function onSelect (iconName) {
-  if (iconName === NONE_OPTION) {
-    searchText.value = '';
+function onSelect (name) {
+  if (name === null) {
     emit(VALUE_UPDATE_EVENT, null);
     return;
   }
-  searchText.value = iconName;
   const isScoped = hasIconSizeBinding(props.bindings);
-  emit(VALUE_UPDATE_EVENT, iconNameToTemplate(iconName, isScoped));
-}
-
-function onReset () {
-  searchText.value = templateToIconName(props.defaultValue) ?? '';
-  emit(VALUE_UPDATE_EVENT, props.defaultValue);
-}
-
-function onInputInternal (value, onInput) {
-  searchText.value = value;
-  if (!value) {
-    emit(VALUE_UPDATE_EVENT, null);
-  }
-  onInput(value);
+  emit(VALUE_UPDATE_EVENT, iconNameToTemplate(name, isScoped));
 }
 </script>
 
