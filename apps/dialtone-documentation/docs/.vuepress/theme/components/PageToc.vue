71ccc301a5298d137a149802f41b0ca336cc62a5
francisrupert <francis.rupert@dialpad.com>
2026-06-16 13:52:29 -0500

    remove alwaysExpanded logic and associated styles from PageToc component

--- apps/dialtone-documentation/docs/.vuepress/theme/components/PageToc.vue
+++ apps/dialtone-documentation/docs/.vuepress/theme/components/PageToc.vue
@@ -13,28 +13,6 @@
             @navigate="handleNavigate($event, header)"
           />
         </li>
-        <li v-else-if="alwaysExpanded">
-          <toc-item
-            :active="isItemActive(header)"
-            :to="header.link"
-            :text="header.title"
-            @navigate="handleNavigate($event, header)"
-          />
-          <ul class="d-pis-100">
-            <li
-              v-for="child in header.children"
-              :key="child.slug"
-              class="d-fw-wrap"
-            >
-              <toc-item
-                :active="isItemActive(child)"
-                :to="child.link"
-                :text="child.title"
-                @navigate="handleNavigate($event, child)"
-              />
-            </li>
-          </ul>
-        </li>
         <dt-collapsible
           v-else
           element-type="li"
@@ -82,10 +60,6 @@ const props = defineProps({
     type: Array,
     default: () => [],
   },
-  alwaysExpanded: {
-    type: Boolean,
-    default: false,
-  },
   activeHash: {
     type: String,
     default: '',
@@ -107,11 +81,3 @@ function handleNavigate (event, item) {
   emit('navigate', event, item);
 }
 </script>
-
-<style lang="less" scoped>
-.dialtone-toc {
-  inline-size: var(--dt-layout-300);
-  block-size: calc(100vh - var(--dt-layout-100));
-  inset-block-start: var(--dt-layout-100);
-}
-</style>
